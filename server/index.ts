3693975237b8514949b80e685f1e3be3c7814d76
Ryan DECIAN <ryan.decian.propgmail.com>
2025-01-27 13:04:22 +0100

    Ajout de commentaire pour la lisibilité

--- server/index.ts
+++ server/index.ts
@@ -3,10 +3,19 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = 8080;
 
+/**
+ * Route de base
+ * Path: /
+ * Action callBack
+ * Methode: GET
+ */
 app.get("/", (req: Request, res: Response) => {
     res.send("API de Anne SAUNIER");
 })
 
+/**
+ * Le server se lance sur le port 8080
+ */
 app.listen(port, () => {
     console.log(`Server lancé sur http://localhost:${port}`);
 });
