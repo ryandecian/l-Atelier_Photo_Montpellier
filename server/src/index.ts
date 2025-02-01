cbf378a19359e51f86377af96e38ef35cc3d51a1
Ryan DECIAN <ryan.decian.propgmail.com>
2025-02-01 23:36:45 +0100

    Mise a jour de la route, correction de /login qui passe de get a post

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ app.get("/", (req: Request, res: Response) => {
  * Action callBack
  * Methode: POST
  */
-app.get("/login", (req: Request, res: Response) => {
+app.post("/login", (req: Request, res: Response) => {
     res.json("ok");
 })
 
