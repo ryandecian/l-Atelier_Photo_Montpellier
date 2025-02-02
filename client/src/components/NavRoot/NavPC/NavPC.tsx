8f51809527e39f0154ea51c360eb41e84caba6de
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-02 16:15:56 +0100

    Ajout du Logo

--- client/src/components/NavRoot/NavPC/NavPC.tsx
+++ client/src/components/NavRoot/NavPC/NavPC.tsx
@@ -1,9 +1,17 @@
 import "./NavPC.css";
+import { Link } from "react-router-dom";
+
+/* Import des Images*/
+import Logo from "../../../assets/Logo/LOGO-Avec-Ou-Sans-Flash.jpeg";
 
 function NavPC() {
     return (
         <nav className="NavPC">
-            <div className="ContainerLogoNavPC"></div>
+            <div className="ContainerLogoNavPC">
+                <Link to="/">
+                   <img src={Logo} alt="Logo Avec Ou Sans Flash" />
+                </Link>
+            </div>
             <div className="ContainerNavigationNavPC"></div>
         </nav>
     )
