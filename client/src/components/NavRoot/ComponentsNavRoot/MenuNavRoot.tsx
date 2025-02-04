94490a4ae206fc4f126a8e18d47d781ea92b5ab7
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-04 11:32:49 +0100

    Création du sous menu

--- client/src/components/NavRoot/ComponentsNavRoot/MenuNavRoot.tsx
+++ client/src/components/NavRoot/ComponentsNavRoot/MenuNavRoot.tsx
@@ -1,39 +1,54 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-/*Le style de ce composant est directement géré par le 
-CSS du composant qui l'importe et l'utilise*/
-
 interface MenuNavRootProps {
-    moduleMenuUl: string;
-    moduleMenuLi: string;
-    moduleMenuLink: string;
+  moduleMenuUl: string;
+  moduleMenuLi: string;
+  moduleMenuLink: string;
+  moduleSubMenu?: string;
+  moduleSubMenuLi?: string;
 }
 
 function MenuNavRoot(Props: MenuNavRootProps) {
-    const {moduleMenuUl, moduleMenuLi, moduleMenuLink} = Props
-    return (
-        <>
-         <ul className={moduleMenuUl}>
+  const { moduleMenuUl, moduleMenuLi, moduleMenuLink, moduleSubMenu, moduleSubMenuLi } = Props;
 
-             <li className={moduleMenuLi}>
-                 <Link to="/actualite" className={moduleMenuLink}>Actualité</Link>
-             </li>
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
 
-             <li className={moduleMenuLi}>
-                 <Link to="/mariage" className={moduleMenuLink}>Mariage</Link>
-             </li>
-             
-             <li className={moduleMenuLi}>
-                 <Link to="portraits" className={moduleMenuLink}>Portraits</Link>
-             </li>
+  return (
+    <ul className={moduleMenuUl}>
+      <li className={moduleMenuLi}>
+        <Link to="/actualite" className={moduleMenuLink}>Actualité</Link>
+      </li>
 
-             <li className={moduleMenuLi}>
-                 <Link to="tarifs" className={moduleMenuLink}>Tarifs</Link>
-             </li>
+      <li className={moduleMenuLi}>
+        <Link to="/mariage" className={moduleMenuLink}>Mariage</Link>
+      </li>
 
-         </ul>
-        </>
-    )
+      {/* Menu déroulant pour Portraits */}
+      <li className={moduleMenuLi} onClick={() => setIsSubMenuOpen(!isSubMenuOpen)}>
+        <span className={moduleMenuLink} style={{ cursor: "pointer" }}>
+          Portraits ▼
+        </span>
+        {isSubMenuOpen && (
+          <ul className={moduleSubMenu}>
+            <li className={moduleSubMenuLi}>
+              <Link to="/portraits/studio" className={moduleMenuLink}>Studio</Link>
+            </li>
+            <li className={moduleSubMenuLi}>
+              <Link to="/portraits/exterieur" className={moduleMenuLink}>Extérieur</Link>
+            </li>
+            <li className={moduleSubMenuLi}>
+              <Link to="/portraits/famille" className={moduleMenuLink}>Famille</Link>
+            </li>
+          </ul>
+        )}
+      </li>
+
+      <li className={moduleMenuLi}>
+        <Link to="/tarifs" className={moduleMenuLink}>Tarifs</Link>
+      </li>
+    </ul>
+  );
 }
 
 export default MenuNavRoot;
