41d73afe5d74019fa0d6b6fc972e698f1fe5da80
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-07 10:36:00 +0100

    Ajout des paquets de mysql2 + création du dossier database + ajout du fichier .env

---
+++ server/database/config.ts
@@ -0,0 +1,9 @@
+// Get the client
+import mysql from 'mysql2/promise';
+
+// Create the connection to database
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test',
+});
