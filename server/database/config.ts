c2151c2762e4e4b89a36bd378fad795ec633ca39
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-07 12:18:35 +0100

    Configuration de la DB

--- server/database/config.ts
+++ server/database/config.ts
@@ -1,9 +1,15 @@
+/**
+ * Import des .env pour l'utilisation
+ */
+import "dotenv/config";
+
 // Get the client
 import mysql from 'mysql2/promise';
 
 // Create the connection to database
 const connection = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'test',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
