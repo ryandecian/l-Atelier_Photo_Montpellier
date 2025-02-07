a75aa06a8b96810e34c18282cafb0eeb95c1f768
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-07 13:37:36 +0100

    Création de la fonction de connection à la DB avec l'utilisation d'une fonction async await pour les promesse, et intégration des try catch pour la gestion des erreurs. Le code est donc fiable et robuste

--- server/database/config.ts
+++ server/database/config.ts
@@ -6,10 +6,25 @@ import "dotenv/config";
 // Get the client
 import mysql from 'mysql2/promise';
 
-// Create the connection to database
-const connection = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// Fonction pour créer la connexion avec gestion des erreurs
+async function createConnection() {
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+
+    return connection;
+  }
+
+  catch (error) {
+    console.error("❌ Erreur lors de la connexion à MySQL :", error);
+    throw error; // Relancer l'erreur pour que l'application puisse la gérer
+  }
+}
+
+// Export d'une promesse de connexion
+export const dbConnection = createConnection();
