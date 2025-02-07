ec9492e4e16c966208b5242194c0ed9966ce7c61
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-07 19:09:32 +0100

    Création de la première connection a la db pour vérifier si un user existe

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,17 @@ app.use(express.json());
  * Methode: GET
  */
 app.get("/", (req: Request, res: Response) => {
-    res.send("API de Anne SAUNIER !!!");
+    res.status(200).send("API de Anne SAUNIER !!!");
+})
+
+/**
+ * Route de base
+ * Path: /
+ * Action callBack
+ * Methode: POST
+ */
+app.post("/", (req: Request, res: Response) => {
+    res.status(502).json({ Reponse: "Requête invalide !", data: req.body })
 })
 
 /**
@@ -28,9 +38,28 @@ app.get("/", (req: Request, res: Response) => {
  * Action callBack
  * Methode: POST
  */
-app.post("/login", (req: Request, res: Response) => {
-    console.log(req.body)
-    res.json(req.body);
+interface UserType {
+    id: number;
+    firstname: string;
+    lastname: string;
+    email: string
+}
+app.post("/login", async(req: Request, res: Response) => {
+    try {
+        const connection = await useConnection;
+        const [results] = await connection.query("SELECT * FROM user WHERE email= ?", [req.body.email]) as any[];
+        if (results.length !== 0) {
+            res.status(200).json({ Reponse: "Il existe !", data: results})
+        }
+        else {
+        res.status(404).json({ response: "il n'existe pas" })}
+    }
+    
+    catch (error) {
+        console.error("Erreur lors de la requête SQL :", error)
+        res.status(500).json({ error: "Erreur lors de l'accès à la base de données." })
+        throw error;
+    }
 })
 
 
