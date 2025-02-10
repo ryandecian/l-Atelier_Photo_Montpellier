56b1c3768fe4e8075d717985d26e22b3505ec2c8
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-10 11:53:05 +0100

    Création de la logique register

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from "express";
+import express, { query, Request, Response } from "express";
 import { useConnection } from "./database/config";
-import { RowDataPacket } from "mysql2";
+import { ResultSetHeader, RowDataPacket } from "mysql2";
 import { promises } from "dns";
 
 const app = express();
@@ -35,6 +35,46 @@ app.post("/", (req: Request, res: Response) => {
 })
 
 /**
+ * Route de register
+ * Path: /register
+ * Action callBack
+ * Methode: POST
+ */
+interface registerType {
+    id: number;
+    firstname: string;
+    lastname: string;
+    address: string;
+    email: string
+    password: string;
+    date_save: string;
+}
+app.post("/register", async (req: Request, res: Response): Promise<void> => {
+    try {
+        const connection = await useConnection;
+        const [results] = await connection.query<ResultSetHeader>(
+            "INSERT INTO user (firstname, lastname, address, email, password) VALUES (?, ?, ?, ?, ?)",
+            [req.body.firstname, req.body.lastname, req.body.address, req.body.email, req.body.password],
+        );
+
+        if (results.affectedRows === 0) {
+            res.status(400).json({ Reponse: "La synthaxe de la requête est erronée"});
+            return;
+        }
+        else {
+            res.status(201).json({ Reponse: "Enregistrement ok", data: results});
+            return;
+        }
+    }
+    catch (error) {
+        console.error ("Erreur lors de la requête SQL :", error);
+        res.status(500).json({ error: "Erreur lors de l'accès à la base de données." });
+        throw error; // Propage l'erreur pour mieux la gérer
+        return;
+    }
+})
+
+/**
  * Route de login
  * Path: /login
  * Action callBack
@@ -44,7 +84,7 @@ interface UserType {
     id: number;
     firstname: string;
     lastname: string;
-    adress: string;
+    address: string;
     email: string
     password: string;
     date_save: string;
@@ -61,7 +101,8 @@ app.post("/login", async (req: Request, res: Response):Promise<void> => {
         if (results.length === 0) {
             res.status(404).json({ Reponse: "Email n'existe pas dans la DB" });
             return;
-        } else {
+        }
+        else {
             if (results[0].password === req.body.password) {
                 res.status(200).json({ response: "Il existe !!", data: results, envois: req.body });
                 return;
