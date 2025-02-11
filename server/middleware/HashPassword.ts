6c98afe5f9be960bee82e1fa62bb4b579e8fe8cb
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-11 16:19:36 +0100

    Initialisation de la fonction

--- server/middleware/HashPassword.ts
+++ server/middleware/HashPassword.ts
@@ -1,1 +1,12 @@
 import * as argon2 from "argon2";
+import { error } from "console";
+import { Request, Response, NextFunction } from "express";
+
+async function HahPassword( req: Request, res: Response, next: NextFunction) {
+    try {}
+    catch (error) {
+        console.error ("Erreur lors de la requête SQL :", error);
+        res.status(500).json({ error: "Erreur lors de l'accès à la base de données." });
+        return;
+    }
+}
