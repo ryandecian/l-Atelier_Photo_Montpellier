acae998e89d8912b8c9da12b3f8af7a5e3f530e5
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-12 02:14:18 +0100

    Initialisation du middleware VerifyEmail.ts

---
+++ server/src/middleware/VerifyEmail.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from "express";
+
+function VerifyEmail(res: Response, req:Request, next: NextFunction) {
+    try {}
+    catch (error) {}
+}
+
+export default VerifyEmail;
