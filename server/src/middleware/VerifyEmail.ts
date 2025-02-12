a8564affedde8128a019e62721c2e94987184182
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-12 02:37:21 +0100

    Amélioration de la réponse du console.error de VerifyKeys pour mieux débuge

--- server/src/middleware/VerifyEmail.ts
+++ server/src/middleware/VerifyEmail.ts
@@ -1,7 +1,25 @@
-import { Request, Response, NextFunction } from "express";
+import { query, Request, Response, NextFunction } from "express";
+import usePoolConnection from "../database/config";
+import { RowDataPacket } from "mysql2";
 
-function VerifyEmail(res: Response, req:Request, next: NextFunction) {
-    try {}
+// Objectif, vérifier que l'email reçu pour l'enregistrement n'est pas utilisé.
+
+async function VerifyEmail(res: Response, req:Request, next: NextFunction) {
+    try {
+        // ✅ Vérification : l'email reçu existe t-il dans la DB ?
+        const [dataUser] = await usePoolConnection.query<RowDataPacket[]>(
+            "SELECT * FROM user WHERE email= ?",
+            [req.body.email]
+        )
+
+        // ✅ Si l'email existe sa dégage, on arrête l'exécution
+        if (dataUser.length > 0) {
+            res.status(409).json({ reponse: "Cet email est déjà utilisé. Veuillez en choisir un autre.", server: dataUser });
+            console.error({})
+            return;
+        }
+        next()
+    }
     catch (error) {}
 }
 
--- server/src/middleware/VerifyKeys.ts
+++ server/src/middleware/VerifyKeys.ts
@@ -12,12 +12,17 @@ function VerifyKeys(requiredKeys: string[]) {
             const missingKeys = requiredKeys.filter(key => !req.body[key]);
 
             if (missingKeys.length > 0) {
-                console.error("❌ Erreur : Clés manquantes dans la requête", {
-                    missingKeys, 
-                    body: req.body, 
-                    middleware: "VerifyKeys.ts",
-                    chemin: "/server/src/middleware/VerifyKeys.ts"})
                 res.status(400).json({ reponse: "La syntaxe de la requête est erronée." })
+                console.error(
+                    {
+                        identity: "VerifyKeys.ts",
+                        type: "middleware",
+                        chemin: "/server/src/middleware/VerifyKeys.ts",
+                        "❌ Nature de l'erreur": "Clés manquantes dans la requête",
+                        missingKeys,
+                        body: req.body
+                    },
+                )
                 return;
             }
 
