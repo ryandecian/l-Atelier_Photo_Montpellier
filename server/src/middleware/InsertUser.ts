b6c255f9013472d150e3c6b3c869d0d7ebbd0ae9
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-12 10:18:06 +0100

    Push partiel de la logique du middleware

--- server/src/middleware/InsertUser.ts
+++ server/src/middleware/InsertUser.ts
@@ -1,6 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 import usePoolConnection from "../database/config";
+import { ResultSetHeader } from "mysql2";
 
-async function InsertUser() {}
+async function InsertUser(req: Request, res: Response, next: NextFunction) {
+    const [results] = await usePoolConnection.query<ResultSetHeader>(
+        "INSERT INTO user (firstname, lastname, address, email, password) VALUES (?, ?, ?, ?, ?)",
+        [req.body.firstname, req.body.lastname, req.body.address, req.body.email, req.body.password],
+    );
+
+    if (results.affectedRows === 0) {};
+};
 
 export default InsertUser;
