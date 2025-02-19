8fe3d071446c37ffc9923ee4800ae41e6f259eca
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-19 15:41:31 +0100

    Récupération des infos du token

--- server/src/middleware/Verify_Crypto_Middleware.ts
+++ server/src/middleware/Verify_Crypto_Middleware.ts
@@ -1,9 +1,31 @@
 import { Request, Response, NextFunction } from "express";
 import crypto from 'crypto';
+import usePoolConnection from "../database/config";
 
 
 async function Verify_Crypto_Middleware(req: Request, res: Response, next: NextFunction) {
-    try {}
+    try {
+        // Vérification supplémentaire, les clé token et newPassword sont-elles présentes ?
+        if (!req.body.token || !req.body.newPassword) {
+            res.status(400).json({ error: "Requête invalide." });
+            console.error(
+                {
+                    identity: "Verify_Crypto_Middleware.ts",
+                    type: "middleware",
+                    chemin: "/server/src/middleware/Verify_Crypto_Middleware.ts",
+                    "❌ Nature de l'erreur": "Requête invalide.",
+                    cause1: "Les champs token et ou newPassword sont absents.",
+                });
+            return;
+        };
+
+        const token = req.body.token;
+
+        // Vérification si le token est valide
+        const [dataToken] = await usePoolConnection.query(
+            "SELECT user_id, expires_at FROM reset_password WHERE token = ?", req.body.token
+        );
+    }
     catch (error) {}
 };
 
