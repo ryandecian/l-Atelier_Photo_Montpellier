59f45251f88572ab0386ef871fdd4a18d8b3d74e
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-26 15:15:00 +0100

    Création du bouton découvrir

--- client/src/components/HomeRoot/HomePC/HomePC.tsx
+++ client/src/components/HomeRoot/HomePC/HomePC.tsx
@@ -1,4 +1,5 @@
 import style from './HomePC.module.css';
+import { Link } from 'react-router-dom';
 
 function HomePC() {
     return (
@@ -8,8 +9,15 @@ function HomePC() {
                 Photographe professionnelle à Montpellier authentiques.
             </h1>
 
-            <div className={style.ImgAccueil}>
+            <div className={style.ContainerAccueil}>
                 <p>Capturez l'émotion du regard</p>
+                <p>
+                    <button type="button" className={style.ButtonDecouvrir}>
+                        <Link to="#" >
+                            Découvrir mon univers !
+                        </Link>
+                    </button>
+                </p>
             </div>
         </div>
     );
