7626f1962f17cd5ed97ef68359357a45fb77ebe7
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-26 15:39:27 +0100

    Fin du style de l'img de la page HomePC[D[D[D[D[D[D

--- client/src/components/HomeRoot/HomePC/HomePC.tsx
+++ client/src/components/HomeRoot/HomePC/HomePC.tsx
@@ -5,20 +5,25 @@ function HomePC() {
     return (
         <div className={style.HomePC}>
 
-            <h1 className={style.PrimaryTitle}>
-                Photographe professionnelle à Montpellier authentiques.
-            </h1>
+            <div className={style.ContainerTitle}>
+                <h1 className={style.PrimaryTitle}>
+                    Photographe professionnelle à Montpellier authentiques
+                </h1>
+            </div>
+            
 
             <div className={style.ContainerAccueil}>
-                <p>Capturez l'émotion du regard</p>
-                <p>
-                    <button type="button" className={style.ButtonDecouvrir}>
+                <p className={style.Slogan}>Capturez l'émotion du regard</p>
+                <p className={style.ButtonDecouvrir}>
+                    <button type="button">
                         <Link to="#" >
                             Découvrir mon univers !
                         </Link>
                     </button>
                 </p>
             </div>
+
+            <div></div>
         </div>
     );
 }
