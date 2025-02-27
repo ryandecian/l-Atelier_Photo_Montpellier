683127d4e89da732930eb49ae523ac76bf037cf0
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-27 16:09:15 +0100

    Déplacement de toute la logique de Expertise dans un composant Root

---
+++ client/src/components/HomeRoot/HomePC/ContainerExpertiseHomeRoot.tsx
@@ -0,0 +1,43 @@
+import style from "./ContainerExpertiseHomeRoot.module.css"
+
+function ContainerExpertiseHomeRoot() {
+    return (
+        <section className={style.ContainerExpertise}>
+                <header className={style.ExpertiseTitle}>
+                    <h2>
+                        MON EXPERTISE :
+                    </h2>
+                </header>
+                <article className={style.ExpertiseText1}>
+                    <p>
+                        En tant que photographe professionnelle installée à Montpellier et spécialisée en 
+                        portraits – qu’ils soient corporate, lifestyle, réalisés en lumière naturelle ou 
+                        au flash – je mets tout en œuvre pour saisir la personnalité unique de chacun. La 
+                        photographie de portrait professionnel n’est pas seulement une technique, c’est une 
+                        véritable passion que je développe afin que chacun et chacune puisse porter un 
+                        regard sur lui (elle) positif et valorisant. 
+                    </p>
+                </article>
+                <article className={style.ExpertiseText2}>
+                    <p>
+                        Ma vision du mariage se décline sous la forme d’un reportage photographique. Plutôt 
+                        que de créer de simples poses figées, je privilégie l’instantanéité et la spontanéité 
+                        des émotions vécues durant la cérémonie. Je cherche à être au plus près de tous ces 
+                        instants afin qu’ils restent gravés, et cette approche me permet de raconter une histoire 
+                        visuelle complète, où chaque image capte la complicité, la tendresse et l’authenticité 
+                        du grand jour.
+                    </p>
+                </article>
+                <article className={style.ExpertiseText3}>
+                    <p>
+                        En choisissant mes services, vous optez pour un accompagnement sur mesure, alliant expertise 
+                        technique, sensibilité artistique et un engagement passionné pour immortaliser vos moments 
+                        les plus précieux. Vos portraits seront l’image à d’un reportage de mariage authentique et 
+                        vibrant de vie ! 
+                    </p>
+                </article>
+            </section>
+    );
+}
+
+export default ContainerExpertiseHomeRoot;
