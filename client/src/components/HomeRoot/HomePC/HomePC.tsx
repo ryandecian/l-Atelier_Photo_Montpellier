306cdc9fd426eed1bea4c72e95672c64a6a9d68f
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-27 16:45:52 +0100

    Création du nouveau container pour le container Accueil

--- client/src/components/HomeRoot/HomePC/HomePC.tsx
+++ client/src/components/HomeRoot/HomePC/HomePC.tsx
@@ -1,39 +1,23 @@
 import style from './HomePC.module.css';
-import { Link } from 'react-router-dom';
 
 /* Import des composants */
 import ContainerIntroductionHomeRoot from '../ComponentHomeRoot/ContainerIntroductionHomeRoot/ContainerIntroductionHomeRoot';
 import ContainerExpertiseHomeRoot from '../ComponentHomeRoot/ContainerExpertiseHomeRoot/ContainerExpertiseHomeRoot';
+import ContainerAccueilHomeRoot from '../ComponentHomeRoot/ContainerTitleHomeRoot/ContainerAccueilHomeRoot';
 
 function HomePC() {
     return (
         <div className={style.HomePC}>
+            {/* Container 1 : Accueil */}
+            <ContainerAccueilHomeRoot />
 
-            <div className={style.ContainerTitle}>
-                <h1 className={style.PrimaryTitle}>
-                    Photographe professionnelle à Montpellier authentiques
-                </h1>
-            </div>
-            
-
-            <div className={style.ContainerAccueil}>
-                <p className={style.Slogan}>Capturez l'émotion du regard</p>
-                <p className={style.ButtonDecouvrir}>
-                    <button type="button">
-                        <Link to="#" >
-                            Découvrir mon univers !
-                        </Link>
-                    </button>
-                </p>
-            </div>
-
-            {/* Container 1 : Introduction */}
+            {/* Container 2 : Introduction */}
             <ContainerIntroductionHomeRoot />
 
-            {/* Container 2 : Mon expertise */}
+            {/* Container 3 : Mon expertise */}
             <ContainerExpertiseHomeRoot />
 
-            {/* Container 3 : Présentation des services */}
+            {/* Container 4 : Présentation des services */}
             <div className={style.ContainerServices}></div>
         </div>
     );
