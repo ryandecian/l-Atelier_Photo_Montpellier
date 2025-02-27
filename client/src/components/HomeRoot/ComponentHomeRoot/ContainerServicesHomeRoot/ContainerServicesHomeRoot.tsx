2f769c585fc3abae24fb9573fb62554a218c32e7
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-27 16:58:44 +0100

    Création du Composant ContainerServices

---
+++ client/src/components/HomeRoot/ComponentHomeRoot/ContainerServicesHomeRoot/ContainerServicesHomeRoot.tsx
@@ -0,0 +1,15 @@
+import style from "./ContainerServicesHomeRoot.module.css";
+
+function ContainerServicesHomeRoot() {
+    return (
+        <section className={style.ContainerServices}>
+            <header>
+                <h2>
+                    PRESENTATION DES SERVICES
+                </h2>
+            </header>
+        </section>
+    );
+}
+
+export default ContainerServicesHomeRoot;
