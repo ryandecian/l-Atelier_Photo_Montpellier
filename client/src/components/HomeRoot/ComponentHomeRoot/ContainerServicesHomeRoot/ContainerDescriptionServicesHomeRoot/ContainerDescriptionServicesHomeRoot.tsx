3222762f3aabfd22337852cd640e351ab09bcc6d
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-01 22:29:00 +0100

    Création d'un sous composant pour la descriptions des services

---
+++ client/src/components/HomeRoot/ComponentHomeRoot/ContainerServicesHomeRoot/ContainerDescriptionServicesHomeRoot/ContainerDescriptionServicesHomeRoot.tsx
@@ -0,0 +1,21 @@
+import style from "./ContainerDescriptionServicesHomeRoot.module.css";
+
+function ContainerDescriptionServicesHomeRoot() {
+    return (
+        <>
+            <section className={style.ContainerDescriptionServices1}>
+                <header className={style.ServicesTitle}>
+                    <h3>
+                        Service 1 : Le portrait dans tous ses états
+                    </h3>
+                </header>
+
+                <article>
+                    <p></p>
+                </article>
+            </section>
+        </>
+    );
+}
+
+export default ContainerDescriptionServicesHomeRoot;
