f7ff3098220000188086e987bd5de9fca48e034f
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-02 17:10:11 +0100

    Push partiel de la structiure du css du footerpc

--- client/src/components/FooterRoot/FooterPC/FooterPC.tsx
+++ client/src/components/FooterRoot/FooterPC/FooterPC.tsx
@@ -1,23 +1,35 @@
 import style from "./FooterPC.module.css";
+import { Link } from "react-router-dom";
 
 function FooterPC() {
     return (
         <section className={style.FooterPC}>
+
+            {/* Container 1 : Title du Footer */}
             <header className={style.TitleFooter}>
                 <h2>
                     l'Atelier Photo Montpellier
                 </h2>
             </header>
 
-            <section>
-                <header>
-                    <h3></h3>
-                </header>
+            {/* Container 2 : Navigation du Footer */}
+            <div className={style.ContainerNavFooter}>
+                <section className={style.SubContainerNavFooter}>
+                    <header className={style.TitleNavFooter}>
+                        <h3>
+                            A propos
+                        </h3>
+                    </header>
 
-                <article>
-                    <p></p>
-                </article>
-            </section>
+                    <article className={style.ContainerTextNavFooter}>
+                        <p className={style.TextLink}>
+                            <Link to="#">
+                                Qui suis-je ?
+                            </Link>
+                        </p>
+                    </article>
+                </section>
+            </div>
         </section>
     );
 }
