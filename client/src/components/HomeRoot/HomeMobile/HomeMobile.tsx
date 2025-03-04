05e9d7750c87a5ebcf3a4b0d75cf3a648cc78ec9
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-04 08:06:00 +0100

    Création du composant HomeMobile

---
+++ client/src/components/HomeRoot/HomeMobile/HomeMobile.tsx
@@ -0,0 +1,13 @@
+import style from './HomeMobile.module.css';
+
+/* Import des composants */
+import ContainerAccueilHomeRoot from '../ComponentHomeRoot/ContainerTitleHomeRoot/ContainerAccueilHomeRoot';
+import ContainerIntroductionHomeRoot from '../ComponentHomeRoot/ContainerIntroductionHomeRoot/ContainerIntroductionHomeRoot';
+import ContainerExpertiseHomeRoot from '../ComponentHomeRoot/ContainerExpertiseHomeRoot/ContainerExpertiseHomeRoot';
+import ContainerServicesHomeRoot from '../ComponentHomeRoot/ContainerServicesHomeRoot/ContainerServicesHomeRoot';
+
+function HomeMobile() {
+    return (  );
+}
+
+export default HomeMobile;
