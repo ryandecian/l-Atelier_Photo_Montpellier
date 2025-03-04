144f4e2169a88e661d8b45243bdf66b8519ce894
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-04 17:53:00 +0100

    Push partiel du composant MentionsLegalesSectionA.tsx

--- client/src/components/MentionsLegalesRoot/MentionsLegalesPC/MentionsLegalesPC.tsx
+++ client/src/components/MentionsLegalesRoot/MentionsLegalesPC/MentionsLegalesPC.tsx
@@ -3,6 +3,7 @@ import style from "./MentionsLegalesPC.module.css";
 /* Import des composants */
 import Separator from "../ComponentsMentionsLegalesRoot/Separator/Separator";
 import TitleMentionsLegalesRoot from "../ComponentsMentionsLegalesRoot/TitleMentionsLegalesRoot/TitleMentionsLegalesRoot";
+import MentionsLegaleSectionA from "../ComponentsMentionsLegalesRoot/MentionsLegalesSectionA/MentionsLegaleSectionA";
 
 function MentionsLegalesPC() {
     return (
@@ -10,6 +11,9 @@ function MentionsLegalesPC() {
             <TitleMentionsLegalesRoot />
 
             <Separator />
+
+            <MentionsLegaleSectionA />
+
 
             <section>
                 <header>
