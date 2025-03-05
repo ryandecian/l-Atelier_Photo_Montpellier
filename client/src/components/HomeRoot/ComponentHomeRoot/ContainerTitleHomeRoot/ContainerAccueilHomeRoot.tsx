3035e644e8e01cd55e00718f8bd10b66cb9d7118
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-05 07:32:26 +0100

    Ajout des fichiers de la nouvelle police d'écriture

--- client/src/components/HomeRoot/ComponentHomeRoot/ContainerTitleHomeRoot/ContainerAccueilHomeRoot.tsx
+++ client/src/components/HomeRoot/ComponentHomeRoot/ContainerTitleHomeRoot/ContainerAccueilHomeRoot.tsx
@@ -5,23 +5,23 @@ function ContainerAccueilHomeRoot() {
         <section className={style.ContainerAccueil}>            
 
             <article className={style.ContainerSubAccueil}>
-                <div className={style.test}>
-                <p className={style.Slogan}>
-                    {"DES PORTRAITS"}
-                </p>
-                <p className={style.Slogan}>
-                    {"QUI"}
-                </p>
-                <p className={style.Slogan}>
-                    {"PARLENT DE"}
-                </p>
-                <p className={style.Slogan}>
-                    {"VOUS"}
-                </p>
-                </div>
-                <div className={style.testB}>
-                    <p>PHOTOGRAPHE DE PORTRAITS ET DE MARIAGE A MONTPELLIER</p>
-                </div>
+                <article className={style.ContainerSlogan}>
+                    <p className={style.Slogan}>
+                        {"DES PORTRAITS"}
+                    </p>
+                    <p className={style.Slogan}>
+                        {"QUI"}
+                    </p>
+                    <p className={style.Slogan}>
+                        {"PARLENT DE"}
+                    </p>
+                    <p className={style.Slogan}>
+                        {"VOUS"}
+                    </p>
+                </article>
+                <header className={style.ContainerTitle}>
+                    <h1>PHOTOGRAPHE DE PORTRAITS ET DE MARIAGE A MONTPELLIER</h1>
+                </header>
             </article>
         </section>
     );
