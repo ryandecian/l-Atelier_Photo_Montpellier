17bbbfe7e8e21b6ddf20934c3dd9c2f039958deb
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-07 20:45:46 +0100

    Intégration du composant D dans les logique PC et Mobile

--- client/src/components/HomeRoot/ComponentHomeRoot/HomeRootComposantD/HomeRootComposantD.tsx
+++ client/src/components/HomeRoot/ComponentHomeRoot/HomeRootComposantD/HomeRootComposantD.tsx
@@ -2,7 +2,13 @@ import style from "./HomeRootComposantD.module.css"
 
 function HomeRootComposantD() {
     return (
-        <section className={style.HomeRootComposantD}></section>
+        <section className={style.HomeRootComposantD}>
+            <header className={style.ContainerTitle}>
+                <h2 className={style.Title}>
+                    Des photos, des expériences, des mots, ils m'ont fait confiance !
+                </h2>
+            </header>
+        </section>
     );
 }
 
