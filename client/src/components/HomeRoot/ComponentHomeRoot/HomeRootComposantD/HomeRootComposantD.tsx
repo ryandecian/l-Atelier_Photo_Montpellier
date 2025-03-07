50a7855e0d10db734ea29ec0327520590805ffae
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-07 20:42:09 +0100

    Création partiel du composant D dans la section Home

---
+++ client/src/components/HomeRoot/ComponentHomeRoot/HomeRootComposantD/HomeRootComposantD.tsx
@@ -0,0 +1,9 @@
+import style from "./HomeRootComposantD.module.css"
+
+function HomeRootComposantD() {
+    return (
+        <section className={style.HomeRootComposantD}></section>
+    );
+}
+
+export default HomeRootComposantD;
