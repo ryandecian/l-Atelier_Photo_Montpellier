924165232a59be87dc0f1f5100c1849a0d81a4cc
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-07 14:04:22 +0100

    Restructuration des dossiers côté composant HomeRoot

--- client/src/components/HomeRoot/ComponentHomeRoot/HomeRootComposantC/HomeRootComposantC.tsx
+++ client/src/components/HomeRoot/ComponentHomeRoot/HomeRootComposantC/HomeRootComposantC.tsx
@@ -1,5 +1,5 @@
 import style from "./HomeRootComposantC.module.css"
-import ContainerImgHomeRoot from "../ComposantHomeRoot/ContainerImgHomeRoot";
+import ContainerImgHomeRoot from "../ComposantHomeRoot/ContainerImgHomeRoot/ContainerImgHomeRoot";
 import { Link } from "react-router-dom";
 import ListDataRouter from "../../../../router/router";
 
