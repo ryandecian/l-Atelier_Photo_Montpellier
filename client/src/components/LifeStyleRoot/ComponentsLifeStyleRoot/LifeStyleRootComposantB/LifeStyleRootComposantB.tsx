0320c7ef2ae57fd5e96e058ea783556bd9c9983d
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-09 02:40:41 +0100

    Push partiel de la logique du composant B

--- client/src/components/LifeStyleRoot/ComponentsLifeStyleRoot/LifeStyleRootComposantB/LifeStyleRootComposantB.tsx
+++ client/src/components/LifeStyleRoot/ComponentsLifeStyleRoot/LifeStyleRootComposantB/LifeStyleRootComposantB.tsx
@@ -8,12 +8,47 @@ import img5 from "../../../../assets/Images/ImageHomePage/HomeImg5.jpg";
 import img6 from "../../../../assets/Images/ImageHomePage/HomeImg6.jpg";
 
 const services = [
-    { id: 1, title: "Passion Live", name: "Portrait Hobbies", path: "/portrait-hobbies", img: img1 },
-    { id: 2, title: "Moments Précieux", name: "Séance Famille", path: "/seance-famille", img: img2 },
-    { id: 3, title: "Style Unique", name: "Mode & Lifestyle", path: "/mode-lifestyle", img: img3 },
-    { id: 4, title: "Émotion Authentique", name: "Mariage & Engagement", path: "/mariage-engagement", img: img4 },
-    { id: 5, title: "Instants Magiques", name: "Grossesse & Bébé", path: "/grossesse-bebe", img: img5 },
-    { id: 6, title: "Corporate & Pro", name: "Portrait Professionnel", path: "/portrait-pro", img: img6 },
+    { 
+        id: 1, 
+        title: "Passion Live", 
+        name: "Portrait Hobbies", 
+        path: "/portrait-hobbies", 
+        img: img1 
+    },
+    { 
+        id: 2, 
+        title: "Moments Précieux", 
+        name: "Séance Famille", 
+        path: "/seance-famille", 
+        img: img2 
+    },
+    { 
+        id: 3, 
+        title: "Style Unique", 
+        name: "Mode & Lifestyle", 
+        path: "/mode-lifestyle", 
+        img: img3 
+    },
+    { 
+        id: 4, 
+        title: "Émotion Authentique", 
+        name: "Mariage & Engagement", 
+        path: "/mariage-engagement", 
+        img: img4 
+    },
+    { 
+        id: 5, title: "Instants Magiques", 
+        name: "Grossesse & Bébé", 
+        path: "/grossesse-bebe", 
+        img: img5 
+    },
+    { 
+        id: 6, 
+        title: "Corporate & Pro", 
+        name: "Portrait Professionnel", 
+        path: "/portrait-pro", 
+        img: img6 
+    },
 ];
 
 function LifeStyleRootComposantB() {
