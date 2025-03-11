35d277c4799189b6940f8cc2e18b4fc2f6b34d16
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-11 15:54:06 +0100

    Fin de la création du et de l'intégration du composant C

--- client/src/components/CGVRoot/ComponentsCGVRoot/CGVComposantC/CGVComposantC.tsx
+++ client/src/components/CGVRoot/ComponentsCGVRoot/CGVComposantC/CGVComposantC.tsx
@@ -21,10 +21,12 @@ function CGVComposantC() {
                     Sauf mention contraire, les images livrées sont destinées à une utilisation commerciale limitée 
                     (site web, réseaux sociaux, supports de communication internes).
                 </p>
+                <div className={style.Separator20}/>
 
                 <h4 className={style.TitleH4}>
                     8.1. Objet et portée des droits cédés
                 </h4>
+                <div className={style.Separator10}/>
 
                 <p className={style.Text}>
                     Le photographe, L’Atelier Photo Montpellier, cède au client une licence d’exploitation des photographies 
@@ -32,36 +34,47 @@ function CGVComposantC() {
                     n’entraîne en aucun cas un transfert de propriété intellectuelle, le photographe restant titulaire 
                     des droits d’auteur sur les œuvres (articles L.121-1 et suivants du Code de la propriété intellectuelle).
                 </p>
+                <div className={style.Separator20}/>
 
                 <h4 className={style.TitleH4}>
                     8.2. Durée de la cession
                 </h4>
+                <div className={style.Separator10}/>
 
                 <p className={style.Text}>
                     Les droits d’exploitation des images sont accordés pour une durée de 3 (trois) ans, à compter de la date 
                     de livraison des fichiers numériques. Toute utilisation au-delà de cette période devra faire l’objet 
                     d’un renouvellement contractuel et financier.  
                 </p>
+                <div className={style.Separator20}/>
 
                 <h4 className={style.TitleH4}>
                     8.3. Périmètre géographique
                 </h4>
+                <div className={style.Separator10}/>
 
                 <p className={style.Text}>
                     La présente cession de droits est accordée pour un usage national comme international, sans restriction 
                     de pays.
                 </p>
+                <div className={style.Separator20}/>
 
                 <h4 className={style.TitleH4}>
                     8.4. Supports autorisés et conditions d’exploitation
                 </h4>
+                <div className={style.Separator10}/>
+
                 <p className={style.Text}>
                     Le client est autorisé à utiliser les images sur les supports publicitaires suivants, dans la 
                     limite des volumes définis au devis :  
                 </p>
+                <div className={style.Separator20}/>
+
                 <p className={style.Text}>
                     Type de support : Supports imprimés  
                 </p>
+                <div className={style.Separator10}/>
+
                 <ul className={style.ContainerList}>
                     <li className={style.ElementLi}>
                         Exemples d’utilisation : Affiches, brochures, flyers, panneaux publicitaires, catalogues
@@ -70,9 +83,13 @@ function CGVComposantC() {
                         Plafond d’impression / diffusion : Nombre d’exemplaires défini au devis
                     </li>
                 </ul>
+                <div className={style.Separator20}/>
+
                 <p className={style.Text}>
                     Type de support : Supports digitaux
                 </p>
+                <div className={style.Separator10}/>
+
                 <ul className={style.ContainerList}>
                     <li className={style.ElementLi}>
                         Exemples d’utilisation : Sites web, campagnes display, réseaux sociaux, emailings
@@ -81,9 +98,13 @@ function CGVComposantC() {
                         Plafond d’impression / diffusion : Diffusion illimitée pendant 3 ans  
                     </li>
                 </ul>
+                <div className={style.Separator20}/>
+
                 <p className={style.Text}>
                     Type de support : Publicité audiovisuelle 
                 </p>
+                <div className={style.Separator10}/>
+
                 <ul className={style.ContainerList}>
                     <li className={style.ElementLi}>
                         Exemples d’utilisation : Spots TV, publicités web, panneaux LED, vidéo marketing
@@ -92,9 +113,13 @@ function CGVComposantC() {
                         Plafond d’impression / diffusion : Durée et zones de diffusion définies dans le devis
                     </li>
                 </ul>
+                <div className={style.Separator20}/>
+
                 <p className={style.Text}>
                     Type de support : Supports presse
                 </p>
+                <div className={style.Separator10}/>
+
                 <ul className={style.ContainerList}>
                     <li className={style.ElementLi}>
                         Exemples d’utilisation : Magazines, journaux, articles sponsorisés
@@ -103,26 +128,35 @@ function CGVComposantC() {
                         Plafond d’impression / diffusion : Nombre d’éditions défini dans le devis
                     </li>
                 </ul>
+                <div className={style.Separator20}/>
 
                 <h4 className={style.TitleH4}>
                     8.5. Conditions spécifiques et limitations
                 </h4>
+                <div className={style.Separator10}/>
+
                 <p className={style.Text}>
                     Interdiction de revente : Les images ne peuvent en aucun cas être revendues, transférées, cédées ou 
                     sous-licenciées à des tiers sans l’accord écrit du photographe.
                 </p>
+                <div className={style.Separator20}/>
+
                 <p className={style.Text}>
                     Le tarif applicable est précisé dans le devis. Toute extension de droits fera l’objet d’une 
                     facturation complémentaire.
                 </p>
+                <div className={style.Separator20}/>
 
                 <h4 className={style.TitleH4}>
                     8.6. Sanctions en cas de non-respect
                 </h4>
+                <div className={style.Separator10}/>
                 <p className={style.Text}>
                     En cas d’utilisation non conforme aux présentes conditions (dépassement de durée, supports non 
                     autorisés, diffusion supérieure au plafond défini, revente illégale, etc.), le client s’expose à :
                 </p>
+                <div className={style.Separator10}/>
+
                 <ul className={style.ContainerList}>
                     <li className={style.ElementLi}>
                         Une facturation rétroactive des droits non réglés.
@@ -134,9 +168,12 @@ function CGVComposantC() {
                         Des poursuites judiciaires pour contrefaçon (article L.335-2 du Code de la propriété intellectuelle).
                     </li>
                 </ul>
+                <div className={style.Separator20}/>
+
                 <h4 className={style.TitleH4}>
                     8.7. Fin de la cession des droits
                 </h4>
+                <div className={style.Separator10}/>
                 <p className={style.Text}>
                     À l’expiration de la période de 3 ans, le client devra cesser toute diffusion des images sur les 
                     supports concernés, sauf renouvellement des droits via un nouvel accord contractuel.
@@ -150,6 +187,8 @@ function CGVComposantC() {
                 <h3 className={style.TitleH3}>
                     Article 9 – Responsabilité du photographe
                 </h3>
+                <div className={style.Separator10}/>
+
                 <ul className={style.ContainerList}>
                     <li className={style.ElementLi}>
                         Le photographe met tout en œuvre pour assurer la meilleure qualité de service, mais ne peut être 
@@ -170,6 +209,7 @@ function CGVComposantC() {
                 <h3 className={style.TitleH3}>
                     Article 10 – Propriété intellectuelle et confidentialité 
                 </h3>
+                <div className={style.Separator10}/>
                 <ul className={style.ContainerList}>
                     <li className={style.ElementLi}>
                         Le photographe reste propriétaire des fichiers bruts (RAW), qui ne sont jamais livrés au client.
@@ -188,6 +228,8 @@ function CGVComposantC() {
                 <h3 className={style.TitleH3}>
                     Article 11 – Loi applicable et règlement des litiges
                 </h3>
+                <div className={style.Separator10}/>
+
                 <ul className={style.ContainerList}>
                     <li className={style.ElementLi}>
                         Les présentes CGV sont soumises à la législation française.
@@ -200,6 +242,7 @@ function CGVComposantC() {
                     </li>
                 </ul>
             </article>
+            <div className={style.Separator30}/>
 
         </section>
     );
