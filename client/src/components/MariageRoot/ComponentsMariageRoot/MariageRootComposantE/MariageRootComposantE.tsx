068b1b52a396ffe1bc30aaedd73a6ea6f4a34f39
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-12 00:23:02 +0100

    Fin de la création du composant E

--- client/src/components/MariageRoot/ComponentsMariageRoot/MariageRootComposantE/MariageRootComposantE.tsx
+++ client/src/components/MariageRoot/ComponentsMariageRoot/MariageRootComposantE/MariageRootComposantE.tsx
@@ -8,45 +8,36 @@ function MariageRootComposantE() {
             </h2>
 
             <p className={style.Text}>
-                Votre mariage est un {" "}
+                Le mariage est souvent une journée pleine de {" "}
                 <span className={style.SpanBold}>
-                    {"réflexe de votre personnalité"}
+                    {"stress"}
                 </span>
-                {" "} et de vos goûts. Vous souhaitez des photos qui traduisent l’esprit de votre union, mais aussi de 
-                vos valeurs et de vos envies esthétiques. C’est pourquoi je m’engage à respecter votre {" "}
+                {" "} et d’émotions. C’est pourquoi, pendant toute la séance photo, je vous aide à rester {" "}
                 <span className={style.SpanBold}>
-                    {"vision de l’image,"}
+                    {"détendus"}
                 </span>
-                {" "} qu’elle soit simple et épurée, 
+                {" "} et à vous sentir {" "}
                 <span className={style.SpanBold}>
-                    {"sophistiquée et raffinée,"}
+                    {"à l’aise"}
                 </span>
-                {" "} ou {" "}
-                <span className={style.SpanBold}>
-                    {"très moderne et dynamique,"}
-                </span>
-                {" "} inspirée des dernières {" "}
-                <span className={style.SpanBold}>
-                    {"tendances Instagram."}
-                </span>
+                {" "} devant l’objectif. Que vous soyez nerveux à l’idée d’être sous les projecteurs ou que vous 
+                ne sachiez pas comment vous comporter devant un appareil photo, je serai là pour vous guider avec 
+                des conseils simples sur la posture, l’expression et les moments à capturer.
             </p>
             <p className={style.Text}>
-                Nous discuterons ensemble de vos envies concernant le style de photos que vous souhaitez. 
-                    Voulez-vous un {" "}
+                L'idée n'est pas de vous imposer des poses mais plutôt de vous aider à {" "}
                 <span className={style.SpanBold}>
-                    {"style simple et naturel,"}
-                </span> 
-                {" "} proche du documentaire ? Ou un {" "}
+                    {"être vous-même."}
+                </span>
+                {" "} Une bonne photo de mariage réside souvent dans la {" "}
                 <span className={style.SpanBold}>
-                    {"style plus sophistiqué"}
-                </span> 
-                {" "} et travaillé, avec des photos plus posées et scénarisées ? Peut-être êtes-vous un couple 
-                influencé par les {" "}
+                    {"sincérité du moment"}
+                </span>
+                {" "} et dans la {" "}
                 <span className={style.SpanBold}>
-                    {"tendances Instagram"}
-                </span> 
-                {" "} et préférez-vous des images plus modernes et stylisées ? Quelle que soit votre vision, 
-                je serai à l’écoute de vos attentes pour réaliser des photos qui vous ressemblent.
+                    {"simplicité des gestes,"}
+                </span>
+                {" "} et je serai là pour m’assurer que tout se déroule dans la plus grande fluidité.
             </p>          
         </article>
     );
