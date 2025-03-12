f4a0164e60197bf07378f609293689f00a11518b
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-12 12:06:22 +0100

    Déplacement des datas images du premier composant dans le controller LifeSTyle

---
+++ client/src/components/LifeStyleRoot/ControllerDataImageLifeStyle.tsx
@@ -0,0 +1,145 @@
+import Image1 from "../../assets/Images/ImageLifeStyle/Femme-a_la_cigarette.jpg";
+import Image2 from "../../assets/Images/ImageLifeStyle/portrait_duo_exterieur_.jpg";
+import Image3 from "../../assets/Images/ImageLifeStyle/Portraits_Famille_Flash.jpg";
+import Image4 from "../../assets/Images/ImageLifeStyle/Premier_jour_portrait.jpg";
+import Image5 from "../../assets/Images/ImageLifeStyle/portrait_9mois_domicile.jpg";
+import Image6 from "../../assets/Images/ImageLifeStyle/Portrait_Serie_PeakyBlinders.jpg";
+
+
+
+interface ControllerDataImageLifeStyleType {
+    src: string;
+    alt: string;
+}
+
+const ControllerDataImageLifeStyle: ControllerDataImageLifeStyleType[] = [
+    /* Menu en image LifeStyle : 6 images */
+    /* {ControllerDataImageLifeStyle[0].src} */
+    {
+        src: Image1,
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[1].src} */
+    {
+        src: Image2,
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[2].src} */
+    {
+        src: Image3,
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[3].src} */
+    {
+        src: Image4,
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[4].src} */
+    {
+        src: Image5,
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[5].src} */
+    {
+        src: Image6,
+        alt: "",
+    },
+
+
+
+    /* Tableau 1 d'images : 3 ou 4 images */
+    /* {ControllerDataImageLifeStyle[6].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[7].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[8].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[9].src} */
+    {
+        src: "",
+        alt: "",
+    },
+
+
+
+    /* Tableau 2 d'images : 3 ou 4 images */
+    /* {ControllerDataImageLifeStyle[10].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[11].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[12].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[13].src} */
+    {
+        src: "",
+        alt: "",
+    },
+
+
+
+    /* {ControllerDataImageLifeStyle[14].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[15].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[16].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[17].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[18].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[19].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[20].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[21].src} */
+    {
+        src: "",
+        alt: "",
+    },
+    /* {ControllerDataImageLifeStyle[22].src} */
+    {
+        src: "",
+        alt: "",
+    },
+];
+
+export default ControllerDataImageLifeStyle;
