26d9ab1f1b592a7c11e2bfcd4a1019bc5e3c99b2
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-13 14:03:02 +0100

    Création de la route login dans le sub router

--- server/src/router/router.ts
+++ server/src/router/router.ts
@@ -1,5 +1,11 @@
 import express from "express";
 
+// Import des sub route indépendante !
+import loginRouter from "./loginRouter/loginRouter";
+import { log } from "console";
+
 const router = express.Router();
+
+router.use("/login", loginRouter);
 
 export default router;
