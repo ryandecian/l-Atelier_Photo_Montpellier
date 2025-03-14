3becc4fdc7e4f28e57d9dfa35d10f76b5c4ed048
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-14 09:06:26 +0100

    Fin de la création de la route email qui est testé et fonctionnel

--- server/src/router/emailRouter/emailRouter.ts
+++ server/src/router/emailRouter/emailRouter.ts
@@ -1,5 +1,16 @@
 import express from "express";
 
+// Import des Middlewares :
+import VerifyKeys from "../../middleware/VerifyKeys/VerifyKeys";
+import SendMailer_Middleware from "../../services/mailer/SendMailer_Middleware";
+import emailResponse from "./emailResponse";
+
 const emailRouter = express.Router();
+
+emailRouter.post("/",
+    VerifyKeys(["to", "subject", "text"]),
+    SendMailer_Middleware,
+    emailResponse
+)
 
 export default emailRouter;
