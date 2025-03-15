d0ae04edf8de3ab1c00736dbdba8fd64aa3b19fb
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-15 15:36:37 +0100

    Création des routes get, post, put, delete de la route album

---
+++ server/src/router/albumRouter/albumRouter.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import { Request, Response } from "express";
+
+const albumRouter = express.Router();
+
+albumRouter.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        reponse: "Route Get ok : Récupération de tous les albums",
+    });
+});
+
+albumRouter.post("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        reponse: "Route Post ok : Création album",
+    });
+});
+
+albumRouter.put("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        reponse: "Route Put ok : Modification album",
+    });
+});
+
+albumRouter.delete("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        reponse: "Route Post ok : Suppression album",
+    });
+});
+
+export default albumRouter;
