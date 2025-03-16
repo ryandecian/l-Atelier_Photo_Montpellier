db4d6bf6542df0e5644a14d43039074eb1761bea
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-16 19:49:51 +0100

    Intégration des res réponses aux routes factures  avec commentaires URI

--- server/src/router/facturesRouter/facturesRouter.ts
+++ server/src/router/facturesRouter/facturesRouter.ts
@@ -3,7 +3,23 @@ import express from "express";
 const facturesRouter = express.Router();
 
 //import des Middlewares :
+import facturesGetResponse from "./facturesResponse/facturesGetResponse";
+import facturesPostResponse from "./facturesResponse/facturesPostResponse";
+import facturesDeleteResponse from "./facturesResponse/facturesDeleteResponse";
 
-facturesRouter.get("/", );
+// URI : /api/factures
+facturesRouter.get("/", 
+    facturesGetResponse,
+);
+
+// URI : /api/factures/upload
+facturesRouter.post("/upload", 
+    facturesPostResponse,
+);
+
+// URI : /api/factures/:id
+facturesRouter.delete("/:id", 
+    facturesDeleteResponse,
+);
 
 export default facturesRouter;
