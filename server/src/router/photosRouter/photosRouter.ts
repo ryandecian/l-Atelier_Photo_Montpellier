df7fb6ef14b0c586660a3384dfe4c86af3eaa7eb
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-16 19:17:11 +0100

    Ajout de commentaire URI sur la route Photos

--- server/src/router/photosRouter/photosRouter.ts
+++ server/src/router/photosRouter/photosRouter.ts
@@ -7,14 +7,17 @@ import photosGetResponse from "./photosResponse/photosGetResponse";
 import photosPostResponse from "./photosResponse/photosPostResponse";
 import photosDeleteResponse from "./photosResponse/photosDeleteResponse";
 
+// URI : /api/photos
 photosRouter.get("/",
     photosGetResponse,
 );
 
+// URI : /api/photos/upload
 photosRouter.post("/upload",
     photosPostResponse,
 );
 
+// URI : /api/photos/:id
 photosRouter.delete("/:id",
     photosDeleteResponse,
 );
