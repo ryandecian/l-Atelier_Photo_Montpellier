313b821f95eaa40c20dee530323373baea7f55d2
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-21 05:21:39 +0100

    Création de la page Admin (Initialisation)

---
+++ client/src/pages/AdminPage/AdminPage.tsx
@@ -0,0 +1,22 @@
+import style from "./AdminPage.module.css";
+import NavRoot from "../../components/NavRoot/NavRoot";
+// import AdminRoot from "../../components/AdminRoot/AdminRoot";
+import FooterRoot from "../../components/FooterRoot/FooterRoot";
+
+function AdminPage() {
+    return (
+        <div className={style.AdminPage}>
+            <header className={style.Header}>
+                <NavRoot />
+            </header>
+            <main className={style.Main}>
+                <h1>Admin</h1>
+            </main>
+            <footer className={style.Footer}>
+                <FooterRoot />
+            </footer>
+        </div>
+    );
+}
+
+export default AdminPage;
