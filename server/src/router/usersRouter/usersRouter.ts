61b6b4ad2637ce8465484b94a2c16b5be1675c84
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-21 04:00:23 +0100

    Création de getUserMe

--- server/src/router/usersRouter/usersRouter.ts
+++ server/src/router/usersRouter/usersRouter.ts
@@ -3,10 +3,37 @@ import express from "express";
 const usersRouter = express.Router();
 
 // Import des Middlewares :
+import isAdmin from "../../middleware/isAdmin/isAdmin";
+import Verify_JWT_Middleware from "../../middleware/JWT/Verify_JWT_Middleware";
+import usePoolConnection from "../../database/config";
+import { RowDataPacket } from "mysql2";
+import getAllUsers from "../../middleware/getUsers/getAllUser";
 
-
-
+// Liste de tous les utilisateurs (admin seulement)
 // URI : /api/user
-usersRouter.get("/", (req, res) => {})
+usersRouter.get("/", 
+    Verify_JWT_Middleware,
+    isAdmin,
+    getAllUsers,
+    (req, res) => {
+        try {
+            // Envois de la liste de tout les utilisateurs
+            res.status(200).json({ data: req.body.dataUser });
+        }
+        catch (error) {
+            res.status(500).json({ error: "Erreur interne serveur." });
+            console.error({
+                identity: "usersRouter.ts",
+                type: "router",
+                chemin: "/server/src/router/usersRouter/usersRouter.ts",
+                "❌ Nature de l'erreur": "Erreur non gérée dans le serveur !",
+                details: error,
+            });
+            return;
+        }
+    }
+);
+
+usersRouter.get("/user/me",);
 
 export default usersRouter;
