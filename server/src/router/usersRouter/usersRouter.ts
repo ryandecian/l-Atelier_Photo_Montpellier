43befe06c7272d797a7f471d71233712f7a2d3cd
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-21 04:34:57 +0100

    création de la route users avec methode delete

--- server/src/router/usersRouter/usersRouter.ts
+++ server/src/router/usersRouter/usersRouter.ts
@@ -82,4 +82,90 @@ usersRouter.put("/me",
     }
 )
 
+// Modification des données d'un utilisateur (admin seulement)
+// URI : /api/users/:id
+usersRouter.put("/:id",
+    Verify_JWT_Middleware,
+    isAdmin,
+    async (req, res) => {
+        try {
+            const userId = req.params.id;
+
+            const [dataUser] = await usePoolConnection.query(
+                "UPDATE user SET firstname = ?, lastname = ?, email = ?, address = ? WHERE id = ?",
+                [req.body.firstname, req.body.lastname, req.body.email, req.body.address, userId]
+            );
+
+            // Vérification : au moins une ligne modifiée ?
+            if ((dataUser as any).affectedRows === 0) {
+                res.status(404).json({ message: "Échec de la modification" });
+                console.error({
+                    identity: "usersRouter.ts",
+                    type: "router",
+                    chemin: "/server/src/router/usersRouter/usersRouter.ts",
+                    "❌ Nature de l'erreur": "Échec de la modification des données de l'utilisateur.",
+                });
+                return;
+            }
+
+            // Envois de la confirmation de la modification
+            res.status(200).json({ message: "Modification réussie" });
+        }
+        catch (error) {
+            res.status(500).json({ error: "Erreur interne serveur." });
+            console.error({
+                identity: "usersRouter.ts",
+                type: "router",
+                chemin: "/server/src/router/usersRouter/usersRouter.ts",
+                "❌ Nature de l'erreur": "Erreur non gérée dans le serveur !",
+                details: error,
+            });
+            return;
+        }
+    }
+);
+
+// Suppression d'un utilisateur (admin seulement)
+// URI : /api/users/:id
+usersRouter.delete("/:id",
+    Verify_JWT_Middleware,
+    isAdmin,
+    async (req, res) => {
+        try {
+            const userId = req.params.id;
+
+            const [dataUser] = await usePoolConnection.query(
+                "DELETE FROM user WHERE id = ?",
+                [userId]
+            );
+
+            // Vérification : au moins une ligne supprimée ?
+            if ((dataUser as any).affectedRows === 0) {
+                res.status(404).json({ message: "Échec de la suppression" });
+                console.error({
+                    identity: "usersRouter.ts",
+                    type: "router",
+                    chemin: "/server/src/router/usersRouter/usersRouter.ts",
+                    "❌ Nature de l'erreur": "Échec de la suppression de l'utilisateur.",
+                });
+                return;
+            }
+
+            // Envois de la confirmation de la suppression
+            res.status(200).json({ message: "Suppression réussie" });
+        }
+        catch (error) {
+            res.status(500).json({ error: "Erreur interne serveur." });
+            console.error({
+                identity: "usersRouter.ts",
+                type: "router",
+                chemin: "/server/src/router/usersRouter/usersRouter.ts",
+                "❌ Nature de l'erreur": "Erreur non gérée dans le serveur !",
+                details: error,
+            });
+            return;
+        }
+    }
+);
+
 export default usersRouter;
