44d6a660d09d0d2338d637140361737991c3750a
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-31 23:40:27 +0200

    Fin de la rectification de la page login

--- client/src/components/LoginRoot/LoginRoot.tsx
+++ client/src/components/LoginRoot/LoginRoot.tsx
@@ -8,9 +8,9 @@ function LoginRoot() {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setError("");
+  const formulaireLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Empêche le rechargement de la page
+    setError(""); // Réinitialise le message d'erreur
 
     try {
       const response = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
@@ -23,24 +23,27 @@ function LoginRoot() {
 
       const data = await response.json();
 
+      // Vérifier si la réponse est correcte
+      // Si le serveur renvoie une erreur, on l'affiche
       if (!response.ok) {
-        throw new Error(data.message || "Identifiants incorrects.");
+        throw new Error(data.message || "Identifiants incorrects."); // A revoir les message côté serveur
       }
 
-      // 🔐 Stocker les infos utilisateur + token
+      // Stocker les infos utilisateur + token
       localStorage.setItem("jwtToken", data.jwt);
       localStorage.setItem("userId", String(data.id));
       localStorage.setItem("userEmail", data.email);
       localStorage.setItem("userRole", data.role);
       localStorage.setItem("userName", `${data.firstname} ${data.lastname}`);
 
-      // ✅ Rediriger vers la bonne page
+      // Rediriger vers la bonne page
       if (data.role === "admin") {
         navigate("/admin");
       } else {
         navigate("/compte");
       }
-    } catch (err: unknown) {
+    } 
+    catch (err: unknown) {
       if (err instanceof Error) {
         console.error("Erreur login:", err.message);
         setError(err.message);
@@ -52,15 +55,15 @@ function LoginRoot() {
   };
 
   return (
-    <div className={style.LoginRoot}>
+    <section className={style.LoginRoot}>
       <h1>Connexion 🔐</h1>
-      <form onSubmit={handleLogin} className={style.form}>
+      <form onSubmit={formulaireLogin} className={style.form}>
         <label>Email</label>
         <input
           type="email"
           value={email}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-          required
+          required // Champ obligatoire
         />
 
         <label>Mot de passe</label>
@@ -68,13 +71,13 @@ function LoginRoot() {
           type="password"
           value={password}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-          required
+          required // Champ obligatoire
         />
 
         <button type="submit">Se connecter</button>
         {error && <p className={style.error}>{error}</p>}
       </form>
-    </div>
+    </section>
   );
 }
 
