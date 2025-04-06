3ff7662be8f8d5b591b9fbca96198df685bc36b3
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-06 10:03:45 +0200

    Initialisation du controller resetPasswordConfirmController.ts

---
+++ server/src/controllers/resetPasswordConfirmController.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const resetPasswordConfirmController = express.Router();
+
+// Import des Types :
+
+// Import des Middlewares :
+
+// Import des Repositories :
+
+// Import des Services :
+
+// Import des Outils :
+
+// Vérification :
+// URI : /api/resetpassword/confirm
+
