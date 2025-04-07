befd9c97334388430e10c558ca6b4ea59b8eaa41
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-07 22:25:10 +0200

    Rectification des classname

--- client/src/components/HomeRoot/ComponentsHomeRoot/HomeRootComponentB/HomeRootComponentB.tsx
+++ client/src/components/HomeRoot/ComponentsHomeRoot/HomeRootComponentB/HomeRootComponentB.tsx
@@ -1,34 +1,34 @@
-import styleRoot from "../../../StyleRootComponent.module.css";
+import style from "../../../StyleRootComponent.module.css";
 import ListDataRouter from "../../../../router/router";
 import { Link } from "react-router-dom";
 
 function HomeRootComponentB() {
     return (
-        <section className={`${styleRoot.HomeRootContainerB} ${styleRoot.ContainerRoot}`}> 
-            <header className={styleRoot.ContainerTitle}>
-                <h2 className={styleRoot.TitleH2}>
+        <section className={`${style.HomeRootContainerB} ${style.ContainerRoot}`}> 
+            <header className={style.ContainerTitle}>
+                <h2 className={style.TitleH2}>
                     J'observe, je vous mets à l'aise, je fige l'instant avec justesse.
                 </h2>
             </header>
 
-            <article className={styleRoot.ContainerText}>
-                <p className={styleRoot.TextP4}>
+            <article className={style.ContainerText}>
+                <p className={style.TextP4}>
                     En tant que {" "}
-                    <span className={styleRoot.SpanBold}>
+                    <span className={style.SpanBold}>
                         {"photographe professionnelle"}
                     </span> 
                     {""} installée à Montpellier et {" "}
-                    <span className={styleRoot.SpanBold}>
+                    <span className={style.SpanBold}>
                         {"spécialisée en portraits"}
                     </span>
                     {" "} – qu’ils soient {" "} 
-                    <span className={styleRoot.SpanLink}>
+                    <span className={style.SpanLink}>
                         <Link to={ListDataRouter[11].path}>
                             {"corporate"}
                         </Link>
                     </span>
                     , {" "}
-                    <span className={styleRoot.SpanLink}>
+                    <span className={style.SpanLink}>
                         <Link to={ListDataRouter[12].path}>
                             {"lifestyle"}
                         </Link>
@@ -41,9 +41,9 @@ function HomeRootComponentB() {
                     (elle) positif et valorisant.
                 </p>
             </article>
-            <article className={styleRoot.ContainerText}>
-                <p className={styleRoot.TextP4}>
-                    <span className={styleRoot.SpanBold}>
+            <article className={style.ContainerText}>
+                <p className={style.TextP4}>
+                    <span className={style.SpanBold}>
                         {"Ma vision du mariage"}
                     </span>
                     {" "} se décline sous la forme d’un reportage photographique. 
@@ -51,7 +51,7 @@ function HomeRootComponentB() {
                     et la spontanéité des émotions vécues durant la cérémonie. Je cherche à 
                     être au plus près de tous ces instants afin qu’ils restent gravés, et cette 
                     approche me permet de {" "}
-                    <span className={styleRoot.SpanBold}>
+                    <span className={style.SpanBold}>
                         {"raconter une histoire visuelle complète"}
                     </span>
                     , où chaque image capte la complicité, la tendresse et l’authenticité du grand jour.
--- client/src/components/HomeRoot/ComponentsHomeRoot/HomeRootComponentC/HomeRootComponentC.tsx
+++ client/src/components/HomeRoot/ComponentsHomeRoot/HomeRootComponentC/HomeRootComponentC.tsx
@@ -1,4 +1,4 @@
-import styleRoot from "../../../StyleRootComponent.module.css"
+import style from "../../../StyleRootComponent.module.css"
 import css from "./HomeRootComponentC.module.css";
 import Container4ImagesRoot from "../../../ContainerImageRoot/Container4ImagesRoot/Container4ImagesRoot";
 import { Link } from "react-router-dom";
@@ -7,9 +7,9 @@ import ControllerDataImageHome from "../../ControllerDataImageHome";
 
 function HomeRootComponentC() {
     return (
-        <section className={`${styleRoot.HomeRootComponentC} ${styleRoot.ContainerRoot}`}>
-            <header className={styleRoot.ContainerTitle}>
-                <h2 className={styleRoot.TitleH2}>
+        <section className={`${style.HomeRootComponentC} ${style.ContainerRoot}`}>
+            <header className={style.ContainerTitle}>
+                <h2 className={style.TitleH2}>
                     Chaque portrait a son histoire à raconter
                 </h2>
             </header>
@@ -25,45 +25,45 @@ function HomeRootComponentC() {
                 MetaNameImg4={ControllerDataImageHome[3].alt}
             />
 
-            <article className={styleRoot.ContainerText}>
-                <p className={`${styleRoot.TextP4} ${css.TargetText1}`}>
+            <article className={style.ContainerText}>
+                <p className={`${style.TextP4} ${css.TargetText1}`}>
                     Et je cherche à capturer ces histoires. Pour son entreprise, pour ses réseaux 
                     sociaux, pour une connivence…je cherche à trouver la photo de portrait qui fait 
                     votre force, votre énergie, {" "}
-                    <span className={styleRoot.SpanBold}>
+                    <span className={style.SpanBold}>
                         {"votre rayonnement."}
                     </span>
                 </p>
             </article>
-            <article className={styleRoot.ContainerText}>
-                <p className={styleRoot.TextP4}>
+            <article className={style.ContainerText}>
+                <p className={style.TextP4}>
                     Que vous cherchiez un {" "}
-                    <span className={styleRoot.SpanBold}>
+                    <span className={style.SpanBold}>
                         {"portrait corporate"}
                     </span>
                     {" "} pour affirmer votre crédibilité, un {" "}
-                    <span className={styleRoot.SpanBold}>
-                        {"portrait lifestyleRoot"}
+                    <span className={style.SpanBold}>
+                        {"portrait lifestyle"}
                     </span>
                     {" "} qui capte l’ambiance d’un moment, ou encore un moment familial empreint 
                     de complicité, chaque séance est conçue pour vous mettre en lumière dans toute 
                     votre singularité. En tant que photographe professionnelle {" "} 
-                    <span className={styleRoot.SpanBold}>
+                    <span className={style.SpanBold}>
                         {"je peux travailler en studio ou en extérieur"}
                     </span>
                     , selon vos envies, dans une atmosphère calme et chaleureuse.
                 </p>
             </article>
-            <article className={styleRoot.ContainerText}>
-                <p className={styleRoot.TextP4}>
+            <article className={style.ContainerText}>
+                <p className={style.TextP4}>
                     Et pour moi la réussite d’un shooting tient à ce que vous repartiez non seulement 
                     après avoir passé un bon moment, mais surtout avec des photos qui vous plaisent, 
                     que vous avez envie de partager sur vos réseaux, et vous puissiez vous dire {" "}
-                    <span className={styleRoot.SpanLine}>
+                    <span className={style.SpanLine}>
                         {"ah oui je m’aime bien dessus !"}
                     </span>
                     {" "} Commencez à planifier dès maintenant {" "}
-                    <span className={styleRoot.SpanLink}>
+                    <span className={style.SpanLink}>
                             <Link to={ListDataRouter[12].path}>
                                 {"votre séance"}
                             </Link>
