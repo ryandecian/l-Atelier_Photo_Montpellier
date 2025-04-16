fa3670cb9965c4bbd25ec52e5460fd56041f8773
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-16 20:48:35 +0200

    Première partie du composant B ok

--- client/src/components/CorporateRoot/ComponentsCorporateRoot/CorporateRootComponentB/CorporateRootComponentB.tsx
+++ client/src/components/CorporateRoot/ComponentsCorporateRoot/CorporateRootComponentB/CorporateRootComponentB.tsx
@@ -9,6 +9,17 @@ function CorporateRootComponentB() {
                     Prêt(e) à donner du poids à votre signature visuelle d’entreprise ? 
                 </h2>
             </header>
+
+            <p className={style.TextP4}>
+                Avant de déclencher l’appareil, je vous propose un
+                <span className={style.SpanBold}>
+                    {" temps d’échange et de repérage. "}
+                </span>
+                L’objectif ? Définir ensemble le ton, le style et l’ambiance qui correspondent le mieux à votre image de marque. 
+            </p>
+                <span className={style.SpanBold}>
+                    {" "}
+                </span>
         </section>
     )
 }
