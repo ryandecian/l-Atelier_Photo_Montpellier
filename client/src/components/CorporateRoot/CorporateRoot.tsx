d4189432cad23ecb2afbabf05b89e14390245ae2
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-16 18:40:05 +0200

    Création du composant CorporateRoot. Passage au débug du dimensionnement de la page dans sa globalité

--- client/src/components/CorporateRoot/CorporateRoot.tsx
+++ client/src/components/CorporateRoot/CorporateRoot.tsx
@@ -2,7 +2,14 @@ import style from "../StyleRootComponent.module.css";
 
 function CorporateRoot() {
     return (
-        <h1>Corporate</h1>
+        <section className={`CorporateRoot ${style.ContainerRootRacine}`}>
+
+            <header className={style.ContainerTitle}>
+                <h1 className={style.TitleH1}>
+                    L'importance de votre signature visuelle : bien plus qu'une simple photo !
+                </h1>
+            </header>
+        </section>
     )
 }
 
