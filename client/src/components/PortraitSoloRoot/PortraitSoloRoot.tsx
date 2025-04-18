ad415e17703ffd733d96c18ca33d75850cd08a73
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-18 13:30:13 +0200

    Initialisation du ComponentA

--- client/src/components/PortraitSoloRoot/PortraitSoloRoot.tsx
+++ client/src/components/PortraitSoloRoot/PortraitSoloRoot.tsx
@@ -4,7 +4,7 @@ function PortraitSoloRoot() {
     return (
         <section className={`PortraitSoloRoot ${style.ContainerRootRacine}`}>
             <header className={style.ContainerTitle}>
-                <h1 className={style.TitreH1}>
+                <h1 className={style.TitleH1}>
                     Portrait Solo : Une séance photos pour renouer avec votre image !
                 </h1>
             </header>
