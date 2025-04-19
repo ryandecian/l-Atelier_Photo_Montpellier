5b18f7de098cece60b3f6add87982d94cb8f710f
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-19 22:29:19 +0200

    Paragraphe du componentA ok

--- client/src/components/PortraitSoloRoot/ComponentsPortraitSoloRoot/PortraitSoloComponentA/PortraitSoloComponentA.tsx
+++ client/src/components/PortraitSoloRoot/ComponentsPortraitSoloRoot/PortraitSoloComponentA/PortraitSoloComponentA.tsx
@@ -5,9 +5,21 @@ function PortraitSoloComponentA() {
         <section className={style.ContainerRootRacine}>
             <header className={style.ContainerTitle}>
                 <h2 className={style.TitleH2}>
-                    Un Portrait Feel Good, ce n'ets pas qu'un beau cliché !
+                    Un Portrait Feel Good, ce n'est pas qu'un beau cliché !
                 </h2>
             </header>
+            
+            <p className={style.TextP4}>
+                C'est un moment pour soi. <br />
+                Un espace-temps rare où l’on se pose, on respire, on ose être soi-même… devant l’objectif, mais aussi en dedans.
+            </p>
+
+            <p className={style.TextP4}>
+                Un portrait solo feel good, c’est 
+                <span className={style.SpanBold}>
+                    {" s’autoriser à se voir autrement : "}
+                </span>
+            </p>
         </section>
     )
 }
