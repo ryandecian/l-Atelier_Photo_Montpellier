75303d377fb03173759e9f6664ef0ef87dd719d8
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-30 23:04:47 +0200

    Initialisation du composant GrossesseRootComponenetA

---
+++ client/src/components/GrossesseRoot/ComponentsGrossesseRoot/GrossesseRootComponentA/GrossesseRootComponentA.tsx
@@ -0,0 +1,17 @@
+import style from "../../../StyleRootComponent.module.css";
+
+function GrossesseRootComponentA() {
+    return (
+        <section className={`GrossesseRootComponentC ${style.ContainerRootRacine}`}>
+            <header className={style.ContainerTitle}>
+                <h2 className={style.TitleH2}>
+                    {"Un service clé en main pour les entreprises : simplicité et efficacité !"}
+                </h2>
+            </header>
+            
+            <article></article>
+        </section>
+    )
+}
+
+export default GrossesseRootComponentA;
