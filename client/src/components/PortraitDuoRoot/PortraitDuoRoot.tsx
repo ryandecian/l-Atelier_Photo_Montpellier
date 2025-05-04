e5ce36176f43d3dfc230400ed8004b323919ac56
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-04 16:33:00 +0200

    Initialisation de la Page Duo et couple

---
+++ client/src/components/PortraitDuoRoot/PortraitDuoRoot.tsx
@@ -0,0 +1,22 @@
+import style from "../StyleRootComponent.module.css";
+
+
+function PortraitDuoRoot() {
+    return (
+        <section className={`PortraitDuoRoot ${style.ContainerRootRacine}`}>
+            <header className={style.ContainerTitle}>
+                <h1 className={style.TitleH1}>
+                    Portrait Duo : Une séance photos pour renouer avec votre image !
+                </h1>
+            </header>
+
+            {/* Container A */}
+
+            {/* Container B */}
+
+            {/* Container C : Avis Clients */}
+        </section>
+    )
+}
+
+export default PortraitDuoRoot;
