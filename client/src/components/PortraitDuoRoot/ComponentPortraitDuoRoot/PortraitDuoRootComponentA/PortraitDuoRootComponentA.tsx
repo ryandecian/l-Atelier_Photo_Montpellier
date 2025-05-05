0fccea0232980f68bfcf078cdfeb951e0f12f4f9
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-05 15:43:10 +0200

    Refactorisation du composant A de la page portrait solo

---
+++ client/src/components/PortraitDuoRoot/ComponentPortraitDuoRoot/PortraitDuoRootComponentA/PortraitDuoRootComponentA.tsx
@@ -0,0 +1,11 @@
+import style from "../../../StyleRootComponent.module.css";
+
+function PortraitDuoRootComponentA() {
+  return (
+    <section className={`PortraitDuoRootComponentA ${style.ContainerRacineRoot}`}>
+      <h1>Portrait Duo Root Component A</h1>
+    </section>
+  );
+}
+
+export default PortraitDuoRootComponentA;
