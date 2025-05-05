0c37de9139a511d4694e696207cacfbc97de38d3
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-06 00:58:35 +0200

    Push partiel de la nouvelle logique de vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, // Permet d'écouter sur toutes les interfaces réseau
-    port: 4000, // Ou tout autre port que vous préférez
-  },
+export default defineConfig(() => {
+  const root = process.cwd();
+
+  // Charge uniquement les fichiers souhaités :
+  const envBase = loadEnv('base', root, 'VITE_'); // .env.base
+  const envRoot = loadEnv('root', root, 'VITE_'); // .env.root
+
+  // Fusion manuelle des variables (ordre de priorité : root > base)
+  const env = {
+    ...envBase,
+    ...envRoot
+  };
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 4000
+    },
+
+    // Redéfinit import.meta.env à la main pour bloquer tout chargement implicite, ne charge plus les .env par défauts
+    define: {
+      'import.meta.env': JSON.stringify(env)
+    }
+  };
 });
