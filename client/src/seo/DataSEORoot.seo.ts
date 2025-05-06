74c8c2a462014d31f411c6490d1c11911a6ac442
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-06 15:38:40 +0200

    Rectification de quelque avis et ajout d'une logique d'intégration et d'affichage des avis automatique dans je JSON-LD + calcule automatique de la note moyenne

--- client/src/seo/DataSEORoot.seo.ts
+++ client/src/seo/DataSEORoot.seo.ts
@@ -3,6 +3,10 @@ const DataSEORoot = {
     "@type": "LocalBusiness",
     "name": "L'Atelier Photo Montpellier",
     "telephone": "+33 6 52 67 73 33",
+    "openingHours": [
+        "Mo-Su 10:00-13:00",
+        "Mo-Su 14:00-19:00"
+    ], // Horaires d'ouverture du lundi au dimanche de 10h à 13h et de 14h à 19h
 }
 
 export default DataSEORoot
