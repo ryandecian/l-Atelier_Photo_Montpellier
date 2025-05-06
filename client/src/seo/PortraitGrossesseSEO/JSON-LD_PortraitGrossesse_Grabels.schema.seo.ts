2d54571c289417555267556ed4e1fc1c12bb64f3
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-06 16:31:08 +0200

    Re rectification du nom et intégration des toutes dernières datas

--- client/src/seo/PortraitGrossesseSEO/JSON-LD_PortraitGrossesse_Grabels.schema.seo.ts
+++ client/src/seo/PortraitGrossesseSEO/JSON-LD_PortraitGrossesse_Grabels.schema.seo.ts
@@ -1,15 +1,16 @@
 import DataSEORoot from "../DataSEORoot.seo";
 import AvisClientGrossesseControllerData from "../../components/GrossesseRoot/AvisClientGrossesseControllerData";
-import GenerateAverageRatingSEO from "../../utils/geenerateAverageRatingSEO.utils";
-import GenerateReviewArraySEO from "../../utils/generateReviewArraySEO.utils";
+import generateAverageRatingSEO from "../../utils/GenerateAverageRatingSEO.utils";
+import generateReviewArraySEO from "../../utils/GenerateReviewArraySEO.utils";
+import DataSEO_PortraitGrossesse from "./DataSEO_PortraitGrossesse.seo";
 
 const JSON_LD_PortraitGrossesse_Grabels_Schema_SEO = JSON.stringify({
     "@context": DataSEORoot["@context"], // (Obligatoire) Contexte de la donnée
     "@type": DataSEORoot["@type"], // (Obligatoire) LocalBusiness
     "name": DataSEORoot.name, // (Obligatoire) Nom de l'entreprise
-    "image": SEO.img, // (Obligatoire) URL de l'image représentative de l'entreprise
-    "description": SEO.description,
-    "url": SEO.url, // (Obligatoire) URL de la page de l'entreprise
+    "image": DataSEO_PortraitGrossesse.img, // (Obligatoire) URL de l'image représentative de l'entreprise
+    "description": DataSEO_PortraitGrossesse.description,
+    "url": DataSEO_PortraitGrossesse.url, // (Obligatoire) URL de la page de l'entreprise
     "telephone": DataSEORoot.telephone, // (Obligatoire) Numéro de téléphone de l'entreprise
     "address": {
       "@type": "PostalAddress",
@@ -20,8 +21,8 @@ const JSON_LD_PortraitGrossesse_Grabels_Schema_SEO = JSON.stringify({
     }, // (Obligatoire) Adresse de l'entreprise
     "priceRange": "€€",
     "openingHours": DataSEORoot.openingHours, // Heures d'ouverture de l'entreprise
-    "aggregateRating": GenerateAverageRatingSEO(AvisClientGrossesseControllerData), // Note moyenne des avis clients et le nombre d'avis
-    "review": GenerateReviewArraySEO(AvisClientGrossesseControllerData), // Met a disposition les avis clients dans le SEO avec les bonnes clés
+    "aggregateRating": generateAverageRatingSEO(AvisClientGrossesseControllerData), // Note moyenne des avis clients et le nombre d'avis
+    "review": generateReviewArraySEO(AvisClientGrossesseControllerData), // Met a disposition les avis clients dans le SEO avec les bonnes clés
 });
 
 export default JSON_LD_PortraitGrossesse_Grabels_Schema_SEO;
