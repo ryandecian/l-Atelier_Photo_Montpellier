3b4d5be749f390e459f1bac261c5612bb3ee4776
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-08 20:17:25 +0200

    Création du Helmet de la page Home avec DataSEO_Home

--- client/src/seo/HomeSEO/DataSEO_Home.seo.ts
+++ client/src/seo/HomeSEO/DataSEO_Home.seo.ts
@@ -1,13 +1,13 @@
 import ListDataRouter from "../../router/router";
 import DataSEOTargetOneType from "../../types/DataSEOTargetOne";
 
-function DataSEO_PortraitGrossesse(): DataSEOTargetOneType {
+function DataSEO_Home(): DataSEOTargetOneType {
     const DataSEO = {
         /* Data Helmet */
-        title: "Portrait Grossesse et nouveau né - l'Atelier Photo Montpellier",
+        title: "Accueil - l'Atelier Photo Montpellier",
         autor: "Anne SAUNIER",
-        description: "Avec l’Atelier Photo Montpellier, Anne SAUNIER capture la magie de la grossesse et les premiers instants de vie à travers des portraits doux et authentiques.",
-        url: `${import.meta.env.VITE_DOMAIN_CLIENT}${ListDataRouter[17].path}`, /*URL de la page*/
+        description: "Photographe professionnelle à Montpellier, Anne SAUNIER vous accueille à l’Atelier Photo Montpellier pour des portraits vrais, élégants et lumineux.",
+        url: `${import.meta.env.VITE_DOMAIN_CLIENT}${ListDataRouter[0].path}`, /*URL de la page*/
         twitterUrlImg: "", /* Lien URL de l'image */
         keywords: {
             1: "Anne SAUNIER", /* 1 mot clés */
@@ -15,13 +15,13 @@ function DataSEO_PortraitGrossesse(): DataSEOTargetOneType {
             3: "Photographe Montpellier", /* 3 mots clés */
             4: "l'Atelier Photo Montpellier", /* 4 mots clés */
             5: "LAPM", /* 5 mots clés */
-            6: "Service", /* 6 mots clés */
-            7: "Portrait Grossesse", /* 7 mots clés */
-            8: "Portrait nouveau né", /* 8 mots clés */
-            9: "", /* 9 mots clés */
-            10: "", /* 10 mots clés */
-            11: "", /* 11 mots clés */
-            12: "", /* 12 mots clés */
+            6: "Meilleur photogaphe montpellier", /* 6 mots clés */
+            7: "Photographe professionnel", /* 7 mots clés */
+            8: "Photographe sud de france", /* 8 mots clés */
+            9: "Photographe Grabels", /* 9 mots clés */
+            10: "Photographe rapide", /* 10 mots clés */
+            11: "Déplacement photographe", /* 11 mots clés */
+            12: "Photographe animalier Montpellier", /* 12 mots clés */
             13: "", /* 13 mots clés */
             14: "", /* 14 mots clés */
             15: "", /* 15 mots clés */
@@ -37,7 +37,7 @@ function DataSEO_PortraitGrossesse(): DataSEOTargetOneType {
         /* Data Helmet et JSON-LD */
         img: `${import.meta.env.VITE_DOMAIN_CLIENT}/images-seo/portrait-grossesse/dans-les-yeux-de-sacha.jpg`, /*Lien URL public de l'image*/
         id_Service: `${import.meta.env.VITE_DOMAIN_CLIENT}${ListDataRouter[17].path}#service-portrait-grossesse`, // @id SEO-friendly, correspond à l'URL de la page + mots clés
-        serviceType: "Photographie grossesse et nouveau-né",
+        serviceType: "Découverte des services de photographie professionnelle",
     };
 
     return (
@@ -45,4 +45,4 @@ function DataSEO_PortraitGrossesse(): DataSEOTargetOneType {
     )
 }
 
-export default DataSEO_PortraitGrossesse;
+export default DataSEO_Home;
