0e42c3b5d54563c0079813dc25519546c92c53ff
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-10 00:35:44 +0200

    Création du JSON-LD Service de la page Corporate

--- client/src/seo/PortraitGrossesseSEO/JSON-LD_Service_PortraitGrossesse.schema.seo.tsx
+++ client/src/seo/PortraitGrossesseSEO/JSON-LD_Service_PortraitGrossesse.schema.seo.tsx
@@ -21,6 +21,7 @@ function JSON_LD_Service_PortraitGrossesse_Schema_SEO() : string {
         "name": DataSEORoot.name, /* (Obligatoire) Nom de l'entreprise */
         "image": DataSEO_PortraitGrossesse.img_JSON_LD, /* (Obligatoire) URL de l'image de l'entreprise */
         "description": DataSEO_PortraitGrossesse.description,
+        "termsOfService": DataSEORoot.termsOfService,
         "provider": {
             "@id": DataSEORoot.id_LocalBusiness /* (Obligatoire) Référence à l'ID de l'Entreprise */
         },
