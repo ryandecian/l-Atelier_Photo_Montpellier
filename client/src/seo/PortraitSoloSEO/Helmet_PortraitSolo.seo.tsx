30546a860948f1dd141d0796b3d0987647b02fe1
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-13 23:30:56 +0200

    Edition du Helmet de la page Solo

--- client/src/seo/PortraitSoloSEO/Helmet_PortraitSolo.seo.tsx
+++ client/src/seo/PortraitSoloSEO/Helmet_PortraitSolo.seo.tsx
@@ -2,29 +2,29 @@ import { Helmet } from "react-helmet-async";
 import DataSEOHelmetType from "../../types/DataSEOHelmet";
 
 import DataSEORoots from "../DataSEORoot.seo";
-import DataSEO_PortraitGrossesses from "./DataSEO_PortraitSolo.seo";
+import DataSEO_PortraitSolos from "./DataSEO_PortraitSolo.seo";
 
 import DataSEORootType from "../../types/DataSEORoot.type";
 import DataSEOTargetOneType from "../../types/DataSEOTargetOne";
 
 import JSON_LD_LocalBusiness_Root_Schema_SEO from "../JSON-LD_LocalBusiness_Root.schema.seo";
-import JSON_LD_Service_PortraitGrossesse_Schema_SEO from "./JSON-LD_Service_PortraitSolo.schema.seo";
+import JSON_LD_Service_PortraitSolo_Schema_SEO from "./JSON-LD_Service_PortraitSolo.schema.seo";
 
 
-function Helmet_PortraitGrossesse_SEO() {
+function Helmet_PortraitSolo_SEO() {
     const DataSEORoot: DataSEORootType = DataSEORoots(); /* Récupération des données SEO */
-    const DataSEO_PortraitGrossesse: DataSEOTargetOneType = DataSEO_PortraitGrossesses(); /* Récupération des données SEO Portrait Grossesse */
+    const DataSEO_PortraitSolo: DataSEOTargetOneType = DataSEO_PortraitSolos(); /* Récupération des données SEO Portrait Solo */
 
     const SEO: DataSEOHelmetType = {
-        title: DataSEO_PortraitGrossesse.title,
+        title: DataSEO_PortraitSolo.title,
         autor: DataSEORoot.autor,
-        description: DataSEO_PortraitGrossesse.description,
-        url: DataSEO_PortraitGrossesse.url,
-        img: DataSEO_PortraitGrossesse.img_Helmet,
-        twitterUrlImg: DataSEO_PortraitGrossesse.twitterUrlImg || DataSEO_PortraitGrossesse.img_Helmet, // fallback
+        description: DataSEO_PortraitSolo.description,
+        url: DataSEO_PortraitSolo.url,
+        img: DataSEO_PortraitSolo.img_Helmet,
+        twitterUrlImg: DataSEO_PortraitSolo.twitterUrlImg || DataSEO_PortraitSolo.img_Helmet, // fallback
         twitterCompte: DataSEORoot.twitterCompte,
-        keywords: DataSEO_PortraitGrossesse.keywords,
-        type: DataSEO_PortraitGrossesse.type,
+        keywords: DataSEO_PortraitSolo.keywords,
+        type: DataSEO_PortraitSolo.type,
     };
 
     const filterKeywords = Object.values(SEO.keywords).filter(keyword => keyword.trim() !== "").join(", ");
@@ -36,7 +36,7 @@ function Helmet_PortraitGrossesse_SEO() {
                 {JSON_LD_LocalBusiness_Root_Schema_SEO()}
             </script>
             <script type="application/ld+json">
-                {JSON_LD_Service_PortraitGrossesse_Schema_SEO()}
+                {JSON_LD_Service_PortraitSolo_Schema_SEO()}
             </script>
 
             {/* Langue principale du document */}
@@ -74,4 +74,4 @@ function Helmet_PortraitGrossesse_SEO() {
     );
 }
 
-export default Helmet_PortraitGrossesse_SEO;
+export default Helmet_PortraitSolo_SEO;
