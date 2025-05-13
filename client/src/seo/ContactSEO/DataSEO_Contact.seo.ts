a38f09ab851d91c3bc9185a597d0b346ba92a017
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-14 00:15:07 +0200

    JSON-LD et DataSEO de la page contact ok

---
+++ client/src/seo/ContactSEO/DataSEO_Contact.seo.ts
@@ -0,0 +1,49 @@
+import ListDataRouter from "../../router/router";
+import DataSEOTargetOneType from "../../types/DataSEOTargetOne";
+
+function DataSEO_PortraitSolo(): DataSEOTargetOneType {
+    const DataSEO = {
+        /* Data Helmet */
+        title: "Service Portrait Solo - l'Atelier Photo Montpellier",
+        autor: "Anne SAUNIER",
+        description: "Portrait Solo à Montpellier – Une séance photo pour se recentrer, se révéler et se faire du bien. Un regard bienveillant par Anne SAUNIER, photographe professionnelle.",
+        url: `${import.meta.env.VITE_DOMAIN_CLIENT}${ListDataRouter[3].path}`, /*URL de la page*/
+        twitterUrlImg: "", /* Lien URL de l'image */
+        keywords: {
+            1: "Anne SAUNIER", /* 1 mot clés */
+            2: "Photographe", /* 2 mots clés */
+            3: "Photographe Montpellier", /* 3 mots clés */
+            4: "l'Atelier Photo Montpellier", /* 4 mots clés */
+            5: "LAPM", /* 5 mots clés */
+            6: "Service", /* 6 mots clés */
+            7: "Portrait Solo", /* 7 mots clés */
+            8: "", /* 8 mots clés */
+            9: "", /* 9 mots clés */
+            10: "", /* 10 mots clés */
+            11: "", /* 11 mots clés */
+            12: "", /* 12 mots clés */
+            13: "", /* 13 mots clés */
+            14: "", /* 14 mots clés */
+            15: "", /* 15 mots clés */
+        }, /* 10 a 15 mots max */
+        type : {
+            website: "website", /*(Valeur par défaut) indique qu'il s'agit d'un site web classique.*/
+            article: "article", /*Pour des articles de blog ou du contenu éditorial.*/
+            video: "video.movie", /*Pour les pages contenant des vidéos de films.*/
+            music: "music.song", /*Pour les pages dédiées à la musique.*/
+            profile: "profile", /*Pour une page personnelle (profil d'une personne).*/
+        },
+    
+        /* Data Helmet et JSON-LD */
+        img_JSON_LD: `${import.meta.env.VITE_DOMAIN_CLIENT}/images-seo/portrait-grossesse/google/photo-professionnel-portrait-grossesse-montpellier.jpg`, /*Lien URL public de l'image*/
+        img_Helmet: `${import.meta.env.VITE_DOMAIN_CLIENT}/images-seo/portrait-grossesse/helmet/photo-professionnel-portrait-grossesse-montpellier.jpg`, /*Lien URL public de l'image*/
+        id_Service: `${import.meta.env.VITE_DOMAIN_CLIENT}${ListDataRouter[3].path}#service-portrait-solo`, // @id SEO-friendly, correspond à l'URL de la page + mots clés
+        serviceType: "Photo – portrait solo – séance de photographie individuelle en studio ou en extérieur",
+    };
+
+    return (
+        DataSEO
+    )
+}
+
+export default DataSEO_PortraitSolo;
