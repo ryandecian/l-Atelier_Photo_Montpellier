e1b27513abac86146ef065837bbe7a9f6c5ca3bc
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-17 18:37:55 +0200

    Création du JSON-LD Breadcrumb

--- client/src/seo/PortraitDuoSEO/JSON-LD_Breadcrumb_PortraitDuo.schema.seo.ts
+++ client/src/seo/PortraitDuoSEO/JSON-LD_Breadcrumb_PortraitDuo.schema.seo.ts
@@ -1,12 +1,12 @@
 import DataSEORoots from "../DataSEORoot.seo";
 import DataSEORootType from "../../types/DataSEORoot.type";
 
-import DataSEO_PortraitGrossesses from "./DataSEO_PortraitDuo.seo";
+import DataSEO_PortraitDuos from "./DataSEO_PortraitDuo.seo";
 import DataSEOTargetOneType from "../../types/DataSEOTargetOne";
 
-function JSON_LD_Breadcrumb_PortraitGrossesse_Schema_SEO() : string {
+function JSON_LD_Breadcrumb_PortraitDuo_Schema_SEO() : string {
     const DataSEORoot: DataSEORootType = DataSEORoots(); /* Récupération des données SEO */
-    const DataSEO_PortraitGrossesse: DataSEOTargetOneType = DataSEO_PortraitGrossesses(); /* Récupération des données SEO de la page */
+    const DataSEO_PortraitDuo: DataSEOTargetOneType = DataSEO_PortraitDuos(); /* Récupération des données SEO de la page */
 
     const JSON_LD = JSON.stringify({
         "@context": DataSEORoot["@context"],
@@ -14,9 +14,9 @@ function JSON_LD_Breadcrumb_PortraitGrossesse_Schema_SEO() : string {
         "itemListElement": [
             {
                 "@type": "ListItem",
-                "position": DataSEO_PortraitGrossesse.position,
-                "name": DataSEO_PortraitGrossesse.title,
-                "item": DataSEO_PortraitGrossesse.url,
+                "position": DataSEO_PortraitDuo.position,
+                "name": DataSEO_PortraitDuo.title,
+                "item": DataSEO_PortraitDuo.url,
             },
         ],
     });
@@ -26,4 +26,4 @@ function JSON_LD_Breadcrumb_PortraitGrossesse_Schema_SEO() : string {
     )
 }
 
-export default JSON_LD_Breadcrumb_PortraitGrossesse_Schema_SEO;
+export default JSON_LD_Breadcrumb_PortraitDuo_Schema_SEO;
