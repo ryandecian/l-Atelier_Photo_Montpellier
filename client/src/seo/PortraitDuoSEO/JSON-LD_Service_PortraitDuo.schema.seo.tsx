c765f6f0d6f1ea3f21867456f814c409f1c25618
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-17 18:34:45 +0200

    Création du JSON-LD de la page Portrait Duo

--- client/src/seo/PortraitDuoSEO/JSON-LD_Service_PortraitDuo.schema.seo.tsx
+++ client/src/seo/PortraitDuoSEO/JSON-LD_Service_PortraitDuo.schema.seo.tsx
@@ -1,27 +1,27 @@
 import DataSEORoots from "../DataSEORoot.seo";
 import DataSEORootType from "../../types/DataSEORoot.type";
 
-import DataSEO_PortraitGrossesses from "./DataSEO_PortraitDuo.seo";
+import DataSEO_PortraitDuos from "./DataSEO_PortraitDuo.seo";
 import DataSEOTargetOneType from "../../types/DataSEOTargetOne";
 
-import AvisClientPortraitGrossesseControllerData from "../../components/PortraitGrossesseRoot/AvisClientPortraitGrossesseControllerData";
+import AvisClientPortraitDuoControllerData from "../../components/PortraitDuoRoot/AvisClientPortraitDuoControllerData";
 import generateAverageRatingSEO from "../../utils/generateAverageRatingSEO.utils";
 import generateReviewArraySEO from "../../utils/generateReviewArraySEO.utils";
 
 
-function JSON_LD_Service_PortraitGrossesse_Schema_SEO() : string {
+function JSON_LD_Service_PortraitDuo_Schema_SEO() : string {
     const DataSEORoot: DataSEORootType = DataSEORoots(); /* Récupération des données SEO */
-    const DataSEO_PortraitGrossesse: DataSEOTargetOneType = DataSEO_PortraitGrossesses(); /* Récupération des données SEO de la page */
+    const DataSEO_PortraitDuo: DataSEOTargetOneType = DataSEO_PortraitDuos(); /* Récupération des données SEO de la page */
 
     const JSON_LD = JSON.stringify({
         "@context": DataSEORoot["@context"], /* (Obligatoire) Contexte de la donnée */
         "@type": DataSEORoot["@type"].Service, /* (Obligatoire) Type de la donnée */
-        "@id": DataSEO_PortraitGrossesse.id_Service,
-        "serviceType": DataSEO_PortraitGrossesse.serviceType,
+        "@id": DataSEO_PortraitDuo.id_Service,
+        "serviceType": DataSEO_PortraitDuo.serviceType,
         "name": DataSEORoot.name, /* (Obligatoire) Nom de l'entreprise */
-        "image": DataSEO_PortraitGrossesse.img_JSON_LD, /* (Obligatoire) URL de l'image de l'entreprise */
-        "description": DataSEO_PortraitGrossesse.description,
-        "url": DataSEO_PortraitGrossesse.url,
+        "image": DataSEO_PortraitDuo.img_JSON_LD, /* (Obligatoire) URL de l'image de l'entreprise */
+        "description": DataSEO_PortraitDuo.description,
+        "url": DataSEO_PortraitDuo.url,
         "termsOfService": DataSEORoot.termsOfService,
         "provider": {
             "@id": DataSEORoot.id_LocalBusiness /* (Obligatoire) Référence à l'ID de l'Entreprise */
@@ -40,10 +40,10 @@ function JSON_LD_Service_PortraitGrossesse_Schema_SEO() : string {
         // ⚠️ A décommenter lors du passage de site vitrine en e-commerce
         // "availableChannel": {
         // "@type": "ServiceChannel",
-        // "serviceUrl": DataSEO_PortraitGrossesse.url
+        // "serviceUrl": DataSEO_PortraitDuo.url
         // },
-        "aggregateRating": generateAverageRatingSEO(AvisClientPortraitGrossesseControllerData),
-        "review": generateReviewArraySEO(AvisClientPortraitGrossesseControllerData)
+        "aggregateRating": generateAverageRatingSEO(AvisClientPortraitDuoControllerData),
+        "review": generateReviewArraySEO(AvisClientPortraitDuoControllerData)
     });
 
     return (
@@ -51,4 +51,4 @@ function JSON_LD_Service_PortraitGrossesse_Schema_SEO() : string {
     )
 }
 
-export default JSON_LD_Service_PortraitGrossesse_Schema_SEO;
+export default JSON_LD_Service_PortraitDuo_Schema_SEO;
