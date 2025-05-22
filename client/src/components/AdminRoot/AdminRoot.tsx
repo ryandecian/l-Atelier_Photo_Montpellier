53dcf53a9e1ec2d084271650d65aeb4bcc577544
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-22 06:34:06 +0200

    Intégration parfaite du nouveau Hook de protection des pages simplifiant ainsi la protection des pages

--- client/src/components/AdminRoot/AdminRoot.tsx
+++ client/src/components/AdminRoot/AdminRoot.tsx
@@ -1,33 +1,20 @@
 import css from "./AdminRoot.module.css";
 import style from "../StyleRootComponent.module.css";
-import useAuthCheck from "../../hook/useAuthCheck.utils.hook";
 import ServiceCardComponent from "../ServiceCardComponent/ServiceCardComponent";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import useLogout from "../../hook/useLogout.hook";
 import ListDataRouter from "../../router/router";
+import useLockedPage from "../../hook/useLockedPage.security.hook";
 
 function AdminRoot() {
-  const { isLoggedIn, userInfo, isChecking } = useAuthCheck();
+  const userInfo = useLockedPage("admin");
   const logout = useLogout();
-  const navigate = useNavigate();
 
-  // Vérification du Role et de la connexion
-  useEffect(() => {
-    if (!isChecking && userInfo?.role === "user") {
-      navigate("/compte");
-    }
-  }, [isChecking, userInfo, navigate]);
-
-  // En attente de vérification du token
-  if (isChecking) return <p>Chargement...</p>;
-
-  // Sécurité supplémentaire
-  if (!isLoggedIn || !userInfo) return <p>Accès refusé</p>;
+  if (!userInfo) return <p>Chargement...</p>;
 
   return (
     <section className={`${css.AdminRoot} ${style.ContainerRootRacine}`}>
       <div className={css.TopBar}>
+        <p>Bonjour {userInfo.firstname} {userInfo.lastname}</p>
         <button className={css.LogoutButton} onClick={logout}>Déconnexion</button>
       </div>
         <h1 className={css.Title}>👑 Interface Admin</h1>
