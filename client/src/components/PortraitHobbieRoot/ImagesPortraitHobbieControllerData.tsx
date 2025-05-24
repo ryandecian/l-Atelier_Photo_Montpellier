26122f474f1b4bbd77d0b540756abbb383ffc870
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-24 13:09:43 +0200

    Initialisation des avis et images de la page Hobbie

---
+++ client/src/components/PortraitHobbieRoot/ImagesPortraitHobbieControllerData.tsx
@@ -0,0 +1,59 @@
+import ImagesControllerDataType from "../../types/ImagesControllerData.type";
+
+/**
+ * Compteur mot clés : 
+ * 
+ * photographe-professionnelle : 4/4
+ * photo-professionnel : 1/2
+ * photographie-professionnelle : 1/2
+ */
+
+const ImagesPortraitHobbieControllerData: ImagesControllerDataType[] = [
+    /* Tableau 1 : PortraitHobbieComponentA */
+    /* {ImagesPortraitHobbieControllerData[0].src} */
+    {
+        src: "/images/image-portrait-Hobbie/lapm-photographe-professionnelle-portrait-Hobbie-mere-et-fils-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnelle - portrait Hobbie - mère et fils - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesPortraitHobbieControllerData[1].src} */
+    {
+        src: "/images/image-portrait-Hobbie/lapm-photographe-professionnelle-portrait-Hobbie-pere-et-fils-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnelle - portrait Hobbie - père et fils - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesPortraitHobbieControllerData[2].src} */
+    {
+        src: "/images/image-portrait-Hobbie/lapm-photographe-professionnelle-portrait-Hobbie-fils-et-mere-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnelle - portrait Hobbie - fils et mère - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesPortraitHobbieControllerData[3].src} */
+    {
+        src: "/images/image-portrait-Hobbie/lapm-photographe-professionnelle-portrait-Hobbie-parents-bebe-l-atelier-photo-montpellier.jpeg",
+        alt: "LAPM - photographe professionnelle - portrait Hobbie - parents et bébé - L'Atelier Photo Montpellier",
+    },
+
+
+
+    /* Tableau 2 : PortraitHobbieComponentB */
+    /* {ImagesPortraitHobbieControllerData[4].src} */
+    {
+        src: "/images/image-portrait-Hobbie/lapm-photo-professionnel-portrait-Hobbie-intergenerationnel-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photo professionnel - portrait Hobbie intergénérationnel - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesPortraitHobbieControllerData[5].src} */
+    {
+        src: "/images/image-portrait-Hobbie/",
+        alt: "",
+    },
+    /* {ImagesPortraitHobbieControllerData[6].src} */
+    {
+        src: "/images/image-portrait-Hobbie/",
+        alt: "",
+    },
+    /* {ImagesPortraitHobbieControllerData[7].src} */
+    {
+        src: "/images/image-portrait-Hobbie/lapm-photographie-professionnelle-portrait-Hobbie-parent-enfants-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographie professionnelle - portrait Hobbie - parent et enfants - L'Atelier Photo Montpellier",
+    },
+];
+
+export default ImagesPortraitHobbieControllerData;
