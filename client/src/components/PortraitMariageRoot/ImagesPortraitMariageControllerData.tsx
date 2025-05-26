8d209132d03027e4ae05954c08956891f30692ee
Ryan DECIAN <ryan.decian.propgmail.com>
2025-05-26 22:03:20 +0200

    Push partiel de l'intégration des images de la page Mariage

--- client/src/components/PortraitMariageRoot/ImagesPortraitMariageControllerData.tsx
+++ client/src/components/PortraitMariageRoot/ImagesPortraitMariageControllerData.tsx
@@ -1,34 +1,9 @@
 import ImagesControllerDataType from "../../types/ImagesControllerData.type";
 
-import Image1 from "../../assets/Images/ImagePortraitMariage/Parc-ideal-pour-photo-de-mariage-Nimes.jpg";
-import Image2 from "../../assets/Images/ImagePortraitMariage/Maries-Parc-de-la-tour-Magne-a-Nimes.jpg";
-import Image3 from "../../assets/Images/ImagePortraitMariage/Florian-et-Elsa-enlaces-Nimes.jpg";
-import Image4 from "../../assets/Images/ImagePortraitMariage/Florian-et-Elsa-engagement.jpg";
-
-import Image5 from "../../assets/Images/ImagePortraitMariage/Maries-et-Sun-Glass.jpg";
-import Image6 from "../../assets/Images/ImagePortraitMariage/Les-discours-aux-maries.jpg";
-import Image7 from "../../assets/Images/ImagePortraitMariage/mariage-villeneuve-les-maguelones.jpg";
-import Image8 from "../../assets/Images/ImagePortraitMariage/Les-maries-en-plein-soleil.jpg";
-
-import Image9 from "../../assets/Images/ImagePortraitMariage/mariage-rome.jpg";
-import Image10 from "../../assets/Images/ImagePortraitMariage/mariage-pride-montpellier.jpg";
-import Image11 from "../../assets/Images/ImagePortraitMariage/mariage-photo-exterieur.jpg";
-import Image12 from "../../assets/Images/ImagePortraitMariage/mariage-photo-exterieur-rome.jpg";
-
-import Image13 from "../../assets/Images/ImagePortraitMariage/Flash-a-la-mairie-de-Montpellier.jpg";
-import Image14 from "../../assets/Images/ImagePortraitMariage/La-signature-des-maries.jpg";
-import Image15 from "../../assets/Images/ImagePortraitMariage/Le-bouquet-de-la-mariee.jpg";
-import Image16 from "../../assets/Images/ImagePortraitMariage/Les-bagues-sur-le-chapeau-de-la-mariee.jpg";
-
-import Image17 from "../../assets/Images/ImagePortraitMariage/Mariage-Parc-de-Gramont.jpg";
-import Image18 from "../../assets/Images/ImagePortraitMariage/Parc-de-la-Fontaine-Nimes.jpg";
-import Image19 from "../../assets/Images/ImagePortraitMariage/La-guiguette-de-villeneuve-les-Maguelones.jpg";
-import Image20 from "../../assets/Images/ImagePortraitMariage/maries-canisses-BW.jpg";
-
 /**
  * Compteur mot clés : 
  * 
- * photographe-professionnelle : 6/10
+ * photographe-professionnelle : 10/10
  * photo-professionnel : 5/5
  * photographie-professionnelle : 5/5
  */
@@ -37,23 +12,23 @@ const ImagePortraitMariageControllerData: ImagesControllerDataType[] = [
     /* Tableau 1 d'images : 4 images */
     /* {ImagePortraitMariageControllerData[0].src} */
     {
-        src: Image1,
-        alt: "",
+        src: "/images/image-portrait-mariage/lapm-photographe-professionnel-portrait-mariage-parc-ideal-nimes-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnel - portrait mariage - Parc Idéal Nîmes - L'Atelier Photo Montpellier",
     },
     /* {ImagePortraitMariageControllerData[1].src} */
     {
-        src: Image2,
-        alt: "",
+        src: "/images/image-portrait-mariage/lapm-photographe-professionnel-portrait-mariage-parc-tour-magne-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnel - portrait mariage - Parc Tour Magne - L'Atelier Photo Montpellier",
     },
     /* {ImagePortraitMariageControllerData[2].src} */
     {
-        src: Image3,
-        alt: "",
+        src: "/images/image-portrait-mariage/lapm-photo-professionnel-portrait-mariage-florian-et-elsa-enlace-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photo professionnel - portrait mariage - Florian et Elsa enlacés - L'Atelier Photo Montpellier",
     },
     /* {ImagePortraitMariageControllerData[3].src} */
     {
-        src: Image4,
-        alt: "",
+        src: "/images/image-portrait-mariage/lapm-photographe-professionnel-portrait-mariage-couple-engagement-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnel - portrait mariage - couple engagement - L'Atelier Photo Montpellier",
     },
 
 
@@ -61,23 +36,23 @@ const ImagePortraitMariageControllerData: ImagesControllerDataType[] = [
     /* Tableau 2 d'images : 4 images */
     /* {ImagePortraitMariageControllerData[4].src} */
     {
-        src: Image5,
-        alt: "",
+        src: "/images/image-portrait-mariage/lapm-photographe-professionnel-portrait-mariage-et-sun-glass-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnel - portrait mariage - et sun glass - L'Atelier Photo Montpellier",
     },
     /* {ImagePortraitMariageControllerData[5].src} */
     {
-        src: Image6,
-        alt: "",
+        src: "/images/image-portrait-mariage/lapm-photographe-professionnel-portrait-mariage-discours-aux-maries-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnel - portrait mariage - discours aux mariés - L'Atelier Photo Montpellier",
     },
     /* {ImagePortraitMariageControllerData[6].src} */
     {
-        src: Image7,
-        alt: "",
+        src: "/images/image-portrait-mariage/lapm-photo-professionnel-portrait-a-villeneuve-les-maguelones-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photo professionnel - portrait à Villeneuve-lès-Maguelone - L'Atelier Photo Montpellier",
     },
     /* {ImagePortraitMariageControllerData[7].src} */
     {
-        src: Image8,
-        alt: "",
+        src: "/images/image-portrait-mariage/lapm-photo-professionnel-portrait-mariage-maries-en-plein-soleil-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photo professionnel - portrait mariage - mariés en plein soleil - L'Atelier Photo Montpellier",
     },
 
 
@@ -85,23 +60,23 @@ const ImagePortraitMariageControllerData: ImagesControllerDataType[] = [
     /* Tableau 3 d'images : 4 images */
     /* {ImagePortraitMariageControllerData[8].src} */
     {
-        src: Image9,
-        alt: "",
+        src: "/images/image-portrait-mariage/lapm-photographe-professionnel-portrait-mariage-a-montpellier-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnel - portrait mariage à Montpellier - L'Atelier Photo Montpellier",
     },
     /* {ImagePortraitMariageControllerData[9].src} */
     {
-        src: Image10,
-        alt: "",
+        src: "/images/image-portrait-mariage/lapm-photographie-professionnelle-portrait-mariage-photo-exterieur-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographie professionnelle - portrait mariage - photo extérieur - L'Atelier Photo Montpellier",
     },
     /* {ImagePortraitMariageControllerData[10].src} */
     {
-        src: Image11,
-        alt: "",
+        src: "/images/image-portrait-mariage/lapm-photo-professionnel-portrait-mariage-mariage-photo-exterieur-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photo professionnel - portrait mariage - mariage photo extérieur - L'Atelier Photo Montpellier",
     },
     /* {ImagePortraitMariageControllerData[11].src} */
     {
-        src: Image12,
-        alt: "",
+        src: "/images/image-portrait-mariage/lapm-photographie-professionnelle-portrait-mariage-mariage-pride-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographie professionnelle - portrait mariage - mariage Pride - L'Atelier Photo Montpellier",
     },
 
 
@@ -109,22 +84,22 @@ const ImagePortraitMariageControllerData: ImagesControllerDataType[] = [
     /* Tableau 4 d'images : 4 images */
     /* {ImagePortraitMariageControllerData[12].src} */
     {
-        src: Image13,
+        src: "/images/image-portrait-mariage/",
         alt: "",
     },
     /* {ImagePortraitMariageControllerData[13].src} */
     {
-        src: Image14,
+        src: "/images/image-portrait-mariage/",
         alt: "",
     },
     /* {ImagePortraitMariageControllerData[14].src} */
     {
-        src: Image15,
+        src: "/images/image-portrait-mariage/",
         alt: "",
     },
     /* {ImagePortraitMariageControllerData[15].src} */
     {
-        src: Image16,
+        src: "/images/image-portrait-mariage/",
         alt: "",
     },
 
@@ -133,37 +108,22 @@ const ImagePortraitMariageControllerData: ImagesControllerDataType[] = [
     /* Tableau 5 d'images : 4 images */
     /* {ImagePortraitMariageControllerData[16].src} */
     {
-        src: Image17,
+        src: "/images/image-portrait-mariage/",
         alt: "",
     },
     /* {ImagePortraitMariageControllerData[17].src} */
     {
-        src: Image18,
+        src: "/images/image-portrait-mariage/",
         alt: "",
     },
     /* {ImagePortraitMariageControllerData[18].src} */
     {
-        src: Image19,
+        src: "/images/image-portrait-mariage/",
         alt: "",
     },
     /* {ImagePortraitMariageControllerData[19].src} */
     {
-        src: Image20,
-        alt: "",
-    },
-    /* {ImagePortraitMariageControllerData[20].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagePortraitMariageControllerData[21].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagePortraitMariageControllerData[22].src} */
-    {
-        src: "",
+        src: "/images/image-portrait-mariage/",
         alt: "",
     },
 ];
