eb39a1333c46b3eded8f138ded606872227e515d
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-27 19:57:28 +0200

    Néttoyage du controller data images de la page

--- client/src/components/PortraitSoloRoot/ImagesPortraitSoloControllerData.tsx
+++ client/src/components/PortraitSoloRoot/ImagesPortraitSoloControllerData.tsx
@@ -1,36 +1,34 @@
 import ImagesControllerDataType from "../../types/ImagesControllerData.type";
 
-import Image1 from "../../assets/Images/ImagePortraitSolo/Portrait-LifeStyle-Jardin.jpg";
-import Image2 from "../../assets/Images/ImagePortraitSolo/Portrait-Homme-Exterieur-Flash.jpg";
-import Image3 from "../../assets/Images/ImagePortraitSolo/Portrait-jeune-fille.jpg";
-import Image4 from "../../assets/Images/ImagePortraitSolo/Homme-LifeStyle.jpg";
-
-import Image5 from "../../assets/Images/ImagePortraitSolo/Studio-portrait-clair-obscur-homme.jpg";
-import Image6 from "../../assets/Images/ImagePortraitSolo/Portraits-Homme-domicile-Flash-NB.jpg";
-import Image7 from "../../assets/Images/ImagePortraitSolo/Life-style-exterieur-domicile.jpg";
-import Image8 from "../../assets/Images/ImagePortraitSolo/Parc-de-Fontcolombe-Flash.jpg";
+/**
+ * Compteur mot clés : 
+ * 
+ * photographe-professionnelle : 0/4
+ * photo-professionnel : 0/2
+ * photographie-professionnelle : 0/2
+ */
 
 const ImagesPortraitSoloControllerData: ImagesControllerDataType[] = [
     /* Tableau 1 : CorporateComponentA */
     /* {ImagesPortraitSoloControllerData[0].src} */
     {
-        src: Image1,
-        alt: "Portrait LifeStyle Jardin",
+        src: "/images/image-portrait-solo/",
+        alt: "",
     },
     /* {ImagesPortraitSoloControllerData[1].src} */
     {
-        src: Image2,
-        alt: "Portrait Homme Exterieur Flash",
+        src: "/images/image-portrait-solo/",
+        alt: "",
     },
     /* {ImagesPortraitSoloControllerData[2].src} */
     {
-        src: Image3,
-        alt: "Portrait jeune fille",
+        src: "/images/image-portrait-solo/",
+        alt: "",
     },
     /* {ImagesPortraitSoloControllerData[3].src} */
     {
-        src: Image4,
-        alt: "Homme LifeStyle",
+        src: "/images/image-portrait-solo/",
+        alt: "",
     },
 
 
@@ -38,94 +36,22 @@ const ImagesPortraitSoloControllerData: ImagesControllerDataType[] = [
     /* Tableau 2 : CorporateComponentB */
     /* {ImagesPortraitSoloControllerData[4].src} */
     {
-        src: Image5,
-        alt: "Studio portrait clair obscur homme",
+        src: "/images/image-portrait-solo/",
+        alt: "",
     },
     /* {ImagesPortraitSoloControllerData[5].src} */
     {
-        src: Image6,
-        alt: "Portraits Homme domicile Flash NB",
+        src: "/images/image-portrait-solo/",
+        alt: "",
     },
     /* {ImagesPortraitSoloControllerData[6].src} */
     {
-        src: Image7,
-        alt: "Life style exterieur domicile",
+        src: "/images/image-portrait-solo/",
+        alt: "",
     },
     /* {ImagesPortraitSoloControllerData[7].src} */
     {
-        src: Image8,
-        alt: "Parc de Fontcolombe Flash",
-    },
-
-
-
-    /* Tableau 3 : */
-    /* {ImagesPortraitSoloControllerData[8].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitSoloControllerData[9].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitSoloControllerData[10].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitSoloControllerData[11].src} */
-    {
-        src: "",
-        alt: "",
-    },
-
-
-
-    /* Tableau 4 : */
-    /* {ImagesPortraitSoloControllerData[12].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitSoloControllerData[13].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitSoloControllerData[14].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitSoloControllerData[15].src} */
-    {
-        src: "",
-        alt: "",
-    },
-
-
-
-    /* Tableau 5 : */
-    /* {ImagesPortraitSoloControllerData[16].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitSoloControllerData[17].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitSoloControllerData[18].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitSoloControllerData[19].src} */
-    {
-        src: "",
+        src: "/images/image-portrait-solo/",
         alt: "",
     },
 ];
