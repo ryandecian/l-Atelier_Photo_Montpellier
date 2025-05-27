d33ff438d4e854da228cbc4d1b604fa9f8807e73
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-27 20:13:30 +0200

    Intégration du premier tableau sans alt

--- client/src/components/PortraitSoloRoot/ImagesPortraitSoloControllerData.tsx
+++ client/src/components/PortraitSoloRoot/ImagesPortraitSoloControllerData.tsx
@@ -3,7 +3,7 @@ import ImagesControllerDataType from "../../types/ImagesControllerData.type";
 /**
  * Compteur mot clés : 
  * 
- * photographe-professionnel : 2/4
+ * photographe-professionnel : 4/4
  * photo-professionnel : 2/2
  * photographie-professionnelle : 2/2
  */
@@ -12,22 +12,22 @@ const ImagesPortraitSoloControllerData: ImagesControllerDataType[] = [
     /* Tableau 1 : CorporateComponentA */
     /* {ImagesPortraitSoloControllerData[0].src} */
     {
-        src: "/images/image-portrait-solo/",
+        src: "/images/image-portrait-solo/lapm-photographe-professionnel-portrait-solo-en-jardin-lifestyle-l-atelier-photo-montpellier.jpg",
         alt: "",
     },
     /* {ImagesPortraitSoloControllerData[1].src} */
     {
-        src: "/images/image-portrait-solo/",
+        src: "/images/image-portrait-solo/lapm-photographie-professionnelle-portrait-solo-homme-exterieur-l-atelier-photo-montpellier.jpg",
         alt: "",
     },
     /* {ImagesPortraitSoloControllerData[2].src} */
     {
-        src: "/images/image-portrait-solo/",
+        src: "/images/image-portrait-solo/lapm-photographie-professionnelle-portrait-solo-jeune-fille-grabels-l-atelier-photo-montpellier.jpg",
         alt: "",
     },
     /* {ImagesPortraitSoloControllerData[3].src} */
     {
-        src: "/images/image-portrait-solo/",
+        src: "/images/image-portrait-solo/lapm-photographe-professionnel-portrait-solo-homme-lifestyle-l-atelier-photo-montpellier.jpg",
         alt: "",
     },
 
