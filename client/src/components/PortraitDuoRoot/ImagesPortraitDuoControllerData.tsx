4d32a758cd3f2472a1d7258e83592ec51e03505c
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-28 19:45:06 +0200

    Nettoyage du controller data images de la page Duo

--- client/src/components/PortraitDuoRoot/ImagesPortraitDuoControllerData.tsx
+++ client/src/components/PortraitDuoRoot/ImagesPortraitDuoControllerData.tsx
@@ -1,35 +1,25 @@
 import ImagesControllerDataType from "../../types/ImagesControllerData.type";
 
-import Image1 from "../../assets/images/ImagePortraitDuo/portrait-couple-jeune-studio.jpg";
-import Image2 from "../../assets/images/ImagePortraitDuo/portrait-couple-studio-bis.jpg";
-import Image3 from "../../assets/images/ImagePortraitDuo/portrait-tandem-exterieur.jpg";
-import Image4 from "../../assets/images/ImagePortraitDuo/portrait-couple-montpellier.jpg";
-
-import Image5 from "../../assets/images/ImagePortraitDuo/couple-studio-detente.jpg";
-import Image6 from "../../assets/images/ImagePortraitDuo/duo-flash-studio-domicile.jpg";
-import Image7 from "../../assets/images/ImagePortraitDuo/portrait-duo-exterieur.jpg";
-import Image8 from "../../assets/images/ImagePortraitDuo/portrait-couple-studio-domicile.jpg";
-
 const ImagesPortraitDuoControllerData: ImagesControllerDataType[] = [
     /* Tableau 1 : PortraitDuoComponentA */
     /* {ImagesPortraitDuoControllerData[0].src} */
     {
-        src: Image1,
+        src: "",
         alt: "Portrait couple jeune studio",
     },
     /* {ImagesPortraitDuoControllerData[1].src} */
     {
-        src: Image2,
+        src: "",
         alt: "Portrait couple studio bis",
     },
     /* {ImagesPortraitDuoControllerData[2].src} */
     {
-        src: Image3,
+        src: "",
         alt: "Portrait tandem extérieur",
     },
     /* {ImagesPortraitDuoControllerData[3].src} */
     {
-        src: Image4,
+        src: "",
         alt: "Portrait couple Montpellier",
     },
 
@@ -38,95 +28,23 @@ const ImagesPortraitDuoControllerData: ImagesControllerDataType[] = [
     /* Tableau 2 : PortraitDuoComponentB */
     /* {ImagesPortraitDuoControllerData[4].src} */
     {
-        src: Image5,
+        src: "",
         alt: "Couple studio détente",
     },
     /* {ImagesPortraitDuoControllerData[5].src} */
     {
-        src: Image6,
+        src: "",
         alt: "Duo flash studio domicile",
     },
     /* {ImagesPortraitDuoControllerData[6].src} */
     {
-        src: Image7,
+        src: "",
         alt: "Portrait duo extérieur",
     },
     /* {ImagesPortraitDuoControllerData[7].src} */
     {
-        src: Image8,
+        src: "",
         alt: "Portrait couple studio domicile",
-    },
-
-
-
-    /* Tableau 3 : */
-    /* {ImagesPortraitDuoControllerData[8].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitDuoControllerData[9].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitDuoControllerData[10].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitDuoControllerData[11].src} */
-    {
-        src: "",
-        alt: "",
-    },
-
-
-
-    /* Tableau 4 : */
-    /* {ImagesPortraitDuoControllerData[12].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitDuoControllerData[13].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitDuoControllerData[14].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitDuoControllerData[15].src} */
-    {
-        src: "",
-        alt: "",
-    },
-
-
-
-    /* Tableau 5 : */
-    /* {ImagesPortraitDuoControllerData[16].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitDuoControllerData[17].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitDuoControllerData[18].src} */
-    {
-        src: "",
-        alt: "",
-    },
-    /* {ImagesPortraitDuoControllerData[19].src} */
-    {
-        src: "",
-        alt: "",
     },
 ];
 
