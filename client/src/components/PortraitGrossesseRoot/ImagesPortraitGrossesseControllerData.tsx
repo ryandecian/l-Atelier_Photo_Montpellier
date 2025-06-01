0aa64bd047f0ed29f7b96e18b6814c0c77bb2797
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-01 16:45:14 +0200

    Mise a jour du controller data image de la page Grossesse

--- client/src/components/PortraitGrossesseRoot/ImagesPortraitGrossesseControllerData.tsx
+++ client/src/components/PortraitGrossesseRoot/ImagesPortraitGrossesseControllerData.tsx
@@ -4,16 +4,16 @@ import ImagesControllerDataType from "../../types/ImagesControllerData.type";
  * Compteur mot clés : 
  * 
  * photographe-professionnel : 3/4
- * photo-professionnel : 0/2
- * photographie-professionnelle : 1/2
+ * photo-professionnel : 2/2
+ * photographie-professionnelle : 2/2
  */
 
 const ImagesPortraitGrossesseControllerData: ImagesControllerDataType[] = [
     /* Tableau 1 : PortraitGrossesseComponentA */
     /* {ImagesPortraitGrossesseControllerData[0].src} */
     {
-        src: "/images/image-portrait-grossesse/lapm-photographe-professionnelle-portrait-premieres-images-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photographe professionnelle - portrait grossesse - premières images - L'Atelier Photo Montpellier",
+        src: "/images/image-portrait-grossesse/lapm-photo-professionnel-portrait-grossesse-premieres-images-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photo professionnel - portrait grossesse - premières images - L'Atelier Photo Montpellier",
     },
     /* {ImagesPortraitGrossesseControllerData[1].src} */
     {
@@ -27,8 +27,8 @@ const ImagesPortraitGrossesseControllerData: ImagesControllerDataType[] = [
     },
     /* {ImagesPortraitGrossesseControllerData[3].src} */
     {
-        src: "/images/image-portrait-grossesse/lapm-photographe-professionnel-portrait-nouveaux-nes-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photographe professionnel - portrait grossesse - nouveaux nés - L'Atelier Photo Montpellier",
+        src: "/images/image-portrait-grossesse/lapm-photographe-professionnel-portrait-nouveau-ne-dans-berceau-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnel - portrait nouveau né - dans berceau - L'Atelier Photo Montpellier",
     },
 
 
@@ -36,23 +36,23 @@ const ImagesPortraitGrossesseControllerData: ImagesControllerDataType[] = [
     /* Tableau 2 : PortraitGrossesseComponentB */
     /* {ImagesPortraitGrossesseControllerData[4].src} */
     {
-        src: "/images/image-portrait-grossesse/",
-        alt: "",
+        src: "/images/image-portrait-grossesse/lapm-photo-professionnel-portrait-nouveau-ne-berceau-et-tableau-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photo professionnel - portrait nouveau né - berceau et tableau - L'Atelier Photo Montpellier",
     },
     /* {ImagesPortraitGrossesseControllerData[5].src} */
     {
-        src: "/images/image-portrait-grossesse/",
-        alt: "",
+        src: "/images/image-portrait-grossesse/lapm-photographie-professionnelle-portrait-nouveau-ne-pere-fils-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographie professionnelle - portrait nouveau né - père fils - L'Atelier Photo Montpellier",
     },
     /* {ImagesPortraitGrossesseControllerData[6].src} */
     {
-        src: "/images/image-portrait-grossesse/",
-        alt: "",
+        src: "/images/image-portrait-grossesse/lapm-photographe-professionnel-portrait-nouveau-ne-famille-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnel - portrait nouveau né - famille - L'Atelier Photo Montpellier",
     },
     /* {ImagesPortraitGrossesseControllerData[7].src} */
     {
-        src: "/images/image-portrait-grossesse/",
-        alt: "",
+        src: "/images/image-portrait-grossesse/lapm-photographe-professionnel-portrait-nouveau-ne-un-regard-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnel - portrait - nouveau né - un regard - L'Atelier Photo Montpellier",
     },
 ];
 
