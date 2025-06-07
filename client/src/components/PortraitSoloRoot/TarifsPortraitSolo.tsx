c92f71fd7552911573d362d06609439dc9bc2a2a
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-07 11:50:18 +0200

    Correction des carte Tarifs

--- client/src/components/PortraitSoloRoot/TarifsPortraitSolo.tsx
+++ client/src/components/PortraitSoloRoot/TarifsPortraitSolo.tsx
@@ -7,8 +7,8 @@ import {PrestationType, FormuleType, DataRootEmailType} from "../../types/Presta
 const TarifsPortraitSolo: PrestationType = {
     essentiel: {
         line1: { value1: 'Durée Séance', value2: '45min' },
-        line2: { value1: 'Extérieur', value2: 'Non' },
-        line3: { value1: 'Studio', value2: 'Oui' },
+        line2: { value1: 'Extérieur', value2: 'Oui' },
+        line3: { value1: 'Studio', value2: 'Non' },
         line4: { value1: 'Post Traitement', value2: 'oui' },
         line5: { value1: 'Galerie en Ligne', value2: 'Oui' },
         line6: { value1: 'Nombre de photos', value2: '5' },
@@ -17,7 +17,7 @@ const TarifsPortraitSolo: PrestationType = {
     confort: {
         line1: { value1: 'Durée Séance', value2: '1h00' },
         line2: { value1: 'Extérieur', value2: 'Oui' },
-        line3: { value1: 'Studio', value2: 'Oui' },
+        line3: { value1: 'Ou Studio', value2: 'Oui' },
         line4: { value1: 'Post Traitement', value2: 'oui' },
         line5: { value1: 'Galerie en Ligne', value2: 'non' },
         line6: { value1: 'Nombre de photos', value2: '15' },
@@ -26,7 +26,7 @@ const TarifsPortraitSolo: PrestationType = {
     premium: {
         line1: { value1: 'Durée Séance', value2: '02h00' },
         line2: { value1: 'Extérieur', value2: 'oui' },
-        line3: { value1: 'Studio', value2: 'oui' },
+        line3: { value1: 'Et Studio', value2: 'oui' },
         line4: { value1: 'Post Traitement', value2: 'oui' },
         line5: { value1: 'Galerie en Ligne', value2: 'oui' },
         line6: { value1: 'Nombre de photos', value2: '22' },
@@ -39,7 +39,8 @@ export {TarifsPortraitSolo};
 /* ---------- ----------- ---------- ---------- ---------- */
 /* Bloc logique 2 */
 
-const Service: string = "Réservation Portrait Solo"; /* Bloc a modifier si besoin */
+const Service: string = "Portrait Solo"; /* Bloc a modifier si besoin */
+const Reservation: string = `Réservation ${Service}`; /* Bloc a modifier si besoin */
 
 /* Nom des formules disponibles */
 const Formule: FormuleType = {
@@ -50,6 +51,14 @@ const Formule: FormuleType = {
 
 /* ---------- ----------- ---------- ---------- ---------- */
 /* Bloc logique 3 */
+
+const SegmentTexte = {
+    intro: "Bonjour Anne, %0D%0A %0D%0A Je souhaite réserver la formule",
+    contact: `%0D%0A Pouvez-vous me recontacter ?`,
+    cordialement: `%0D%0A %0D%0A Cordialement`,
+    info: `%0D%0A %0D%0A [Prénom - Téléphone éventuel]`,
+    ps: `%0D%0A P.S : (au besoin) Je vous donne ci-dessous quelques précisions utiles : %0D%0A [disponibilités, souhaits particuliers, objectif du portrait, autres...]`,
+}
 
 const dataRootEmail: DataRootEmailType = {
     essentiel: {
@@ -64,8 +73,8 @@ const dataRootEmail: DataRootEmailType = {
     },
     premium: {
         mailto: externalLinks.emailAnne,
-        subject: `${Service} - Formule ${Formule.premium}`,
-        body: `Bonjour Anne, %0D%0A %0D%0A Je souhaite réserver la formule ${Formule.premium} pour un portrait solo. %0D%0A Pouvez-vous de me recontacter. %0D%0A %0D%0A Cordialement,`
+        subject: `${Reservation} - Formule ${Formule.premium}`,
+        body: `${SegmentTexte.intro} ${Formule.premium} pour un ${Service}. ${SegmentTexte.contact} ${SegmentTexte.cordialement} ${SegmentTexte.info} ${SegmentTexte.ps}`
     },
 };
 
