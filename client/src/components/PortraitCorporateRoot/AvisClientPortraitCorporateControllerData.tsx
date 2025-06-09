32556583d207aadd7e9242e4a4a294427b408edd
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-09 16:11:41 +0200

    Mise a jour des avis client

--- client/src/components/PortraitCorporateRoot/AvisClientPortraitCorporateControllerData.tsx
+++ client/src/components/PortraitCorporateRoot/AvisClientPortraitCorporateControllerData.tsx
@@ -4,24 +4,24 @@ const AvisClientPortraitCorporateControllerData: AvisClientControllerDataType[] 
     /* Commentaire 1 */
     {
         id: 1,
-        nom: "Emma",
+        nom: "Béa BOUTBOUL",
         note: 5,
         commentaire: "Test",
-        date: "14 septembre 2024",
+        date: "06 mai 2025",
     },
     {
         id: 2,
-        nom: "Marie Pierre",
+        nom: "Hugo MANIAN",
         note: 5,
         commentaire: "Test",
-        date: "16 juillet 2022",
+        date: "14 avril 2025",
     },
     {
         id: 3,
-        nom: "Philippe",
+        nom: "Valérie M",
         note: 5,
         commentaire: "Test",
-        date: "16 juillet 2022",
+        date: "03 avril 2025",
     },
 ];
 
