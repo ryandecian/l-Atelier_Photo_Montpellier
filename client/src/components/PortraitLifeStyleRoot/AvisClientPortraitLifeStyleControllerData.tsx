381da400ebe5b2099178c873adebe68c93f35925
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-09 15:20:29 +0200

    Mise a ajour des avis client

--- client/src/components/PortraitLifeStyleRoot/AvisClientPortraitLifeStyleControllerData.tsx
+++ client/src/components/PortraitLifeStyleRoot/AvisClientPortraitLifeStyleControllerData.tsx
@@ -4,24 +4,24 @@ const AvisClientPortraitLifeStyleControllerData: AvisClientControllerDataType[] 
     /* Commentaire 1 */
     {
         id: 1,
-        nom: "Emma",
+        nom: "Océane P",
         note: 5,
-        commentaire: "",
-        date: "14 septembre 2024",
+        commentaire: "Pas évident d'être photographiée quand on n'est pas à l'aise devant l'objectif. Mais là ça a été tout seul et je suis super fière de mes photos !",
+        date: "03 juin 2025",
     },
     {
         id: 2,
-        nom: "Marie Pierre",
+        nom: "Philippe DUMEZE",
         note: 5,
-        commentaire: "",
-        date: "16 juillet 2022",
+        commentaire: "J’ai passé un super moment pendant le shooting, la qualité des photos est top très artistique, nous met à l’aise super facilement. Je recommande vraiment",
+        date: "08 mai 2025",
     },
     {
         id: 3,
-        nom: "Philippe",
-        note: 5,
-        commentaire: "",
-        date: "16 juillet 2022",
+        nom: "Abdel BOUCHIKI",
+        note: 4,
+        commentaire: "J'avais besoin pour faire mon book pour de la figuration. tout s'est bien passé RAS",
+        date: "13 avril 2025",
     },
 ];
 
