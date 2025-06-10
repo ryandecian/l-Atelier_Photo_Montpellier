c7d1e3dcb98f1598158a811c24d41c123337c4b2
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-10 14:18:51 +0200

    Correction du composant Root

--- client/src/components/PortraitHobbyRoot/PortraitHobbyRoot.tsx
+++ client/src/components/PortraitHobbyRoot/PortraitHobbyRoot.tsx
@@ -2,19 +2,19 @@ import style from "../StyleRootComponent.module.css";
 import useScrollToHash from "../../hook/useScrollToHash";
 
 import GeneratorCardAvisClient from "../GeneratorCardAvisClient/GeneratorCardAvisClient";
-import AvisClientPortraitHobbieControllerData from "./AvisClientPortraitHobbyControllerData";
+import AvisClientPortraitHobbyControllerData from "./AvisClientPortraitHobbyControllerData";
 
 import TarifCardRoot from "../TarifsCard/TarifsCardRoot";
-import { TarifsPortraitHobbie, MailtoLinkPortraitHobbie } from "./TarifsPortraitHobby";
+import { TarifsPortraitHobby, MailtoLinkPortraitHobby } from "./TarifsPortraitHobby";
 
-import PortraitHobbieRootComponentA from "./ComponentPortraitHobbyRoot/PortraitHobbyRootComponentA/PortraitHobbyRootComponentA";
-import PortraitHobbieRootComponentB from "./ComponentPortraitHobbyRoot/PortraitHobbyRootComponentB/PortraitHobbyRootComponentB";
+import PortraitHobbyRootComponentA from "./ComponentPortraitHobbyRoot/PortraitHobbyRootComponentA/PortraitHobbyRootComponentA";
+import PortraitHobbyRootComponentB from "./ComponentPortraitHobbyRoot/PortraitHobbyRootComponentB/PortraitHobbyRootComponentB";
 
-function PortraitHobbieRoot() {
+function PortraitHobbyRoot() {
     useScrollToHash() /* Utilisation du hook pour gérer le scroll vers les sections avec des IDs */
     
     return (
-        <section className={`PortraitHobbieRoot ${style.ContainerRootRacine}`}>
+        <section className={`PortraitHobbyRoot ${style.ContainerRootRacine}`}>
             <header className={style.ContainerTitle}>
                 <h1 className={style.TitleH1}>
                     {"Portrait Hobby : Mettez en lumière ce qui vous anime !"}
@@ -22,18 +22,18 @@ function PortraitHobbieRoot() {
             </header>
 
             {/* Container A */}
-            <PortraitHobbieRootComponentA />
+            <PortraitHobbyRootComponentA />
 
             {/* Container B */}
-            <PortraitHobbieRootComponentB />
+            <PortraitHobbyRootComponentB />
 
             {/* Container C : Carte tarifs */}
-            <TarifCardRoot id="tarifs-portrait-hobbie" tarifs={TarifsPortraitHobbie} mailtoLink={MailtoLinkPortraitHobbie} />
+            <TarifCardRoot id="tarifs-portrait-hobby" tarifs={TarifsPortraitHobby} mailtoLink={MailtoLinkPortraitHobby} />
 
             {/* Container D : Avis client */}
-            <GeneratorCardAvisClient avisClients={AvisClientPortraitHobbieControllerData}/>
+            <GeneratorCardAvisClient avisClients={AvisClientPortraitHobbyControllerData}/>
         </section>
     )
 }
 
-export default PortraitHobbieRoot;
+export default PortraitHobbyRoot;
