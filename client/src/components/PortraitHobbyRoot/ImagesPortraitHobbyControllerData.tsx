5f86ae79408c78252cdad008d3f0421751f4ff26
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-10 14:22:02 +0200

    Mise a jour du Controller data de la page Hobby

--- client/src/components/PortraitHobbyRoot/ImagesPortraitHobbyControllerData.tsx
+++ client/src/components/PortraitHobbyRoot/ImagesPortraitHobbyControllerData.tsx
@@ -8,52 +8,52 @@ import ImagesControllerDataType from "../../types/ImagesControllerData.type";
  * photographie-professionnelle : 2/2
  */
 
-const ImagesPortraitHobbieControllerData: ImagesControllerDataType[] = [
-    /* Tableau 1 : PortraitHobbieComponentA */
-    /* {ImagesPortraitHobbieControllerData[0].src} */
+const ImagesPortraitHobbyControllerData: ImagesControllerDataType[] = [
+    /* Tableau 1 : PortraitHobbyComponentA */
+    /* {ImagesPortraitHobbyControllerData[0].src} */
     {
-        src: "/images/image-portrait-hobbie/lapm-photographie-professionnelle-portrait-hobbie-peaky-blinders-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photographie professionnelle - portrait Hobbie - Peaky Blinders - L'Atelier Photo Montpellier",
+        src: "/images/image-portrait-hobby/lapm-photographie-professionnelle-portrait-hobby-peaky-blinders-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographie professionnelle - portrait Hobby - Peaky Blinders - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitHobbieControllerData[1].src} */
+    /* {ImagesPortraitHobbyControllerData[1].src} */
     {
-        src: "/images/image-portrait-hobbie/lapm-photographe-professionnelle-portrait-hobbie-far-west-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photographe professionnelle - portrait Hobbie - Far West - L'Atelier Photo Montpellier",
+        src: "/images/image-portrait-hobby/lapm-photographe-professionnelle-portrait-hobby-far-west-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnelle - portrait Hobby - Far West - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitHobbieControllerData[2].src} */
+    /* {ImagesPortraitHobbyControllerData[2].src} */
     {
-        src: "/images/image-portrait-hobbie/lapm-photo-professionnel-portrait-sport-foot-jongle-l-atelier-photo-montpellier.jpg",
+        src: "/images/image-portrait-hobby/lapm-photo-professionnel-portrait-sport-foot-jongle-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photo professionnel - portrait sport - foot jongle - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitHobbieControllerData[3].src} */
+    /* {ImagesPortraitHobbyControllerData[3].src} */
     {
-        src: "/images/image-portrait-hobbie/lapm-photo-professionnel-portrait-hobbie-gardienne-but-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photo professionnel - portrait Hobbie - gardienne de but - L'Atelier Photo Montpellier",
+        src: "/images/image-portrait-hobby/lapm-photo-professionnel-portrait-hobby-gardienne-but-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photo professionnel - portrait Hobby - gardienne de but - L'Atelier Photo Montpellier",
     },
 
 
 
-    /* Tableau 2 : PortraitHobbieComponentB */
-    /* {ImagesPortraitHobbieControllerData[4].src} */
+    /* Tableau 2 : PortraitHobbyComponentB */
+    /* {ImagesPortraitHobbyControllerData[4].src} */
     {
-        src: "/images/image-portrait-hobbie/lapm-photographe-professionnelle-portrait-hobbie-chiens-traineau-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photographe professionnelle - portrait Hobbie - chiens de traineau - L'Atelier Photo Montpellier",
+        src: "/images/image-portrait-hobby/lapm-photographe-professionnelle-portrait-hobby-chiens-traineau-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - photographe professionnelle - portrait Hobby - chiens de traineau - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitHobbieControllerData[5].src} */
+    /* {ImagesPortraitHobbyControllerData[5].src} */
     {
-        src: "/images/image-portrait-hobbie/lapm-photographie-professionnel-portrait-animaux-osteopathe-chiens-cani-cross-l-atelier-photo-montpellier.jpg",
+        src: "/images/image-portrait-hobby/lapm-photographie-professionnel-portrait-animaux-osteopathe-chiens-cani-cross-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographie professionnel - portrait animaux - ostéopathe chiens cani-cross - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitHobbieControllerData[6].src} */
+    /* {ImagesPortraitHobbyControllerData[6].src} */
     {
-        src: "/images/image-portrait-hobbie/lapm-photo-professionnel-portrait-animaux-maitre-chiens-complicite-l-atelier-photo-montpellier.jpg",
+        src: "/images/image-portrait-hobby/lapm-photo-professionnel-portrait-animaux-maitre-chiens-complicite-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photo professionnel - portrait animaux - maître chiens complicité - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitHobbieControllerData[7].src} */
+    /* {ImagesPortraitHobbyControllerData[7].src} */
     {
-        src: "/images/image-portrait-hobbie/lapm-photographie-professionnel-portrait-animaux-tandem-chiens-cani-cross-l-atelier-photo-montpellier.jpg",
+        src: "/images/image-portrait-hobby/lapm-photographie-professionnel-portrait-animaux-tandem-chiens-cani-cross-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographie professionnel - portrait animaux - tandem chiens cani-cross - L'Atelier Photo Montpellier",
     },
 ];
 
-export default ImagesPortraitHobbieControllerData;
+export default ImagesPortraitHobbyControllerData;
