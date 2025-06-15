fd2e845c14f7828858e081b954bea4ffed68e166
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-15 12:32:50 +0200

    Intégration du Helmet de la page Qui Suis Je à la racine de la page

--- client/src/pages/QuiSuisJePage/QuiSuisJePage.tsx
+++ client/src/pages/QuiSuisJePage/QuiSuisJePage.tsx
@@ -2,12 +2,12 @@ import NavRoot from "../../components/NavRoot/NavRoot";
 import style from "../stylePage.module.css";
 import QuiSuisJeRoot from "../../components/QuiSuisJeRoot/QuiSuisJeRoot";
 import FooterRoot from "../../components/FooterRoot/FooterRoot";
-// import Helmet_PortraitGrossesse_SEO from "../../seo/PortraitGrossesseSEO/Helmet_PortraitGrossesse.seo";
+import Helmet_QuiSuisJe_SEO from "../../seo/QuiSuisJeSEO/Helmet_QuiSuisJe.seo";
 
 function QuiSuisJePage() {
      return (
         <section className={`QuiSuisJePage ${style.Page}`}>
-            {/* <Helmet_PortraitGrossesse_SEO /> */}
+            <Helmet_QuiSuisJe_SEO />
             <header className={style.Header}>
                  <NavRoot />
             </header>
