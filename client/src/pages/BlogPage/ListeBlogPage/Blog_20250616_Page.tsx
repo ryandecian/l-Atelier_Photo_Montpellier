7433a3c2ea4dfb50633d69d2aeb0cefd6c6770ef
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-16 06:39:34 +0200

    Mise a jour du router avec création d'un composant spécial pour les blogs

---
+++ client/src/pages/BlogPage/ListeBlogPage/Blog_20250616_Page.tsx
@@ -0,0 +1,24 @@
+import style from "../stylePage.module.css";
+import NavRoot from "../../../components/NavRoot/NavRoot";
+import BlogRoot from "../../components/BlogRoot/BlogRoot";
+import FooterRoot from "../../../components/FooterRoot/FooterRoot";
+// import Helmet_Blog_SEO from "../../seo/BlogSEO/Helmet_Blog.seo";
+
+function Blog_20250616_Page() {
+     return (
+        <section className={`BlogPage ${style.Page}`}>
+            {/* <Helmet_Blog_SEO /> */}
+            <header className={style.Header}>
+                 <NavRoot />
+            </header>
+            <main className={style.Main}>
+                <BlogRoot />
+            </main>
+            <footer className={style.Footer}>
+                <FooterRoot />
+            </footer>
+        </section>
+    )
+}
+
+export default Blog_20250616_Page;
