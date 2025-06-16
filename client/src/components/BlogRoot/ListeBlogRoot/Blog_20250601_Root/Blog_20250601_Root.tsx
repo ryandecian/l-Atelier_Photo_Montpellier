4d2c55b8d9a1e378fda7cb40ad8f35035ad95ea8
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-16 14:59:45 +0200

    Push partiel

--- client/src/components/BlogRoot/ListeBlogRoot/Blog_20250601_Root/Blog_20250601_Root.tsx
+++ client/src/components/BlogRoot/ListeBlogRoot/Blog_20250601_Root/Blog_20250601_Root.tsx
@@ -42,7 +42,56 @@ function Blog_20250601_Root() {
                 MetaNameImg3={ImagesBlog_20250601_RootControllerData[2].alt}
                 img4={ImagesBlog_20250601_RootControllerData[3].src}
                 MetaNameImg4={ImagesBlog_20250601_RootControllerData[3].alt}
-            />
+                />
+
+            <p className={style.TextP4}>
+                La photographie de mariage en style reportage, aussi appelée "documentaire", consiste à raconter l’histoire 
+                du jour J sans poser ni diriger constamment les mariés ou leurs invités. C’est une approche discrète, qui 
+                capture les instants tels qu’ils se présentent, sur le vif.
+            </p>
+
+            <p className={style.TextP4}>
+                Contrairement à une séance photo traditionnelle où chaque image est construite, le style reportage 
+                privilégie l’émotion, l’imprévu, le mouvement, le naturel.
+            </p>
+
+            <h3 className={style.TitleNH3}>
+                Quelques caractéristiques du style reportage :
+            </h3>
+
+            <ul className={style.ContainerUl}>
+                <li className={style.TextLiP4}>
+                    Photos prises sur le moment, sans mise en scène
+                </li>
+                <li className={style.TextLiP4}>
+                    Émotions réelles, sans surjeu
+                </li>
+                <li className={style.TextLiP4}>
+                    Récit visuel chronologique de la journée
+                </li>
+                <li className={style.TextLiP4}>
+                    Lumière et ambiance naturelles
+                </li>
+                <li className={style.TextLiP4}>
+                    Interaction minimale du photographe
+                </li>
+                <br />
+            </ul>
+
+            <p className={style.TextP4}>
+                Ce style est idéal pour les couples qui souhaitent vivre pleinement leur journée sans se sentir observés 
+                ou mis en situation.
+            </p>
+
+            {/* ----------- ---------- ---------- ---------- ---------- */}
+
+            <h2 className={style.TitleNH2}>
+                {`Les avantages du style reportage pour votre mariage`}
+            </h2>
+
+            <h3 className={style.TitleNH3}>
+                {`Vivre pleinement l’instant présent`}
+            </h3>
         </section>
     );
 }
