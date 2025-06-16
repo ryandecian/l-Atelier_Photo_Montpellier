42d1255e67556f69e11deb2ce720c5912a1d9106
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-16 06:50:57 +0200

    Création partiel du blog 1

--- client/src/pages/BlogPage/ListeBlogPage/Blog_20250616_Page.tsx
+++ client/src/pages/BlogPage/ListeBlogPage/Blog_20250616_Page.tsx
@@ -1,18 +1,18 @@
 import style from "../stylePage.module.css";
 import NavRoot from "../../../components/NavRoot/NavRoot";
-import BlogRoot from "../../components/BlogRoot/BlogRoot";
+import Blog_20250616_Root from "../../../components/BlogRoot/ListeBlogRoot/Blog_20250616_Root";
 import FooterRoot from "../../../components/FooterRoot/FooterRoot";
 // import Helmet_Blog_SEO from "../../seo/BlogSEO/Helmet_Blog.seo";
 
 function Blog_20250616_Page() {
      return (
-        <section className={`BlogPage ${style.Page}`}>
+        <section className={`Blog_20260616_Page ${style.Page}`}>
             {/* <Helmet_Blog_SEO /> */}
             <header className={style.Header}>
                  <NavRoot />
             </header>
             <main className={style.Main}>
-                <BlogRoot />
+                <Blog_20250616_Root />
             </main>
             <footer className={style.Footer}>
                 <FooterRoot />
