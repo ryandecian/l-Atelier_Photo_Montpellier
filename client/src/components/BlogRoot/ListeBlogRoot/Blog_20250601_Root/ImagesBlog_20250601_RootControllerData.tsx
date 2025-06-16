7fdf8340b9e6ca9f93d879da37771c6c0ab5a4d1
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-16 15:52:47 +0200

    Push partiel des images

--- client/src/components/BlogRoot/ListeBlogRoot/Blog_20250601_Root/ImagesBlog_20250601_RootControllerData.tsx
+++ client/src/components/BlogRoot/ListeBlogRoot/Blog_20250601_Root/ImagesBlog_20250601_RootControllerData.tsx
@@ -13,46 +13,46 @@ const ImagesBlog_20250601_RootControllerData: ImagesControllerDataType[] = [
     /* {ImagesBlog_20250601_RootControllerData[0].src} */
     {
         src: "/images/image-blog/blog_20250601/lapm-photographie-professionnelle-blog-portrait-en-studio-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photographie professionnelle - portrait corporate - en studio - L'Atelier Photo Montpellier",
+        alt: "",
     },
     /* {ImagesBlog_20250601_RootControllerData[1].src} */
     {
         src: "/images/image-blog/blog_20250601/lapm-photo-professionnel-blog-portrait-lumiere-naturelle-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photo professionnel - portrait corporate - lumière naturelle - L'Atelier Photo Montpellier",
+        alt: "",
     },
     /* {ImagesBlog_20250601_RootControllerData[2].src} */
     {
         src: "/images/image-blog/blog_20250601/lapm-photographie-professionnelle-blog-portrait-interieur-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photographie professionnelle - portrait corporate - intérieur - L'Atelier Photo Montpellier",
+        alt: "",
     },
     /* {ImagesBlog_20250601_RootControllerData[3].src} */
     {
         src: "/images/image-blog/blog_20250601/lapm-photographe-professionnel-blog-portrait-pro-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photographe professionnel - portrait pro - L'Atelier Photo Montpellier",
+        alt: "",
     },
 
-    
+
 
     /* Tableau 2 : Blog_20250601_RootComponentB */
     /* {ImagesBlog_20250601_RootControllerData[4].src} */
     {
-        src: "/images/image-blog/blog_20250601/lapm-photographie-professionnelle-blog-portrait-en-studio-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photographie professionnelle - portrait corporate - en studio - L'Atelier Photo Montpellier",
+        src: "/images/image-blog/blog_20250601/lapm-photographie-professionnelle-blog-portrait-mariage-exterieur-l-atelier-photo-montpellier.jpg",
+        alt: "",
     },
     /* {ImagesBlog_20250601_RootControllerData[5].src} */
     {
-        src: "/images/image-blog/blog_20250601/lapm-photo-professionnel-blog-portrait-lumiere-naturelle-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photo professionnel - portrait corporate - lumière naturelle - L'Atelier Photo Montpellier",
+        src: "/images/image-blog/blog_20250601/lapm-photo-professionnel-blog-portrait-mariage-lumiere-naturelle-l-atelier-photo-montpellier.jpg",
+        alt: "",
     },
     /* {ImagesBlog_20250601_RootControllerData[6].src} */
     {
-        src: "/images/image-blog/blog_20250601/lapm-photographie-professionnelle-blog-portrait-interieur-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photographie professionnelle - portrait corporate - intérieur - L'Atelier Photo Montpellier",
+        src: "/images/image-blog/blog_20250601/lapm-photographie-professionnelle-blog-portrait-mariage-noir-et-blanc-l-atelier-photo-montpellier.jpg",
+        alt: "",
     },
     /* {ImagesBlog_20250601_RootControllerData[7].src} */
     {
-        src: "/images/image-blog/blog_20250601/lapm-photographe-professionnel-blog-portrait-pro-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - photographe professionnel - portrait pro - L'Atelier Photo Montpellier",
+        src: "/images/image-blog/blog_20250601/lapm-photographe-professionnel-blog-portrait-mariage-reportage-l-atelier-photo-montpellier.jpg",
+        alt: "",
     },
 ];
 
