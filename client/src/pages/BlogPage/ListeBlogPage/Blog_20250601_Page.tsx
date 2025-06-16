25f4472e2e3058ec5253401dc12e10ab4b00f18f
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-16 23:47:36 +0200

    Intégration des Helmet

--- client/src/pages/BlogPage/ListeBlogPage/Blog_20250601_Page.tsx
+++ client/src/pages/BlogPage/ListeBlogPage/Blog_20250601_Page.tsx
@@ -2,12 +2,12 @@ import style from "../../stylePage.module.css";
 import NavRoot from "../../../components/NavRoot/NavRoot";
 import Blog_20250601_Root from "../../../components/BlogRoot/ListeBlogRoot/Blog_20250601_Root/Blog_20250601_Root";
 import FooterRoot from "../../../components/FooterRoot/FooterRoot";
-// import Helmet_Blog_SEO from "../../seo/BlogSEO/Helmet_Blog.seo";
+import Helmet_Blog_20250601_SEO from "../../../seo/BlogSEO/ListeBlogRootSEO/Blog_20250601/Helmet_Blog_20250601.seo";
 
 function Blog_20250601_Page() {
      return (
         <section className={`Blog_20260616_Page ${style.Page}`}>
-            {/* <Helmet_Blog_SEO /> */}
+            <Helmet_Blog_20250601_SEO />
             <header className={style.Header}>
                  <NavRoot />
             </header>
