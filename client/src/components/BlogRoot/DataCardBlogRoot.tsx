843096cef9d8a9aac2ce34bb5c6bfe58aea88e40
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-16 06:25:40 +0200

    Mise a jour, test effectué,ajout de la fonction de trie par date automatiquement

--- client/src/components/BlogRoot/DataCardBlogRoot.tsx
+++ client/src/components/BlogRoot/DataCardBlogRoot.tsx
@@ -9,7 +9,16 @@ function DataCardPrestation() {
             id: "tarifs-portrait-mariage",
             title: "After day / trash the dress",
             description: "Prolongez la magie après le mariage.",
-            date: "",
+            date: "10/10/2023",
+            image: "/images/image-prestation-card/canette.jpg",
+        },
+        {
+            reactKey: 2,
+            path: ListDataRouter[13].path,
+            id: "tarifs-portrait-mariage",
+            title: "After day / trash the dress",
+            description: "Prolongez la magie après le mariage.",
+            date: "10/10/2025",
             image: "/images/image-prestation-card/canette.jpg",
         },
     ]
