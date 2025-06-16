4d51c358acd08a66d89a8edaca689b931f171867
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-16 07:49:29 +0200

    Push partiel de la page

--- client/src/components/BlogRoot/ListeBlogRoot/Blog_20250616_Root.tsx
+++ client/src/components/BlogRoot/ListeBlogRoot/Blog_20250616_Root.tsx
@@ -14,6 +14,8 @@ function Blog_20250616_Root() {
                 renforcer l’impact de votre candidature auprès des recruteurs.
             </p>
 
+            {/* ----------- ---------- ---------- ---------- ---------- */}
+            
             <h2 className={style.TitleH2}>
                 {`Pourquoi ajouter une photo professionnelle à son CV ?`}
             </h2>
@@ -33,8 +35,10 @@ function Blog_20250616_Root() {
                 facilement dans les processus de recrutement.
             </p>
 
+            {/* ----------- ---------- ---------- ---------- ---------- */}
+
             <h2 className={style.TitleH2}>
-                {`Comment un portrait pro influence la perception des recruteurs`}
+                {`Comment un portrait pro influence la perception des recruteurs ?`}
             </h2>
 
             <p className={style.TextP4}>
@@ -45,6 +49,111 @@ function Blog_20250616_Root() {
                 </span>
                 permet de :
             </p>
+
+            <ul className={style.ContainerUl}>
+                <li className={style.TextLiP4}>
+                    Humaniser le CV en mettant un visage sur un nom
+                </li>
+                <li className={style.TextLiP4}>
+                    Instaurer un climat de confiance dès le premier regard
+                </li>
+                <li className={style.TextLiP4}>
+                    Montrer que vous prenez votre démarche de recherche d’emploi au sérieux
+                </li>
+                <li className={style.TextLiP4}>
+                    Valoriser votre personnalité et votre potentiel
+                </li>
+            </ul>
+
+            <p className={style.TextP4}>
+                Le cerveau traite une image en moins de 7 secondes : soigner cette image est donc un véritable levier d’impact.
+            </p>
+
+            {/* ----------- ---------- ---------- ---------- ---------- */}
+
+            <h2 className={style.TitleH2}>
+                {`Les qualités essentielles d’un portrait professionnel pour CV`}
+            </h2>
+
+            <p className={style.TextP4}>
+                <span className={style.SpanBold}>
+                    {`1️⃣ Confiance en soi`}
+                </span>
+            </p>
+            <p className={style.TextP4}>
+                Un portrait où vous êtes bien préparé, bien habillé, et où vous dégagez une posture confiante envoie 
+                un message clair de professionnalisme et de dynamisme.
+            </p>
+
+            <p className={style.TextP4}>
+                <span className={style.SpanBold}>
+                    {`2️⃣ Authenticité`}
+                </span>
+            </p>
+            <p className={style.TextP4}>
+                Un portrait naturel, sans retouche excessive, inspirera davantage confiance qu’une photo trop figée 
+                ou artificielle.
+            </p>
+
+            <p className={style.TextP4}>
+                <span className={style.SpanBold}>
+                    {`3️⃣ Approche professionnelle`}
+                </span>
+            </p>
+            <p className={style.TextP4}>
+                Lumière maîtrisée, fond adapté, cadrage précis : tous ces détails renforcent l’image de sérieux et 
+                de rigueur que vous souhaitez projeter.
+            </p>
+
+            <p className={style.TextP4}>
+                <span className={style.SpanBold}>
+                    {`4️⃣ Sentiment de proximité`}
+                </span>
+            </p>
+            <p className={style.TextP4}>
+                Une bonne photo professionnelle crée une connexion immédiate avec le recruteur, qui perçoit un profil 
+                engageant et accessible.
+            </p>
+
+            {/* ----------- ---------- ---------- ---------- ---------- */}
+
+            <h2 className={style.TitleH2}>
+                {`Photo professionnelle VS photo amateur : quelles différences pour un CV ?`}
+            </h2>
+
+            <p className={style.TextP4}>
+                De nombreux candidats utilisent encore des photos informelles (soirée, événement, selfie). <br />
+                Or, ces images ne renvoient pas le même degré de professionnalisme. <br />
+                Une 
+                <span className={style.SpanBold}>
+                    {` photo prise par un photographe portrait pro `}
+                </span>
+                vous garantit :
+            </p>
+
+            <ul className={style.ContainerUl}>
+                <li className={style.TextLiP4}>
+                    Un cadrage et une lumière optimisés
+                </li>
+                <li className={style.TextLiP4}>
+                    Une posture valorisante
+                </li>
+                <li className={style.TextLiP4}>
+                    Un style en accord avec votre secteur professionnel
+                </li>
+                <li className={style.TextLiP4}>
+                    Une cohérence avec l’image que vous souhaitez transmettre
+                </li>
+            </ul>
+
+            <p className={style.TextP4}>
+                À Montpellier, dans un marché de l’emploi dynamique et concurrentiel, cette attention à l’image peut 
+                clairement faire la différence.
+            </p>
+
+            {/* ----------- ---------- ---------- ---------- ---------- */}
+
+            <h2 className={style.TitleH2}></h2>
         </section>
     );
 }
