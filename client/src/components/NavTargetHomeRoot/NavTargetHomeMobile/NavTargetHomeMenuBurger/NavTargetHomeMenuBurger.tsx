0d3a88b94b4e6fc56a8fca572b7a4148107fb0a0
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-06-22 00:46:26 +0200

    Ajout de la fonctionnalité supprimant ainsi le bouton d'ouverture du MenuBurger dans la Nav dit normal

--- client/src/components/NavTargetHomeRoot/NavTargetHomeMobile/NavTargetHomeMenuBurger/NavTargetHomeMenuBurger.tsx
+++ client/src/components/NavTargetHomeRoot/NavTargetHomeMobile/NavTargetHomeMenuBurger/NavTargetHomeMenuBurger.tsx
@@ -12,53 +12,53 @@ function NavTargetHomeMenuBurger() {
         }
     return (
         <>
-        <button className={`sideCloseNav ${active ? "active" : ""}`} type="button" onClick={funcActive}>Fermer</button>
-                 <div className={`sideNav ${active ? "active" : ""}`} id="mySideNav">
+            <button className={`sideCloseNav ${active ? "active" : ""}`} type="button" onClick={funcActive}>Fermer</button>
+            <div className={`sideNav ${active ? "active" : ""}`} id="mySideNav">
 
-                     <div>
-                         <button type="button" 
-                         id="closeBtn" 
-                         className="close"
-                         onClick={funcActive}>
-                            x
-                        </button>
-                     </div>
-
-                     {/*Le style de ce composant est directement géré par le module CSS :
-                     MenuNavRoot.module.css*/}
-                     <MenuNavTargetHomeRoot
-                         moduleMenuUl={style.MenuUlMobile}
-                         moduleMenuLi={style.MenuLiMobile}
-                         moduleMenuLink={style.MenuLinkMobile}
-                         moduleSubMenuUl={style.SubMenuUlMobile}
-                         moduleSubMenuLi={style.SubMenuLiMobile}
-                         moduleSubMenuLink={style.SubMenuLinkMobile}
-                         moduleSpanPortraits={style.SpanPortraitsMobile}
-
-                         moduleMenuLiTarget1={style.MenuLiTarget1Mobile}
-                         moduleMenuLiTarget2={style.MenuLiTarget2Mobile}
-                         moduleMenuLiTarget3={style.MenuLiTarget3Mobile}
-                         moduleMenuLiTarget4={style.MenuLiTarget4Mobile}
-                         moduleMenuLiTarget5={style.MenuLiTarget5Mobile}
-                         moduleMenuLiTarget6={style.MenuLiTarget6Mobile}
-
-                         moduleButtonSubMenu={style.ButtonSubMenuMobile}
-                     />
-
-
-                 </div>
-
-                {active === false &&
+                <div>
                     <button type="button" 
-                        id="openBtn"
-                        onClick={funcActive}>
-                        <span className="menuBurger">
-                            <span />
-                            <span />
-                            <span />
-                        </span>
+                    id="closeBtn" 
+                    className="close"
+                    onClick={funcActive}>
+                        x
                     </button>
-                }
+                </div>
+
+                {/*Le style de ce composant est directement géré par le module CSS :
+                MenuNavRoot.module.css*/}
+                <MenuNavTargetHomeRoot
+                    moduleMenuUl={style.MenuUlMobile}
+                    moduleMenuLi={style.MenuLiMobile}
+                    moduleMenuLink={style.MenuLinkMobile}
+                    moduleSubMenuUl={style.SubMenuUlMobile}
+                    moduleSubMenuLi={style.SubMenuLiMobile}
+                    moduleSubMenuLink={style.SubMenuLinkMobile}
+                    moduleSpanPortraits={style.SpanPortraitsMobile}
+
+                    moduleMenuLiTarget1={style.MenuLiTarget1Mobile}
+                    moduleMenuLiTarget2={style.MenuLiTarget2Mobile}
+                    moduleMenuLiTarget3={style.MenuLiTarget3Mobile}
+                    moduleMenuLiTarget4={style.MenuLiTarget4Mobile}
+                    moduleMenuLiTarget5={style.MenuLiTarget5Mobile}
+                    moduleMenuLiTarget6={style.MenuLiTarget6Mobile}
+
+                    moduleButtonSubMenu={style.ButtonSubMenuMobile}
+                />
+
+
+            </div>
+
+            {active === false &&
+                <button type="button" 
+                    id="openBtn"
+                    onClick={funcActive}>
+                    <span className="menuBurger">
+                        <span />
+                        <span />
+                        <span />
+                    </span>
+                </button>
+            }
         </>
     );
 }
