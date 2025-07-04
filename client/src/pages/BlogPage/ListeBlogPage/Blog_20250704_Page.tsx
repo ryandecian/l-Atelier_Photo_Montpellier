9cdbecc374f408cdf332abc03f81b128ea2ed2d4
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-04 11:15:32 +0200

    Mise a disposition du début de page

--- client/src/pages/BlogPage/ListeBlogPage/Blog_20250704_Page.tsx
+++ client/src/pages/BlogPage/ListeBlogPage/Blog_20250704_Page.tsx
@@ -1,13 +1,13 @@
 import style from "../../stylePage.module.css";
 import NavRoot from "../../../components/NavRoot/NavRoot";
-import Blog_20250704_Root from "../../../components/BlogRoot/ListeBlogRoot/Blog_20250616_Root/Blog_20250616_Root";
+import Blog_20250704_Root from "../../../components/BlogRoot/ListeBlogRoot/Blog_20250704_Root/Blog_20250704_Root";
 import FooterRoot from "../../../components/FooterRoot/FooterRoot";
-import Helmet_Blog_20250704_SEO from "../../../seo/BlogSEO/ListeBlogRootSEO/Blog_20250616/Helmet_Blog_20250601.seo";
+// import Helmet_Blog_20250704_SEO from "../../../seo/BlogSEO/ListeBlogRootSEO/Blog_20250704/Helmet_Blog_20250704.seo";
 
 function Blog_20250704_Page() {
      return (
         <section className={`Blog_20260616_Page ${style.Page}`}>
-            <Helmet_Blog_20250704_SEO />
+            {/* <Helmet_Blog_20250704_SEO /> */}
             <header className={style.Header}>
                  <NavRoot />
             </header>
