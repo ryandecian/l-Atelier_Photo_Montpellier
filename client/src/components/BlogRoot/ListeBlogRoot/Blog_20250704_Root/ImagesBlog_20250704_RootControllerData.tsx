3814ad9e0a25a345ad82bb103e4fac767be00579
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-04 12:47:01 +0200

    Push partiel de la page blog 3

--- client/src/components/BlogRoot/ListeBlogRoot/Blog_20250704_Root/ImagesBlog_20250704_RootControllerData.tsx
+++ client/src/components/BlogRoot/ListeBlogRoot/Blog_20250704_Root/ImagesBlog_20250704_RootControllerData.tsx
@@ -9,7 +9,7 @@ import ImagesControllerDataType from "../../../../types/ImagesControllerData.typ
  */
 
 const ImagesBlog_20250704_RootControllerData: ImagesControllerDataType[] = [
-    /* Tableau 1 : Blog_20250704_RootComponentA */
+    /* Tableau 1 */
     /* {ImagesBlog_20250704_RootControllerData[0].src} */
     {
         src: "/images/image-blog/blog_20250704/lapm-photo-portrait-feel-good-professionnel-montpellier-l-atelier-photo-montpellier.jpg",
@@ -29,6 +29,50 @@ const ImagesBlog_20250704_RootControllerData: ImagesControllerDataType[] = [
     {
         src: "/images/image-blog/blog_20250704/lapm-photographe-professionnel-portrait-corporate-chercheuse-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - Photographe professionnel à Montpellier - Portrait Corporate Chercheuse - L'Atelier Photo Montpellier",
+    },
+
+    /* Tableau 2 */
+    /* {ImagesBlog_20250704_RootControllerData[4].src} */
+    {
+        src: "/images/image-blog/blog_20250704/lapm-photographie-professionnelle-portrait-jeune-homme-fellgood-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographie professionnelle à Montpellier - Portrait Jeune Homme Feel Good - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20250704_RootControllerData[5].src} */
+    {
+        src: "/images/image-blog/blog_20250704/lapm-photographie-professionnelle-portrait-lifestyle-exterieur-flash-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographie professionnelle à Montpellier - Portrait Lifestyle Extérieur Flash - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20250704_RootControllerData[6].src} */
+    {
+        src: "/images/image-blog/blog_20250704/lapm-photo-professionnel-portrait-solo-femme-melancolique-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photo professionnel à Montpellier - Portrait Solo Femme Mélancolique - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20250704_RootControllerData[7].src} */
+    {
+        src: "/images/image-blog/blog_20250704/lapm-photographie-professionnelle-portrait-serie-peaky-blinders-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographie professionnelle à Montpellier - Portrait Série Peaky Blinders - L'Atelier Photo Montpellier",
+    },
+
+    /* Tableau 3 */
+    /* {ImagesBlog_20250704_RootControllerData[8].src} */
+    {
+        src: "/images/image-blog/blog_20250704/lapm-photographe-professionnel-portrait-duo-pere-fils-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographe professionnel à Montpellier - Portrait Duo Père Fils - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20250704_RootControllerData[9].src} */
+    {
+        src: "/images/image-blog/blog_20250704/lapm-photographie-professionnelle-portrait-flash-duo-cox-boy-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographie professionnelle à Montpellier - Portrait Flash Duo - Cox Boy - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20250704_RootControllerData[10].src} */
+    {
+        src: "/images/image-blog/blog_20250704/lapm-photo-professionnel-portrait-duo-tandem-exterieur-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photo professionnel à Montpellier - Portrait Duo Tandem Extérieur - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20250704_RootControllerData[11].src} */
+    {
+        src: "/images/image-blog/blog_20250704/lapm-photo-professionnel-portrait-famille-fratrie-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photo professionnel à Montpellier - Portrait Famille Fratrie - L'Atelier Photo Montpellier",
     },
 ];
 
