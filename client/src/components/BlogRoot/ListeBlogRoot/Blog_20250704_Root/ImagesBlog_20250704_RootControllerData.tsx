ad7c1d5f58fec157d7186f92fb6c98f2c8d4f997
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-04 11:59:30 +0200

    Intégration des 4 première images du blog 3

---
+++ client/src/components/BlogRoot/ListeBlogRoot/Blog_20250704_Root/ImagesBlog_20250704_RootControllerData.tsx
@@ -0,0 +1,35 @@
+import ImagesControllerDataType from "../../../../types/ImagesControllerData.type";
+
+/**
+ * Compteur mot clés : 
+ * 
+ * photographe-professionnel : 4/4
+ * photo-professionnel : 2/2
+ * photographie-professionnelle : 2/2
+ */
+
+const ImagesBlog_20250704_RootControllerData: ImagesControllerDataType[] = [
+    /* Tableau 1 : Blog_20250704_RootComponentA */
+    /* {ImagesBlog_20250704_RootControllerData[0].src} */
+    {
+        src: "/images/image-blog/blog_20250704/lapm-photo-portrait-feel-good-professionnel-montpellier-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographe professionnel à Montpellier - Portrait Feel Good - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20250704_RootControllerData[1].src} */
+    {
+        src: "/images/image-blog/blog_20250704/lapm-photographie-professionnelle-portrait-corporate-assistante-direction-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographie professionnelle à Montpellier - Portrait Corporate Assistante de Direction - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20250704_RootControllerData[2].src} */
+    {
+        src: "/images/image-blog/blog_20250704/lapm-photo-professionnel-portrait-corporate-commerciale-immo-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photo professionnel à Montpellier - Portrait Corporate Commerciale Immobilier - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20250704_RootControllerData[3].src} */
+    {
+        src: "/images/image-blog/blog_20250704/lapm-photographe-professionnel-portrait-corporate-chercheuse-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographe professionnel à Montpellier - Portrait Corporate Chercheuse - L'Atelier Photo Montpellier",
+    },
+];
+
+export default ImagesBlog_20250704_RootControllerData;
