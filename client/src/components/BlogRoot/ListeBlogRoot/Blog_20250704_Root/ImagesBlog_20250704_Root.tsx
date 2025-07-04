202672dee79d25edac0ec9f5d13eea617ccfb975
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-04 10:41:57 +0200

    Création du controller image pour l'article de blog et du composant root

---
+++ client/src/components/BlogRoot/ListeBlogRoot/Blog_20250704_Root/ImagesBlog_20250704_Root.tsx
@@ -0,0 +1,35 @@
+import ImagesControllerDataType from "../../../../types/ImagesControllerData.type";
+
+/**
+ * Compteur mot clés : 
+ * 
+ * photographe-professionnel : 4/4
+ * photo-professionnel : 2/2
+ * photographie-professionnelle : 2/2
+ */
+
+const ImagesBlog_20250704_RootControllerData: ImagesControllerDataType[] = [
+    /* Tableau 1 : Blog_20250704_RootComponentA */
+    /* {ImagesBlog_20250704_RootControllerData[0].src} */
+    {
+        src: "/images/image-blog/blog_20250704/",
+        alt: "",
+    },
+    /* {ImagesBlog_20250704_RootControllerData[1].src} */
+    {
+        src: "/images/image-blog/blog_20250704/",
+        alt: "",
+    },
+    /* {ImagesBlog_20250704_RootControllerData[2].src} */
+    {
+        src: "/images/image-blog/blog_20250704/",
+        alt: "",
+    },
+    /* {ImagesBlog_20250704_RootControllerData[3].src} */
+    {
+        src: "/images/image-blog/blog_20250704/",
+        alt: "",
+    },
+];
+
+export default ImagesBlog_20250704_RootControllerData;
