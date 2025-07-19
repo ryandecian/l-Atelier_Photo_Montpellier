b885470f4a6607ea370d3f9ad65e69f96da146a6
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-19 15:02:08 +0200

    Test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,9 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  console.log(`Test: ${import.meta.env.VITE_DOMAIN_CLIENT}`)
+  console.log(`Test 1: ${import.meta.env.VITE_DOMAIN_CLIENT}`)
+  console.log(`Test 2: ${import.meta.env.VITE_NODE_ENV}`)
+  console.log(`Test 3: ${import.meta.env.VITE_DOMAIN_API_SERVER}`)
   return (
     <div className="AppContainer">
       <Outlet />
