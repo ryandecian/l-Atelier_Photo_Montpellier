1460080ffb94d7f1bb9251297f18c7a4b5444647
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-22 15:18:28 +0200

    Mise a jour des Nav secteur Menu Burger

--- client/src/components/NavTargetHomeRoot/NavTargetHomeMobile/NavTargetHomeMenuBurger/NavTargetHomeMenuBurger.tsx
+++ client/src/components/NavTargetHomeRoot/NavTargetHomeMobile/NavTargetHomeMenuBurger/NavTargetHomeMenuBurger.tsx
@@ -13,14 +13,21 @@ function NavTargetHomeMenuBurger() {
         }
     return (
         <>
-            <button className={`${css.sideCloseNav} ${active ? css.active : ""}`} type="button" onClick={funcActive}>Fermer</button>
+            <button 
+                className={`${css.sideCloseNav} ${active ? css.active : ""} ${style.Button}`}
+                type="button" onClick={funcActive}
+            >
+                Fermer
+            </button>
+            
             <div className={`${css.sideNav} ${active ? css.active : ""} ${css.mySideNav}`}>
                 <div>
                     <button type="button" 
                         className={`${css.closeBtn} ${style.Button}`}
                         aria-label="Fermer le menu principal"
-                        onClick={funcActive}>
-                            x
+                        onClick={funcActive}
+                    >
+                        x
                     </button>
                 </div>
 
