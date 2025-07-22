d43282cea86418d738e892141652c27a038664ff
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-22 14:40:44 +0200

    Mise a jour des nav mobile avec restructuration de la sémentique

--- client/src/components/NavTargetHomeRoot/NavTargetHomeMobile/NavTargetHomeMobile.tsx
+++ client/src/components/NavTargetHomeRoot/NavTargetHomeMobile/NavTargetHomeMobile.tsx
@@ -1,26 +1,19 @@
-import style from "./NavTargetHomeMobile.module.css";
+import css from "./NavTargetHomeMobile.module.css";
 import { Link } from "react-router-dom"
 import NavTargetHomeMenuBurger from "./NavTargetHomeMenuBurger/NavTargetHomeMenuBurger";
-
-/* Import des Images*/
 
 function NavTargetHomeMobile() {
 
     return (
-        <>
-             <nav className={style.NavTargetHomeMobile}>
-                <article className={style.ContainerLogoNavTargetHomeMobile}>
-                    <p className={style.TitleNavTargetHomeMobile}>
-                        <Link to="/">
-                            l'Atelier Photo Montpellier
-                        </Link>
-                    </p>
-                </article>
-
-                 <NavTargetHomeMenuBurger />
-
-             </nav>
-        </>
+        <nav className={css.NavTargetHomeMobile}>
+            <div className={css.ContainerLogoNavTargetHomeMobile}>
+                <Link to="/">
+                    l'Atelier Photo Montpellier
+                </Link>
+            </div>
+            
+            <NavTargetHomeMenuBurger />
+        </nav>
     )
 }
 export default NavTargetHomeMobile;
