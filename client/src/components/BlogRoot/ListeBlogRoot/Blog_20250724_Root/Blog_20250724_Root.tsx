93f0e82740dbb6659a110274513985c185ca3470
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-23 19:12:34 +0200

    Push partiel de la page Blog 4

--- client/src/components/BlogRoot/ListeBlogRoot/Blog_20250724_Root/Blog_20250724_Root.tsx
+++ client/src/components/BlogRoot/ListeBlogRoot/Blog_20250724_Root/Blog_20250724_Root.tsx
@@ -1,10 +1,11 @@
 import style from "../../../StyleRootComponent.module.css";
+import css from "./Blog_20250724_Root.module.css";
 // import ListDataRouter from "../../../../router/router";
 // import { Link } from "react-router-dom";
 import useScrollToHash from "../../../../hook/useScrollToHash";
 
-// import ImagesBlog_20250724_RootControllerData from "./ImagesBlog_20250724_RootControllerData";
-// import PremiumCardImgSudoRoot from "../../../PremiumCardImgSudoRoot/PremiumCardImgSudoRoot";
+import ImagesBlog_20250724_RootControllerData from "./ImagesBlog_20250724_RootControllerData";
+import PremiumCardImgSudoRoot from "../../../PremiumCardImgSudoRoot/PremiumCardImgSudoRoot";
 
 function Blog_20250724_Root() {
     useScrollToHash(); // Hook pour gérer le scroll vers les ancres
@@ -18,8 +19,48 @@ function Blog_20250724_Root() {
             </header>
 
             <p className={style.TextP4}>
-                Découvrez pourquoi un portrait lifestyle soigné peut transformer votre image, renforcer votre estime de 
-                vous-même et offrir des souvenirs inoubliables à partager avec votre famille ou sur vos réseaux sociaux.
+                Il y a quelque chose de suspendu dans le temps quand une femme attend un enfant.
+                Ce n’est pas seulement un ventre qui s’arrondit, c’est un corps qui s’adapte, une énergie qui se 
+                transforme, une nouvelle vie qui pousse doucement à l’intérieur.
+            </p>
+
+            <p className={style.TextP4}>
+                Et, un jour, on se demande : <br />
+                <span className={style.SpanBold}>
+                    {`“Et si je gardais une trace de ce moment-là ?”`}
+                </span>
+            </p>
+
+            <p className={style.TextP4}>
+                Pas seulement une photo. Une sensation. Un souvenir à transmettre. Un fragment de ce que l’on est devenue 
+                à travers la maternité.
+            </p>
+
+            <h2 className={style.TitleH2}>
+                {`Shooting grossesse : pourquoi choisir la période entre 28 et 34 semaines`}
+            </h2>
+
+            <div className={css.ContainerImg}>
+                <PremiumCardImgSudoRoot
+                    src={ImagesBlog_20250724_RootControllerData[0].src}
+                    alt={ImagesBlog_20250724_RootControllerData[0].alt}
+                    className={css.Img1}
+                />
+
+                <PremiumCardImgSudoRoot
+                    src={ImagesBlog_20250724_RootControllerData[1].src}
+                    alt={ImagesBlog_20250724_RootControllerData[1].alt}
+                    className={css.Img2}
+                />
+            </div>
+
+            <p className={style.TextP4}>
+                La question revient souvent. <br />
+                <span className={style.SpanBold}>
+                    {`Le bon moment se situe entre la 28e et la 34e semaine de grossesse. `}
+                </span>
+                Ce n’est pas une règle absolue, mais plutôt une observation partagée par les photographes spécialisés et de 
+                nombreuses futures mamans.
             </p>
         </section>
     )
