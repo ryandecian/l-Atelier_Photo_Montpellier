ede7b1cb50e11dda1fe4d339f3f81385c35a3ffc
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-23 18:14:41 +0200

    Ajout des images de l'article de blog et création du controller data image

--- client/src/components/BlogRoot/ListeBlogRoot/Blog_20250724_Root/ImagesBlog_20250724_RootControllerData.tsx
+++ client/src/components/BlogRoot/ListeBlogRoot/Blog_20250724_Root/ImagesBlog_20250724_RootControllerData.tsx
@@ -1,23 +1,15 @@
 import ImagesControllerDataType from "../../../../types/ImagesControllerData.type";
 
-/**
- * Compteur mot clés : 
- * 
- * photographe-professionnel : 4/4
- * photo-professionnel : 2/2
- * photographie-professionnelle : 2/2
-*/
-
 const ImagesBlog_20250724_RootControllerData: ImagesControllerDataType[] = [
     /* {ImagesBlog_20250724_RootControllerData[0].src} */
     {
-        src: "/images/image-blog/blog_20250724/lapm-photo-portrait-feel-good-professionnel-montpellier-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - Photographe professionnel à Montpellier - Portrait Feel Good - L'Atelier Photo Montpellier",
+        src: "/images/image-blog/blog_20250724/lapm-portrait-grossesse-photographe-photo-grossesse-moment-blog-20250724-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Portrait grossesse - Photographe photo grossesse - Moment - Blog 20250724 - L'Atelier Photo Montpellier",
     },
     /* {ImagesBlog_20250724_RootControllerData[1].src} */
     {
-        src: "/images/image-blog/blog_20250724/",
-        alt: "",
+        src: "/images/image-blog/blog_20250724/lapm-photographe-grossesse-montpellier-contre-jour-blog-20250724-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographe grossesse Montpellier - Contre-jour - Blog 20250724 - L'Atelier Photo Montpellier",
     },
 ];
 
