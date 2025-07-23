bc5a3bbd2650429726706d2df00db061a881d091
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-23 17:55:07 +0200

    Création des composants Root

---
+++ client/src/components/BlogRoot/ListeBlogRoot/Blog_20250724_Root/ImagesBlog_20250724_RootControllerData.tsx
@@ -0,0 +1,24 @@
+import ImagesControllerDataType from "../../../../types/ImagesControllerData.type";
+
+/**
+ * Compteur mot clés : 
+ * 
+ * photographe-professionnel : 4/4
+ * photo-professionnel : 2/2
+ * photographie-professionnelle : 2/2
+*/
+
+const ImagesBlog_20250724_RootControllerData: ImagesControllerDataType[] = [
+    /* {ImagesBlog_20250724_RootControllerData[0].src} */
+    {
+        src: "/images/image-blog/blog_20250724/lapm-photo-portrait-feel-good-professionnel-montpellier-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographe professionnel à Montpellier - Portrait Feel Good - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20250724_RootControllerData[1].src} */
+    {
+        src: "/images/image-blog/blog_20250724/",
+        alt: "",
+    },
+];
+
+export default ImagesBlog_20250724_RootControllerData;
