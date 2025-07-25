7dab2364da317daa342d1bb31e2dbec006b0efc0
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-25 17:04:14 +0200

    Merge

--- client/src/pages/BlogPage/ListeBlogPage/Blog_20250724_Page.tsx
+++ client/src/pages/BlogPage/ListeBlogPage/Blog_20250724_Page.tsx
@@ -2,12 +2,12 @@ import style from "../../stylePage.module.css";
 import NavRoot from "../../../components/NavRoot/NavRoot";
 import Blog_20250724_Root from "../../../components/BlogRoot/ListeBlogRoot/Blog_20250724_Root/Blog_20250724_Root";
 import FooterRoot from "../../../components/FooterRoot/FooterRoot";
-// import Helmet_Blog_20250724_SEO from "../../../seo/BlogSEO/ListeBlogRootSEO/Blog_20250724/Helmet_Blog_20250724.seo";
+import Helmet_Blog_20250724_SEO from "../../../seo/BlogSEO/ListeBlogRootSEO/Blog_20250724/Helmet_Blog_20250724.seo";
 
 function Blog_20250724_Page() {
      return (
-        <section className={`Blog_20260616_Page ${style.Page}`}>
-            {/* <Helmet_Blog_20250724_SEO /> */}
+        <section className={`Blog_20250724_Page ${style.Page}`}>
+            <Helmet_Blog_20250724_SEO />
             <header id="top" className={style.Header}>
                  <NavRoot />
             </header>
