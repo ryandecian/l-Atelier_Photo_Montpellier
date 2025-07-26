c729e7931cd10fb359e9ec55a054cd384dfd1eeb
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-26 12:54:15 +0200

    Ajout de 2 nouveaux commentaires dans les corporates

--- client/src/components/PortraitCorporateRoot/AvisClientPortraitCorporateControllerData.tsx
+++ client/src/components/PortraitCorporateRoot/AvisClientPortraitCorporateControllerData.tsx
@@ -4,10 +4,10 @@ const AvisClientPortraitCorporateControllerData: AvisClientControllerDataType[] 
     /* Commentaire 1 */
     {
         id: 1,
-        nom: "Béa BOUTBOUL",
+        nom: "Valérie M",
         note: 5,
-        commentaire: "Je recommande pour avoir des photos de collaborateurs qui soient homogènes. Bonne organisation, mini studio monté en un temps records, tout le monde a été comptant de cette séance.",
-        date: "06/05/2025",
+        commentaire: "J'avais besoin de photos pour Linkdin et CV, rapide et professionnelle. J'en suis satisfaite.",
+        date: "03/04/2025",
     },
     {
         id: 2,
@@ -18,10 +18,24 @@ const AvisClientPortraitCorporateControllerData: AvisClientControllerDataType[] 
     },
     {
         id: 3,
-        nom: "Valérie M",
+        nom: "Béa BOUTBOUL",
         note: 5,
-        commentaire: "J'avais besoin de photos pour Linkdin et CV, rapide et professionnelle. J'en suis satisfaite.",
-        date: "03/04/2025",
+        commentaire: "Je recommande pour avoir des photos de collaborateurs qui soient homogènes. Bonne organisation, mini studio monté en un temps records, tout le monde a été comptant de cette séance.",
+        date: "06/05/2025",
+    },
+    {
+        id: 4,
+        nom: "Myriam QUINIOU",
+        note: 5,
+        commentaire: "Anne a su me mettre à l’aise tout de suite. Elle est efficace et le résultat correspondait complètement à ce que j’attendais, merci encore. Je recommande vivement l’Atelier Photo Montpellier car Anne met vraiment son exigence, sa créativité et son professionnalisme à votre service.",
+        date: "21/06/2025",
+    },
+    {
+        id: 5,
+        nom: "Caroline LEJARS",
+        note: 5,
+        commentaire: "L atelier photo de Grabels, c est bien plus qu un simple atelier photo! C est aussi un moment convivial, chaleureux, parfois très drôle, où l on se sent en confiance. Ma séance a permis de faire des photos professionnelles et quelques photos personnelles. Le résultat a fait l unanimité autant dans la famille, sur les réseaux, qu’au travail. Un immense merci à la photographe pour les photos et le moment partagé !",
+        date: "19/07/2025",
     },
 ];
 
