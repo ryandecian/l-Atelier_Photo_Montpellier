412992542b11fd4f5f25b942e9711569e7391a6a
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-26 13:19:06 +0200

    Ajout de nouveaux avis dans la page grossesse

--- client/src/components/PortraitGrossesseRoot/AvisClientPortraitGrossesseControllerData.tsx
+++ client/src/components/PortraitGrossesseRoot/AvisClientPortraitGrossesseControllerData.tsx
@@ -4,17 +4,24 @@ const AvisClientPortraitGrossesseControllerData: AvisClientControllerDataType[] 
     /* Commentaire 1 */
     {
         id: 1,
+        nom: "Lucie CABELLO",
+        note: 5,
+        commentaire: "J'ai eu des photos de grossesse chez moi, en poussant les meubles on y est arrivé. Anne est quelqu'un de très patient et qui avait pas mal d'idées, et comme c'est une femme j'ai pu faire des photos dans un bain de lait sans que ça me dérange.",
+        date: "26/03/2025",
+    },
+    {
+        id: 2,
         nom: "Thomas R",
         note: 5,
         commentaire: "De chouettes photos de couple et enfant ! J'en referai d'autres avec Anne car elle nous a mis dans une bonne ambiance détendue sans pression, j'ai apprécié !",
         date: "19/04/2025",
     },
     {
-        id: 2,
-        nom: "Lucie CABELLO",
+        id: 3,
+        nom: "Vanessa GROGNIET",
         note: 5,
-        commentaire: "J'ai eu des photos de grossesse chez moi, en poussant les meubles on y est arrivé. Anne est quelqu'un de très patient et qui avait pas mal d'idées, et comme c'est une femme j'ai pu faire des photos dans un bain de lait sans que ça me dérange.",
-        date: "26/03/2025",
+        commentaire: "Une expérience au top du top !! De magnifiques photos. Je recommande Anne à 100% pour vos futurs shooting 📸 ...",
+        date: "23/07/2025",
     },
 ];
 
