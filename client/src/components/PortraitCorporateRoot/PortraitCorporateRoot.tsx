e79ea974e82c96f8226942d78801652f03baccde
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-07-28 17:13:29 +0200

    retrait du texte test

--- client/src/components/PortraitCorporateRoot/PortraitCorporateRoot.tsx
+++ client/src/components/PortraitCorporateRoot/PortraitCorporateRoot.tsx
@@ -21,7 +21,6 @@ function PortraitCorporateRoot() {
                     {"Portrait Corporate : Valorisez votre image professionnelle !"}
                 </h1>
             </header>
-            <h4 className={style.TitleNH4}>Test</h4>
 
             {/* ---------- ---------- ---------- ---------- ---------- */}
 
