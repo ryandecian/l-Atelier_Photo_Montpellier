ca14b23e442df6f065e0d220c117a08fe88c5fd9
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-08-23 15:54:12 +0200

    Déplacement de la racine de la page Portrait Mariage

--- client/src/pages/Public/PortraitMariagePage/PortraitMariagePage.tsx
+++ client/src/pages/Public/PortraitMariagePage/PortraitMariagePage.tsx
@@ -1,9 +1,9 @@
-import NavRoot from "../../components/NavRoot/NavRoot";
-import style from "../stylePage.module.css";
-import PortraitMariageRoot from "../../components/PortraitMariageRoot/MariageRoot";
-import FooterRoot from "../../components/FooterRoot/FooterRoot";
-import Helmet_PortraitMariage_SEO from "../../seo/PortraitMariageSEO/Helmet_PortraitMariage.seo";
-import useScrollToHash from "../../hook/useScrollToHash";
+import style from "../../stylePage.module.css";
+import NavRoot from "../../../components/NavRoot/NavRoot";
+import PortraitMariageRoot from "../../../components/PortraitMariageRoot/MariageRoot";
+import FooterRoot from "../../../components/FooterRoot/FooterRoot";
+import Helmet_PortraitMariage_SEO from "../../../seo/PortraitMariageSEO/Helmet_PortraitMariage.seo";
+import useScrollToHash from "../../../hook/useScrollToHash";
 
 function PortraitMariagePage() {
     useScrollToHash() /* Utilisation du hook pour gérer le scroll vers les sections avec des IDs */
