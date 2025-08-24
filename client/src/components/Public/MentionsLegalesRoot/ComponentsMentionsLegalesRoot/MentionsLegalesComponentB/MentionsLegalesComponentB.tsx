d3ad137b042785d2287a50c43125c2776848f973
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-08-24 10:18:54 +0200

    Refactorisation de la page Mention Legales

--- client/src/components/Public/MentionsLegalesRoot/ComponentsMentionsLegalesRoot/MentionsLegalesComponentB/MentionsLegalesComponentB.tsx
+++ client/src/components/Public/MentionsLegalesRoot/ComponentsMentionsLegalesRoot/MentionsLegalesComponentB/MentionsLegalesComponentB.tsx
@@ -1,5 +1,5 @@
-import style from "../../../StyleRootComponent.module.css";
-import externalLinks from "../../../../constants/externalLinks";
+import style from "../../../../StyleRootComponent.module.css";
+import externalLinks from "../../../../../constants/externalLinks";
 
 function MentionsLegalesComponentB() {
     return (
