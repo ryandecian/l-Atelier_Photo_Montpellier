56fdef19367b437b8b996d32a4ee173bdd27e1d2
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-08-30 23:02:04 +0200

    Suite renommage 3

--- client/src/seo/BlogSEO/JSON-LD_Breadcrumb_Blog.schema.seo.ts
+++ client/src/seo/BlogSEO/JSON-LD_Breadcrumb_Blog.schema.seo.ts
@@ -1,7 +1,7 @@
 import DataSEORoots from "../DataSEORoot.seo";
 import DataSEORootType from "../../types/DataSEORoot.type";
 
-import DataSEO_Blogs from "./DataSEO_Blog.seo";
+import DataSEO_Blogs from "./DataSEO_Blog.data.seo";
 import DataSEOTargetOneType from "../../types/DataSEOTargetOne.type";
 
 function JSON_LD_Breadcrumb_Blog_Schema_SEO() : string {
