3164e4d0c8453b853c0b800775072869460c9ac3
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-08-30 22:58:54 +0200

    Renommage des composant SEO

--- client/src/seo/ContactSEO/Helmet_Contact.helmet.seo.tsx
+++ client/src/seo/ContactSEO/Helmet_Contact.helmet.seo.tsx
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet-async";
 import DataSEOHelmetType from "../../types/DataSEOHelmet";
 
 import DataSEORoots from "../DataSEORoot.seo";
-import DataSEO_Contacts from "./DataSEO_Contact.seo";
+import DataSEO_Contacts from "./DataSEO_Contact.data.seo";
 
 import DataSEORootType from "../../types/DataSEORoot.type";
 import DataSEOTargetOneType from "../../types/DataSEOTargetOne.type";
