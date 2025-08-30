343ad2f1f31160c0fd06544edc818031ab0d84c8
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-08-30 23:10:45 +0200

    Suite renommage 11

--- client/src/seo/PortraitCorporateSEO/Helmet_PortraitCorporate.helmet.seo.tsx
+++ client/src/seo/PortraitCorporateSEO/Helmet_PortraitCorporate.helmet.seo.tsx
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet-async";
 import DataSEOHelmetType from "../../types/DataSEOHelmet";
 
 import DataSEORoots from "../DataSEORoot.seo";
-import DataSEO_PortraitCorporates from "./DataSEO_PortraitCorporate.seo";
+import DataSEO_PortraitCorporates from "./DataSEO_PortraitCorporate.data.seo";
 
 import DataSEORootType from "../../types/DataSEORoot.type";
 import DataSEOTargetOneType from "../../types/DataSEOTargetOne.type";
