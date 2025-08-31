6454381b9e1ae5c82e71ffadc813e6250e9bc463
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-08-31 17:28:11 +0200

    Refactorisation du JWTTokenLAPM_utils

--- server/src/utils/jwtTokenLAPM.utils.ts
+++ server/src/utils/jwtTokenLAPM.utils.ts
@@ -5,7 +5,8 @@ import jwt from "jsonwebtoken";
 
 /* Import des Types : */
 import getAllUsers_type from "../types/user_type/getAllUsers.type";
-import payloadType from "../types/payloadType";
+import payloadJwtTokenServerLAPM_type from "../types/payload_JWT_type/payloadJwtTokenServerLAPM.type";
+import payloadJwtTokenClientLAPM_type from "../types/payload_JWT_type/payloadJwtTokenClientLAPM.type";
 
 /* Import des utils : */
 import { createDate_Number_utils } from "./createDate.utils";
@@ -25,7 +26,7 @@ async function createJwtTokenServerLAPM_utils(dataUser: getAllUsers_type): Promi
     const expiresIn: number = 60 * 60; /* 1 heure */
     const dateNow: number = await createDate_Number_utils(); /* Date actuelle en timestamp UNIX */
 
-    const payload_server: payloadType = {
+    const payload_server: payloadJwtTokenServerLAPM_type = {
         id: dataUser.id,
         email: dataUser.email,
         role: dataUser.role,
@@ -35,7 +36,7 @@ async function createJwtTokenServerLAPM_utils(dataUser: getAllUsers_type): Promi
     /* Création du token server */
     const jwtTokenServerLAPM = jwt.sign(payload_server, SECRET_KEY_TOKEN_SERVER, { expiresIn })
 
-    return jwtTokenServerLAPM;
+    return ( jwtTokenServerLAPM );
 }
 
 export { createJwtTokenServerLAPM_utils };
@@ -50,14 +51,13 @@ async function createJwtTokenClientLAPM_utils(dataUser: getAllUsers_type): Promi
     const expiresIn: number = 60 * 60; /* 1 heure */
     const dateNow: number = await createDate_Number_utils(); /* Date actuelle en timestamp UNIX */
 
-    const payload_client: payloadType = {
+    const payload_client: payloadJwtTokenClientLAPM_type = {
         id: dataUser.id,
         email: dataUser.email,
         firstname: dataUser.firstname,
         lastname: dataUser.lastname,
         address: dataUser.address,
         role: dataUser.role,
-        date_save: dataUser.date_save,
         iat: dateNow, /* ⏳ Date de création du token */
     }
 
@@ -70,7 +70,7 @@ async function createJwtTokenClientLAPM_utils(dataUser: getAllUsers_type): Promi
 export { createJwtTokenClientLAPM_utils };
 
 
-async function verifyJwtTokenLAPM_utils(req: Request): Promise<payloadType | boolean> {
+async function verifyJwtTokenLAPM_utils(req: Request): Promise<payloadJwtTokenServerLAPM_type | boolean> {
     try {
         if (!SECRET_KEY_TOKEN_SERVER) {
             return false
@@ -79,10 +79,10 @@ async function verifyJwtTokenLAPM_utils(req: Request): Promise<payloadType | boo
         // Vérification du token
         const token = req.cookies?.jwtTokenServerLAPM;
         if (!token) return false;
-    
-        const payload = jwt.verify(token, SECRET_KEY_TOKEN_SERVER) as payloadType;
-    
-        return payload;;
+
+        const payload = jwt.verify(token, SECRET_KEY_TOKEN_SERVER) as payloadJwtTokenServerLAPM_type;
+
+        return ( payload );
     }
     catch (error) {
         return false;
