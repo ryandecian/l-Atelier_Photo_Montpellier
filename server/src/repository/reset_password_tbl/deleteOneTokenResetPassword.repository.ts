bbe95008f0c4e31434f08cdce7472f215c82ce82
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-01 16:35:03 +0200

    Mise a jour de la documentation

--- server/src/repository/reset_password_tbl/deleteOneTokenResetPassword.repository.ts
+++ server/src/repository/reset_password_tbl/deleteOneTokenResetPassword.repository.ts
@@ -1,6 +1,7 @@
 import usePoolConnection from "../../database/config";
 import { ResultSetHeader, FieldPacket } from "mysql2";
 
+/* Suppression d'un unique token de réinitialisation ciblé */
 async function deleteOneTokenResetPassword_repository(token: string): Promise<number> {
     const [results]: [ResultSetHeader, FieldPacket[]] = await usePoolConnection.query<ResultSetHeader>(
         "DELETE FROM reset_password WHERE token = ?", [token]
