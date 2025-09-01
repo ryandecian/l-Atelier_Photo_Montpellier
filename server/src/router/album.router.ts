0c8324f02465cea9e08a26d40b865993efee034a
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-02 01:46:49 +0200

    Ajout de la protection de la nouvelle route

--- server/src/router/album.router.ts
+++ server/src/router/album.router.ts
@@ -4,13 +4,15 @@ import { Router } from "express";
 import getAllAlbums_controller from "../controllers/album_controller/getAllAlbums.controller";
 
 /* Import des Middlewares */
+import Verify_JWT_Middleware from "../middleware/JWT/Verify_JWT_Middleware";
+import isAdmin_Middleware from "../middleware/isAdmin/isAdmin_Middleware";
 
 const albumRouter = Router();
 
 /* Route 1 */
 /* Récupération de tous les albums */
 /* URI : /album */
-albumRouter.get("/", 
+albumRouter.get("/", Verify_JWT_Middleware, isAdmin_Middleware,
     getAllAlbums_controller
 );
 
