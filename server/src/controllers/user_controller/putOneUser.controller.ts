e2691ea00ae6811f982e698b54aa586a1fd1ea59
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-01 18:39:40 +0200

    Débug effectué, tout fonctionnne parfaitement. On peut merge sur Deploy pour un test en déploiement

--- server/src/controllers/user_controller/putOneUser.controller.ts
+++ server/src/controllers/user_controller/putOneUser.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import { ResultSetHeader } from "mysql2";
 
 /* Import des Repositories : */
+import verifyEmailById_repository from "../../repository/user_tbl/verifyEmailById.repository";
 import verifyEmailByEmail_repository from "../../repository/user_tbl/verifyEmailByEmail.repository";
 import { putOneUser_repository } from "../../repository/user_tbl/putOneUserById.repository";
 import { getOneUserById_repository } from "../../repository/user_tbl/getOneUserById.repository";
@@ -16,13 +17,25 @@ import getOneUserById_type from "../../types/user_type/getOneUserById.type";
 /* URI : /user */
 const putOneUser_controller = async (req: Request, res: Response) => {
     try {
-        /* Logique métier 1 : Vérification si l'email existe */
-        const dataUserDB: getAllUsers_type[] = await verifyEmailByEmail_repository(req.body.email);
+        /* Logique métier 1 : Vérification si l'utilisateur existe */
+        const dataUserDB: getAllUsers_type[] = await verifyEmailById_repository(req.body.id);
 
-        // Si l'email existe déjà dans la DB, on ne peut pas continuer.
-        if (dataUserDB.length > 0) { // Si c'est supérieur à 0, c'est que l'email existe déjà
-            res.status(409).json({ error: "Cet email est déjà utilisé. Veuillez en choisir un autre." });
+        if (dataUserDB.length === 0) { /* Si la fonction ne retourne rien, c'est que l'utilisateur n'existe pas */
+            res.status(404).json({ error: "Cet utilisateur n'existe pas." });
             return;
+        }
+        
+        /* Logique métier 2 : Vérification si l'email dans la requête est identique à l'email de l'utilisateur en DB */
+        /* Si l'email en DB est différent de celui de la requête c'est que l'admin veux modifier l'email utilisateur */
+        if (dataUserDB[0].email !== req.body.email) {
+            /* On vérifie alors que l'email demandé n'est pas déjà utilisé par un autre utilisateur */
+            const emailExists = await verifyEmailByEmail_repository(req.body.email);
+
+            // Si l'email existe déjà dans la DB, on ne peut pas continuer.
+            if (emailExists.length > 0) { // Si c'est supérieur à 0, c'est que l'email existe déjà
+                res.status(409).json({ error: "Cet email est déjà utilisé. Veuillez en choisir un autre." });
+                return;
+            }
         }
 
         /* Logique métier 2 : Modifier un utilisateur par son id */
