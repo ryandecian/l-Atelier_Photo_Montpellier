fa6013ec1c04694c522009aef0fb96f2023d063b
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-02 01:15:08 +0200

    Création du router albumRouter

---
+++ server/src/router/album.router.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+/* Import des Controllers */
+
+/* Import des Middlewares */
+
+const albumRouter = Router();
+
+/* Route 1 */
+/* Description */
+/* URI : /album */
+
+export default albumRouter;
