0d044116dbe0fa4bd7750a30d85ac9a9a63a09e9
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-10 22:25:10 +0200

    Refacto de getMeUser_controller

--- server/src/controllers/user_controller/getMeUser.controller.ts
+++ server/src/controllers/user_controller/getMeUser.controller.ts
@@ -11,18 +11,18 @@ import getOneUserById_type from "../../types/user_type/getOneUserById.type";
 /* URI : /user/me */
 const getMeUser_controller = async (req: Request, res: Response) => {
     try {
-        // Logique métier 1 : Recuperer l'utilisateur connecté
-        const userId = req.body.dataUser.id; // Récupérer l'ID de l'utilisateur à partir du token JWT
-        const dataUser: getOneUserById_type | null = await getOneUserById_repository(userId);
+        /* Logique métier 1 : Recuperer l'utilisateur connecté */
+            const userId = req.body.dataUser.id; // Récupérer l'ID de l'utilisateur à partir du token JWT
+            const dataUser: getOneUserById_type | null = await getOneUserById_repository(userId);
 
-        if (!dataUser) {
-            res.status(404).json({ error: "Aucun utilisateur trouvé" });
+            if (!dataUser) {
+                res.status(404).json({ error: "Aucun utilisateur trouvé" });
+                return;
+            }
+
+        /* Logique métier 2 : Envois des données de l'utilisateur */
+            res.status(200).json({ data: dataUser });
             return;
-        }
-
-        // Logique métier 2 : Envois des données de l'utilisateur
-        res.status(200).json({ data: dataUser });
-        return;
     } 
     catch (error) {
         res.status(500).json({ error: "Erreur interne serveur inconnue." });
