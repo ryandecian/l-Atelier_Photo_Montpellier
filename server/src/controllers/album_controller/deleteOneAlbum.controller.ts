743f56dbfa2edc25d26c3e5365f39ba4ccdc9921
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-10 22:11:40 +0200

    Refacto de deleteOneAlbum_controller

--- server/src/controllers/album_controller/deleteOneAlbum.controller.ts
+++ server/src/controllers/album_controller/deleteOneAlbum.controller.ts
@@ -10,16 +10,16 @@ import { deleteOneAlbumById_repository } from "../../repository/album_tbl/delete
 const deleteOneAlbum_controller = async (req: Request, res: Response) => {
     try {
         // Logique métier 1 : Suppression de l'album dans la DB
-        const deleteDataAlbum: ResultSetHeader = await deleteOneAlbumById_repository(req.body.id_album);
+            const deleteDataAlbum: ResultSetHeader = await deleteOneAlbumById_repository(req.body.id_album);
 
-        if (deleteDataAlbum.affectedRows === 0) {
-            res.status(404).json({ error: "Aucun album trouvé" });
-            return;
-        }
+            if (deleteDataAlbum.affectedRows === 0) {
+                res.status(404).json({ error: "Aucun album trouvé" });
+                return;
+            }
 
         // Logique métier 2 : Envois de la confirmation de la suppression
-        res.status(200).json({ message: "Suppression réussie" });
-        return;
+            res.status(200).json({ message: "Suppression réussie" });
+            return;
     }
     catch (error) {
         res.status(500).json({ error: "Erreur interne serveur inconnue." });
