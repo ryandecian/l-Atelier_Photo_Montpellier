e0eefa8a403dbc7b8a3a698ffe1ae2edbdbdf4f4
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-15 11:30:37 +0200

    Initialisation de la nouvelle page de blog

---
+++ client/src/components/Public/BlogRoot/ListeBlogRoot/Blog_20250917_Root/Blog_20250917.root.tsx
@@ -0,0 +1,15 @@
+import style from "../../../../StyleRootComponent.module.css";
+
+function Blog_20250917_Root() {
+    return (
+        <section className={`Blog_20250917_Root ${style.ContainerRootRacine}`}>
+            <header className={style.ContainerTitle}>
+                <h1 className={style.TitleH1}>
+                    Blog 20250917
+                </h1>
+            </header>
+        </section>
+    )
+}
+
+export default Blog_20250917_Root;
