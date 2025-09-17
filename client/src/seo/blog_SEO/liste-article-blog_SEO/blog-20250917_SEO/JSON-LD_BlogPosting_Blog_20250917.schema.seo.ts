3c2ef0f4d1fc5cf9c00ecb02b0821e487771b8a8
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-17 13:27:15 +0200

    Mise a jour du JSON_LD BlogPosting

--- client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250917_SEO/JSON-LD_BlogPosting_Blog_20250917.schema.seo.ts
+++ client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250917_SEO/JSON-LD_BlogPosting_Blog_20250917.schema.seo.ts
@@ -1,7 +1,7 @@
-import DataSEORoots from "../../../dataSEO_Root.data.seo";
+import { dataSEO_Root_data_SEO } from "../../../dataSEO_Root.data.seo";
 import DataSEORootType from "../../../../types/DataSEORoot.type";
 
-import DataSEO_Blog_20250724s from "./dataSEO_Blog_20250917.data.seo";
+import { dataSEO_Blog_20250917_data_SEO } from "./dataSEO_Blog_20250917.data.seo";
 import DataSEOTargetOneType from "../../../../types/DataSEOTargetOne.type";
 
 import DataCardBlogRoots from "../../../../components/Public/BlogRoot/DataCardBlogRoot";
@@ -11,46 +11,46 @@ import extractKeywordsFromDataSEO from "../../../../utils/extractKeywordsFromDat
 import { convertDateFrToISO_String } from "../../../../utils/convertDateFrToISO.utils";
 
 function JSON_LD_BlogPosting_Blog_20250724_Schema_SEO() : string {
-    const DataCardBlogRoot: CardBlogType[] = DataCardBlogRoots(); /* Récupération des données de la carte du blog */
-    const DataSEORoot: DataSEORootType = DataSEORoots(); /* Récupération des données SEO */
-    const DataSEO_Blog_20250724: DataSEOTargetOneType = DataSEO_Blog_20250724s(); /* Récupération des données SEO de la page */
+    const dataCardBlogRoot: CardBlogType[] = DataCardBlogRoots(); /* Récupération des données de la carte du blog */
+    const dataSEO_Root: DataSEORootType = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
+    const dataSEO_Blog_20250724: DataSEOTargetOneType = dataSEO_Blog_20250917_data_SEO(); /* Récupération des données SEO de la page */
 
     const JSON_LD = JSON.stringify({
-        "@context": DataSEORoot["@context"],
-        "@type": DataSEORoot["@type"].BlogPosting,
+        "@context": dataSEO_Root["@context"],
+        "@type": dataSEO_Root["@type"].BlogPosting,
 
         /* Identifiants & URL */
-        "@id": DataSEO_Blog_20250724.id_Service, /* @id SEO-friendly, correspond à l'URL de la page + mots clés */
+        "@id": dataSEO_Blog_20250724.id_Service, /* @id SEO-friendly, correspond à l'URL de la page + mots clés */
         "mainEntityOfPage": {
             "@type": "WebPage",
-            "@id": DataSEO_Blog_20250724.url,
+            "@id": dataSEO_Blog_20250724.url,
         },
-        "url": DataSEO_Blog_20250724.url,
+        "url": dataSEO_Blog_20250724.url,
 
         /* Métadonnées principales */
-        "headline": DataSEO_Blog_20250724.name_Service, /* Titre principal affiché par Google */
-        "description": DataSEO_Blog_20250724.description, /* Description de l'article de blog */
-        "keywords": extractKeywordsFromDataSEO(DataSEO_Blog_20250724.keywords), /* Extraction des mots clés */
+        "headline": dataSEO_Blog_20250724.name_Service, /* Titre principal affiché par Google */
+        "description": dataSEO_Blog_20250724.description, /* Description de l'article de blog */
+        "keywords": extractKeywordsFromDataSEO(dataSEO_Blog_20250724.keywords), /* Extraction des mots clés */
 
         /* Image principale */
         "image": {
             "@type": "ImageObject",
-            "url": DataSEO_Blog_20250724.img_JSON_LD, /* Lien URL public de l'image */
+            "url": dataSEO_Blog_20250724.img_JSON_LD, /* Lien URL public de l'image */
             "width": 1200,
             "height": 800
         },
 
         /* Dates */
-        "datePublished": convertDateFrToISO_String(DataCardBlogRoot[0].date), /* Date de publication au format ISO 8601 */
+        "datePublished": convertDateFrToISO_String(dataCardBlogRoot[0].date), /* Date de publication au format ISO 8601 */
 
         /* Auteur */
-        "author": DataSEORoot.author, /* Auteur de l'article, défini dans DataSEORoot */
+        "author": dataSEO_Root.author, /* Auteur de l'article, défini dans DataSEORoot */
 
         /* Éditeur */
-        "publisher": DataSEORoot.publisher, /* Éditeur de l'article, défini dans DataSEORoot */
+        "publisher": dataSEO_Root.publisher, /* Éditeur de l'article, défini dans DataSEORoot */
 
         /* Créatrice */
-        "creator": DataSEORoot.creator, /* Créatrice de l'article, défini dans DataSEORoot */
+        "creator": dataSEO_Root.creator, /* Créatrice de l'article, défini dans DataSEORoot */
     })
 
     return (
