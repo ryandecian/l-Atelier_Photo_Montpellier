66f656a1a8e43b62126f6a25560d6e237380881f
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-18 01:17:30 +0200

    Phase 2/2

--- client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250917_SEO/JSON-LD_BlogPosting_Blog_20250917.schema.seo.ts
+++ client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250917_SEO/JSON-LD_BlogPosting_Blog_20250917.schema.seo.ts
@@ -1,7 +1,7 @@
 import { dataSEO_Root_data_SEO } from "../../../dataSEO_Root.data.seo";
 import DataSEORootType from "../../../../types/DataSEORoot.type";
 
-import { dataSEO_Blog_20250917_data_SEO } from "./DdataSEO_Blog_20250917.data.seo";
+import { dataSEO_Blog_20250917_data_SEO } from "./dataSEO_Blog_20250917.data.seo";
 import DataSEOTargetOneType from "../../../../types/DataSEOTargetOne.type";
 
 import DataCardBlogRoots from "../../../../components/Public/BlogRoot/DataCardBlogRoot";
