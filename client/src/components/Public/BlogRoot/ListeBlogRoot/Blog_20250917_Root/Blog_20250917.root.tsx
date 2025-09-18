6b22bc0e83b92ed864262d39bd4e74bbf140c77c
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-18 17:58:49 +0200

    push partiel de la page

--- client/src/components/Public/BlogRoot/ListeBlogRoot/Blog_20250917_Root/Blog_20250917.root.tsx
+++ client/src/components/Public/BlogRoot/ListeBlogRoot/Blog_20250917_Root/Blog_20250917.root.tsx
@@ -5,9 +5,42 @@ function Blog_20250917_Root() {
         <section className={`Blog_20250917_Root ${style.ContainerRootRacine}`}>
             <header className={style.ContainerTitle}>
                 <h1 className={style.TitleH1}>
-                    Blog 20250917
+                    {`Pourquoi investir dans la vidéo et la photo pour votre entreprise ?`}
                 </h1>
             </header>
+
+            <h2 className={style.TitleH2}>
+                {`La photo et la vidéo événementielle : bien plus qu’un souvenir, un levier d’impact`}
+            </h2>
+
+            <p className={style.TextP4}>
+                C’était le grand soir.
+            </p>
+
+            <p className={style.TextP4}>
+                Des mois de coordination, de feuilles Excel, de visios, d’ajustements de dernière minute. <br />
+                Un lancement de produit pensé comme une déclaration d’ambition.
+            </p>
+
+            <p className={style.TextP4}>
+                Le lieu ? Choisi avec soin, décidé en fonction du budget, certes, mais aussi un piège technique. Trop 
+                sombre dans les recoins, trop lumineux sur scène. Les spots LED créaient des ombres dures, les variations 
+                d’intensité mettaient en péril toute captation cohérente.
+            </p>
+
+            <p className={style.TextP4}>
+                Et le son… un écho subtil dans les angles, des micros récalcitrants, et cette nécessité d’entendre 
+                chaque mot sans faillir, surtout quand il s’agit de porter une vision d’entreprise ou de fédérer les 
+                équipes.
+            </p>
+
+            <p className={style.TextP4}>
+                Un moment rare, qu’on aurait voulu graver avec clarté. Pas juste pour s’en souvenir.
+            </p>
+
+            <p className={style.TextP4}>
+                Mais pour le raconter, le revivre, et le faire rayonner.
+            </p>
         </section>
     )
 }
