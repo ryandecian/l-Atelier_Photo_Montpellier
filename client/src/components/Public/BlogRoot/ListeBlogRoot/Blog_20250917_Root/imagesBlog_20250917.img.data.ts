ed5cf3d92f59c84c776de0c62bb752fe9fa8bfbd
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-18 18:16:02 +0200

    Import des images destiné à la page blog 20250917

--- client/src/components/Public/BlogRoot/ListeBlogRoot/Blog_20250917_Root/imagesBlog_20250917.img.data.ts
+++ client/src/components/Public/BlogRoot/ListeBlogRoot/Blog_20250917_Root/imagesBlog_20250917.img.data.ts
@@ -8,22 +8,17 @@ import ImagesControllerDataType from "../../../../../types/ImagesControllerData.
  * photographie-professionnelle : 2/2
 */
 
-const ImagesBlog_20250710_RootControllerData: ImagesControllerDataType[] = [
-    /* {ImagesBlog_20250710_RootControllerData[0].src} */
+const ImagesBlog_20250917_RootControllerData: ImagesControllerDataType[] = [
+    /* {ImagesBlog_20250917_RootControllerData[0].src} */
     {
-        src: "/images/image-blog/blog_20250710/lapm-photo-portrait-feel-good-professionnel-montpellier-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - Photographe professionnel à Montpellier - Portrait Feel Good - L'Atelier Photo Montpellier",
+        src: "/images/image-blog/blog_20250917/lapm-photo-professionnel-conference-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographe professionnel à Montpellier - Conférence - L'Atelier Photo Montpellier",
     },
-    /* {ImagesBlog_20250710_RootControllerData[1].src} */
+    /* {ImagesBlog_20250917_RootControllerData[1].src} */
     {
-        src: "/images/image-blog/blog_20250710/lapm-photographie-professionnelle-portrait-jeune-homme-fellgood-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - Photographie professionnelle à Montpellier - Portrait Jeune Homme Feel Good - L'Atelier Photo Montpellier",
-    },
-    /* {ImagesBlog_20250710_RootControllerData[2].src} */
-    {
-        src: "/images/image-blog/blog_20250710/lapm-photographie-professionnelle-portrait-lifestyle-exterieur-flash-l-atelier-photo-montpellier.jpg",
-        alt: "LAPM - Photographie professionnelle à Montpellier - Portrait Lifestyle Extérieur Flash - L'Atelier Photo Montpellier",
+        src: "/images/image-blog/blog_20250917/lapm-photo-professionnel-congres-corporate-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographe professionnel à Montpellier - Congrès Corporate - L'Atelier Photo Montpellier",
     },
 ];
 
-export default ImagesBlog_20250710_RootControllerData;
+export default ImagesBlog_20250917_RootControllerData;
