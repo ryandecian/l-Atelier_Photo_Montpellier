ff86d11667e6dbce24ea09e418298104489ea740
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-19 02:23:41 +0200

    Refactorisation de JSON_LD_BlogPosting_Blog_20250616_schema_SEO

--- client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250616_SEO/JSON-LD_BlogPosting_Blog_20250616.schema.seo.tsx
+++ client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250616_SEO/JSON-LD_BlogPosting_Blog_20250616.schema.seo.tsx
@@ -1,19 +1,21 @@
+/* Import des Components */
 import { dataSEO_Root_data_SEO } from "../../../dataSEO_Root.data.seo";
-import DataSEORootType from "../../../../types/seo/dataSEORoot.type";
+import { dataSEO_Blog_20250616_data_SEO } from "./dataSEO_Blog_20250616.data.seo";
+import { dataCardBlog_img_data } from "../../../../components/Public/BlogRoot/dataCardBlog.img.data";
 
-import DataSEO_Blog_20250616s from "./dataSEO_Blog_20250616.data.seo";
-import DataSEOTargetOneType from "../../../../types/seo/dataSEOTargetOne.type";
+/* Import des Types */
+import { DataSEORoot_Type } from "../../../../types/seo/dataSEORoot.type";
+import { DataSEOTargetOne_Type } from "../../../../types/seo/dataSEOTargetOne.type";
+import { CardBlog_Type } from "../../../../types/components/blogs/cardBlog.type";
 
-import DataCardBlogRoots from "../../../../components/Public/BlogRoot/dataCardBlog.img.data";
-import CardBlogType from "../../../../types/components/blogs/cardBlog.type";
+/* Import des Utils */
+import { extractKeywordsFromDataSEO_Utils } from "../../../../utils/extractKeywordsFromDataSEO.utils";
+import { convertDateFrToISO_String_Utils } from "../../../../utils/convertDateFrToISO.utils";
 
-import extractKeywordsFromDataSEO from "../../../../utils/extractKeywordsFromDataSEO.utils";
-import { convertDateFrToISO_String } from "../../../../utils/convertDateFrToISO.utils";
-
-function JSON_LD_BlogPosting_Blog_20250616_Schema_SEO() : string {
-    const DataCardBlogRoot: CardBlogType[] = DataCardBlogRoots(); /* Récupération des données de la carte du blog */
-    const DataSEORoot: DataSEORootType = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
-    const DataSEO_Blog_20250616: DataSEOTargetOneType = DataSEO_Blog_20250616s(); /* Récupération des données SEO de la page */
+function JSON_LD_BlogPosting_Blog_20250616_schema_SEO() : string {
+    const DataCardBlogRoot: CardBlog_Type[] = dataCardBlog_img_data(); /* Récupération des données de la carte du blog */
+    const DataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
+    const DataSEO_Blog_20250616: DataSEOTargetOne_Type = dataSEO_Blog_20250616_data_SEO(); /* Récupération des données SEO de la page */
 
     const JSON_LD = JSON.stringify({
         "@context": DataSEORoot["@context"],
@@ -30,7 +32,7 @@ function JSON_LD_BlogPosting_Blog_20250616_Schema_SEO() : string {
         /* Métadonnées principales */
         "headline": DataSEO_Blog_20250616.name_Service, /* Titre principal affiché par Google */
         "description": DataSEO_Blog_20250616.description, /* Description de l'article de blog */
-        "keywords": extractKeywordsFromDataSEO(DataSEO_Blog_20250616.keywords), /* Extraction des mots clés */
+        "keywords": extractKeywordsFromDataSEO_Utils(DataSEO_Blog_20250616.keywords), /* Extraction des mots clés */
 
         /* Image principale */
         "image": {
@@ -41,7 +43,7 @@ function JSON_LD_BlogPosting_Blog_20250616_Schema_SEO() : string {
         },
 
         /* Dates */
-        "datePublished": convertDateFrToISO_String(DataCardBlogRoot[0].date), /* Date de publication au format ISO 8601 */
+        "datePublished": convertDateFrToISO_String_Utils(DataCardBlogRoot[0].date), /* Date de publication au format ISO 8601 */
 
         /* Auteur */
         "author": DataSEORoot.author, /* Auteur de l'article, défini dans DataSEORoot */
@@ -58,4 +60,4 @@ function JSON_LD_BlogPosting_Blog_20250616_Schema_SEO() : string {
     )
 }
 
-export default JSON_LD_BlogPosting_Blog_20250616_Schema_SEO;
+export { JSON_LD_BlogPosting_Blog_20250616_schema_SEO };
