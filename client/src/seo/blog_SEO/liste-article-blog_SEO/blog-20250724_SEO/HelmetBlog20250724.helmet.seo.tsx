90c7ef6ccf6d3e7c2c9958eefdef488b56c8bd45
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-20 00:35:17 +0200

    Refactorisation de Helmet_Blog_20250724_helmet_SEO

--- client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250724_SEO/HelmetBlog20250724.helmet.seo.tsx
+++ client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250724_SEO/HelmetBlog20250724.helmet.seo.tsx
@@ -1,22 +1,25 @@
+/* Import des composants React */
 import { Helmet } from "react-helmet-async";
-import DataSEOHelmetType from "../../../../types/seo/dataSEOHelmet.type";
 
+/* Import des Components */
 import { dataSEO_Root_data_SEO } from "../../../dataSEO_Root.data.seo";
-import DataSEO_Blog_20250724s from "./dataSEO_Blog_20250724.data.seo";
+import { dataSEO_Blog_20250724_data_SEO } from "./dataSEO_Blog_20250724.data.seo";
 
-import DataSEORootType from "../../../../types/seo/dataSEORoot.type";
-import DataSEOTargetOneType from "../../../../types/seo/dataSEOTargetOne.type";
-
+/* Import des JSON_LD */
 import { JSON_LD_LocalBusiness_Root_schema_SEO } from "../../../JSON-LD_LocalBusiness_Root.schema.seo";
-import JSON_LD_BlogPosting_Blog_20250724_Schema_SEO from "./JSON-LD_BlogPosting_Blog_20250724.schema.seo";
-import JSON_LD_Breadcrumb_Blog_20250724_Schema_SEO from "./JSON-LD_Breadcrumb_Blog_20250724.schema.seo";
+import { JSON_LD_BlogPosting_Blog_20250724_schema_SEO } from "./JSON-LD_BlogPosting_Blog_20250724.schema.seo";
+import { JSON_LD_Breadcrumb_Blog_20250724_schema_SEO } from "./JSON-LD_Breadcrumb_Blog_20250724.schema.seo";
 
+/* Import des Types */
+import { DataSEOHelmet_Type } from "../../../../types/seo/dataSEOHelmet.type";
+import { DataSEORoot_Type } from "../../../../types/seo/dataSEORoot.type";
+import { DataSEOTargetOne_Type } from "../../../../types/seo/dataSEOTargetOne.type";
 
-function Helmet_Blog_20250724_SEO() {
-    const DataSEORoot: DataSEORootType = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
-    const DataSEO_Blog_20250724: DataSEOTargetOneType = DataSEO_Blog_20250724s(); /* Récupération des données SEO de la page */
+function Helmet_Blog_20250724_helmet_SEO() {
+    const DataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
+    const DataSEO_Blog_20250724: DataSEOTargetOne_Type = dataSEO_Blog_20250724_data_SEO(); /* Récupération des données SEO de la page */
 
-    const SEO: DataSEOHelmetType = {
+    const SEO: DataSEOHelmet_Type = {
         title: DataSEO_Blog_20250724.title,
         autor: DataSEO_Blog_20250724.autor,
         description: DataSEO_Blog_20250724.description,
@@ -37,10 +40,10 @@ function Helmet_Blog_20250724_SEO() {
                 {JSON_LD_LocalBusiness_Root_schema_SEO()}
             </script>
             <script type="application/ld+json">
-                {JSON_LD_BlogPosting_Blog_20250724_Schema_SEO()}
+                {JSON_LD_BlogPosting_Blog_20250724_schema_SEO()}
             </script>
             <script type="application/ld+json">
-                {JSON_LD_Breadcrumb_Blog_20250724_Schema_SEO()}
+                {JSON_LD_Breadcrumb_Blog_20250724_schema_SEO()}
             </script>
 
             {/* Langue principale du document */}
@@ -78,4 +81,4 @@ function Helmet_Blog_20250724_SEO() {
     );
 }
 
-export default Helmet_Blog_20250724_SEO;
+export { Helmet_Blog_20250724_helmet_SEO };
