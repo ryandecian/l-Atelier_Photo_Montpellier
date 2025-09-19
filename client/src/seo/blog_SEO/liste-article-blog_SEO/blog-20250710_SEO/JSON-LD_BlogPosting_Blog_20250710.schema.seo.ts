71b03a3f6d4449969a861905600576932c9bb8c0
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-20 00:09:31 +0200

    Refactorisation de JSON_LD_BlogPosting_Blog_20250710_schema_SEO

--- client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250710_SEO/JSON-LD_BlogPosting_Blog_20250710.schema.seo.ts
+++ client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250710_SEO/JSON-LD_BlogPosting_Blog_20250710.schema.seo.ts
@@ -13,46 +13,46 @@ import { extractKeywordsFromDataSEO_Utils } from "../../../../utils/seo/extractK
 import { convertDateFrToISO_String_Utils } from "../../../../utils/seo/convertDateFrToISO.utils";
 
 function JSON_LD_BlogPosting_Blog_20250710_schema_SEO() : string {
-    const DataCardBlogRoot: CardBlog_Type[] = dataCardBlog_img_data(); /* Récupération des données de la carte du blog */
-    const DataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
-    const DataSEO_Blog_20250710: DataSEOTargetOne_Type = dataSEO_Blog_20250710_data_SEO(); /* Récupération des données SEO de la page */
+    const dataCardBlogRoot: CardBlog_Type[] = dataCardBlog_img_data(); /* Récupération des données de la carte du blog */
+    const dataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
+    const dataSEO_Blog_20250710: DataSEOTargetOne_Type = dataSEO_Blog_20250710_data_SEO(); /* Récupération des données SEO de la page */
 
     const JSON_LD = JSON.stringify({
-        "@context": DataSEORoot["@context"],
-        "@type": DataSEORoot["@type"].BlogPosting,
+        "@context": dataSEORoot["@context"],
+        "@type": dataSEORoot["@type"].BlogPosting,
 
         /* Identifiants & URL */
-        "@id": DataSEO_Blog_20250710.id_Service, /* @id SEO-friendly, correspond à l'URL de la page + mots clés */
+        "@id": dataSEO_Blog_20250710.id_Service, /* @id SEO-friendly, correspond à l'URL de la page + mots clés */
         "mainEntityOfPage": {
             "@type": "WebPage",
-            "@id": DataSEO_Blog_20250710.url,
+            "@id": dataSEO_Blog_20250710.url,
         },
-        "url": DataSEO_Blog_20250710.url,
+        "url": dataSEO_Blog_20250710.url,
 
         /* Métadonnées principales */
-        "headline": DataSEO_Blog_20250710.name_Service, /* Titre principal affiché par Google */
-        "description": DataSEO_Blog_20250710.description, /* Description de l'article de blog */
-        "keywords": extractKeywordsFromDataSEO_Utils(DataSEO_Blog_20250710.keywords), /* Extraction des mots clés */
+        "headline": dataSEO_Blog_20250710.name_Service, /* Titre principal affiché par Google */
+        "description": dataSEO_Blog_20250710.description, /* Description de l'article de blog */
+        "keywords": extractKeywordsFromDataSEO_Utils(dataSEO_Blog_20250710.keywords), /* Extraction des mots clés */
 
         /* Image principale */
         "image": {
             "@type": "ImageObject",
-            "url": DataSEO_Blog_20250710.img_JSON_LD, /* Lien URL public de l'image */
+            "url": dataSEO_Blog_20250710.img_JSON_LD, /* Lien URL public de l'image */
             "width": 1200,
             "height": 800
         },
 
         /* Dates */
-        "datePublished": convertDateFrToISO_String_Utils(DataCardBlogRoot[0].date), /* Date de publication au format ISO 8601 */
+        "datePublished": convertDateFrToISO_String_Utils(dataCardBlogRoot[0].date), /* Date de publication au format ISO 8601 */
 
         /* Auteur */
-        "author": DataSEORoot.author, /* Auteur de l'article, défini dans DataSEORoot */
+        "author": dataSEORoot.author, /* Auteur de l'article, défini dans DataSEORoot */
 
         /* Éditeur */
-        "publisher": DataSEORoot.publisher, /* Éditeur de l'article, défini dans DataSEORoot */
+        "publisher": dataSEORoot.publisher, /* Éditeur de l'article, défini dans DataSEORoot */
 
         /* Créatrice */
-        "creator": DataSEORoot.creator, /* Créatrice de l'article, défini dans DataSEORoot */
+        "creator": dataSEORoot.creator, /* Créatrice de l'article, défini dans DataSEORoot */
     })
 
     return (
