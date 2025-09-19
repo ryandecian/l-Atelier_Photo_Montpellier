9ec6e6bcd033bae59a856e919230694f3a0e0c4e
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-20 00:23:01 +0200

    Refactorisation de JSON_LD_Breadcrumb_Blog_20250724_Schema_SEO

--- client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250724_SEO/JSON-LD_Breadcrumb_Blog_20250724.schema.seo.ts
+++ client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250724_SEO/JSON-LD_Breadcrumb_Blog_20250724.schema.seo.ts
@@ -1,26 +1,28 @@
+/* Import des Components */
 import { dataSEO_Root_data_SEO } from "../../../dataSEO_Root.data.seo";
-import DataSEORootType from "../../../../types/seo/dataSEORoot.type";
+import { dataSEO_Blog_20250724_data_SEO } from "./dataSEO_Blog_20250724.data.seo";
 
-import DataSEO_Blog_20250724s from "./dataSEO_Blog_20250724.data.seo";
-import DataSEOTargetOneType from "../../../../types/seo/dataSEOTargetOne.type";
+/* Import des Types */
+import { DataSEORoot_Type } from "../../../../types/seo/dataSEORoot.type";
+import { DataSEOTargetOne_Type } from "../../../../types/seo/dataSEOTargetOne.type";
 
 function JSON_LD_Breadcrumb_Blog_20250724_Schema_SEO() : string {
-    const DataSEORoot: DataSEORootType = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
-    const DataSEO_Blog_20250724: DataSEOTargetOneType = DataSEO_Blog_20250724s(); /* Récupération des données SEO de la page */
+    const dataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
+    const dataSEO_Blog_20250724: DataSEOTargetOne_Type = dataSEO_Blog_20250724_data_SEO(); /* Récupération des données SEO de la page */
 
     const JSON_LD = JSON.stringify({
-        "@context": DataSEORoot["@context"],
-        "@type": DataSEORoot["@type"].BreadcrumbList,
+        "@context": dataSEORoot["@context"],
+        "@type": dataSEORoot["@type"].BreadcrumbList,
         "itemListElement": [
             {
                 "@type": "ListItem",
-                "position": DataSEO_Blog_20250724.position,
-                "name": DataSEO_Blog_20250724.name_Breadcrumb, /* Le libellé du lien dans le fil d’Ariane. Exemple : Portfolio ou Portrait Duo */
+                "position": dataSEO_Blog_20250724.position,
+                "name": dataSEO_Blog_20250724.name_Breadcrumb, /* Le libellé du lien dans le fil d’Ariane. Exemple : Portfolio ou Portrait Duo */
                 "item": {
-                    "@type": DataSEORoot["@type"].WebPage,
-                    "@id": DataSEO_Blog_20250724.id_Service,
-                    "url": DataSEO_Blog_20250724.url,
-                    "name": DataSEO_Blog_20250724.name_Breadcrumb
+                    "@type": dataSEORoot["@type"].WebPage,
+                    "@id": dataSEO_Blog_20250724.id_Service,
+                    "url": dataSEO_Blog_20250724.url,
+                    "name": dataSEO_Blog_20250724.name_Breadcrumb
                 }
             },
         ],
@@ -31,4 +33,4 @@ function JSON_LD_Breadcrumb_Blog_20250724_Schema_SEO() : string {
     )
 }
 
-export default JSON_LD_Breadcrumb_Blog_20250724_Schema_SEO;
+export { JSON_LD_Breadcrumb_Blog_20250724_Schema_SEO };
