caf05fa51a44cea8709bd4f2a0f4a90a082a285b
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-20 12:15:28 +0200

    Refactorisation de dataSEO_PortraitCorporate_data_SEO

--- client/src/seo/PortraitCorporateSEO/Helmet_PortraitCorporate.helmet.seo.tsx
+++ client/src/seo/PortraitCorporateSEO/Helmet_PortraitCorporate.helmet.seo.tsx
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet-async";
 import DataSEOHelmetType from "../../types/seo/dataSEOHelmet.type";
 
 import { dataSEO_Root_data_SEO } from "../dataSEO_Root.data.seo";
-import DataSEO_PortraitCorporates from "./DdataSEO_PortraitCorporate.data.seo";
+import DataSEO_PortraitCorporates from "./dataSEO_PortraitCorporate.data.seo";
 
 import DataSEORootType from "../../types/seo/dataSEORoot.type";
 import DataSEOTargetOneType from "../../types/seo/dataSEOTargetOne.type";
