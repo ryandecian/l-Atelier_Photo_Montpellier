eb77ab30afd865efbf8a6787636f927cb1898e3d
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-20 12:26:07 +0200

    Push for rename

--- client/src/seo/PortraitGrossesseSEO/JSON-LD_Breadcrumb_PortraitGrossesse.schema.seo.ts
+++ client/src/seo/PortraitGrossesseSEO/JSON-LD_Breadcrumb_PortraitGrossesse.schema.seo.ts
@@ -1,7 +1,7 @@
 import { dataSEO_Root_data_SEO } from "../dataSEO_Root.data.seo";
 import DataSEORootType from "../../types/seo/dataSEORoot.type";
 
-import DataSEO_PortraitGrossesses from "./DataSEO_PortraitGrossesse.data.seo";
+import DataSEO_PortraitGrossesses from "./DdataSEO_PortraitGrossesse.data.seo";
 import DataSEOTargetOneType from "../../types/seo/dataSEOTargetOne.type";
 
 function JSON_LD_Breadcrumb_PortraitGrossesse_Schema_SEO() : string {
