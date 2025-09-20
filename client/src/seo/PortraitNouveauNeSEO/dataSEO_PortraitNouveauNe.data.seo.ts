76192f095a06327200a279d77a34c0cd945cb666
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-20 12:43:17 +0200

    Refactorisation de dataSEO_PortraitNouveauNe_data_SEO

--- client/src/seo/PortraitNouveauNeSEO/dataSEO_PortraitNouveauNe.data.seo.ts
+++ client/src/seo/PortraitNouveauNeSEO/dataSEO_PortraitNouveauNe.data.seo.ts
@@ -1,8 +1,11 @@
+/* Import des composants Router */
 import ListDataRouter from "../../router/router";
-import DataSEOTargetOneType from "../../types/seo/dataSEOTargetOne.type";
 
-function DataSEO_PortraitNouveauNe(): DataSEOTargetOneType {
-    const DataSEO = {
+/* Import des Types */
+import { DataSEOTargetOne_Type } from "../../types/seo/dataSEOTargetOne.type";
+
+function dataSEO_PortraitNouveauNe_data_SEO(): DataSEOTargetOne_Type {
+    const dataSEO = {
         /* Data Helmet */
         title: "Portrait Grossesse et nouveau né - l'Atelier Photo Montpellier",
         autor: "Anne SAUNIER",
@@ -45,8 +48,8 @@ function DataSEO_PortraitNouveauNe(): DataSEOTargetOneType {
     };
 
     return (
-        DataSEO
+        dataSEO
     )
 }
 
-export default DataSEO_PortraitNouveauNe;
+export { dataSEO_PortraitNouveauNe_data_SEO };
