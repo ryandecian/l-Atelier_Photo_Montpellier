b0c432751ca42a008c272a3a4fbddf13ed0b2f60
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-20 12:38:17 +0200

    Push for rename

--- client/src/seo/PortraitNouveauNeSEO/JSON-LD_Service_PortraitNouveauNe.schema.seo.tsx
+++ client/src/seo/PortraitNouveauNeSEO/JSON-LD_Service_PortraitNouveauNe.schema.seo.tsx
@@ -1,7 +1,7 @@
 import { dataSEO_Root_data_SEO } from "../dataSEO_Root.data.seo";
 import DataSEORootType from "../../types/seo/dataSEORoot.type";
 
-import DataSEO_PortraitNouveauNes from "./DataSEO_PortraitNouveauNe.data.seo";
+import DataSEO_PortraitNouveauNes from "./DaataSEO_PortraitNouveauNe.data.seo";
 import DataSEOTargetOneType from "../../types/seo/dataSEOTargetOne.type";
 
 function JSON_LD_Service_PortraitNouveauNe_Schema_SEO() : string {
