c668024120a2a569241cc9a73246af07b562ebc2
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-20 12:30:54 +0200

    Refactorisation de dataSEO_PortraitHobby_data_SEO

--- client/src/seo/PortraitHobbySEO/dataSEO_PortraitHobby.data.seo.ts
+++ client/src/seo/PortraitHobbySEO/dataSEO_PortraitHobby.data.seo.ts
@@ -1,8 +1,11 @@
+/* Import des composants Router */
 import ListDataRouter from "../../router/router";
-import DataSEOTargetOneType from "../../types/seo/dataSEOTargetOne.type";
 
-function DataSEO_PortraitHobby(): DataSEOTargetOneType {
-    const DataSEO = {
+/* Import des Types */
+import { DataSEOTargetOne_Type } from "../../types/seo/dataSEOTargetOne.type";
+
+function dataSEO_PortraitHobby_data_SEO(): DataSEOTargetOne_Type {
+    const dataSEO = {
         /* Data Helmet */
         title: "Portrait Hobby - l'Atelier Photo Montpellier",
         autor: "Anne SAUNIER",
@@ -45,8 +48,8 @@ function DataSEO_PortraitHobby(): DataSEOTargetOneType {
     };
 
     return (
-        DataSEO
+        dataSEO
     )
 }
 
-export default DataSEO_PortraitHobby;
+export { dataSEO_PortraitHobby_data_SEO };
