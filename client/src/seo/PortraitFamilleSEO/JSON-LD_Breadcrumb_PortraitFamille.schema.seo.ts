a5c1ad13dda2d796c8aae439d2d003a515bcc923
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-20 12:23:42 +0200

    Push for rename

--- client/src/seo/PortraitFamilleSEO/JSON-LD_Breadcrumb_PortraitFamille.schema.seo.ts
+++ client/src/seo/PortraitFamilleSEO/JSON-LD_Breadcrumb_PortraitFamille.schema.seo.ts
@@ -1,7 +1,7 @@
 import { dataSEO_Root_data_SEO } from "../dataSEO_Root.data.seo";
 import DataSEORootType from "../../types/seo/dataSEORoot.type";
 
-import DataSEO_PortraitFamilles from "./DataSEO_PortraitFamille.data.seo";
+import DataSEO_PortraitFamilles from "./DdataSEO_PortraitFamille.data.seo";
 import DataSEOTargetOneType from "../../types/seo/dataSEOTargetOne.type";
 
 function JSON_LD_Breadcrumb_PortraitFamille_Schema_SEO() : string {
