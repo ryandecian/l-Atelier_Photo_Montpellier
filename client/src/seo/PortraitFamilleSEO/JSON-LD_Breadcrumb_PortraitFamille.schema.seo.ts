a01ff969006d6c353e588b2ae17962eb816ec502
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-21 21:47:40 +0200

    Refactorisation de JSON_LD_Breadcrumb_PortraitFamille_schema_SEO

--- client/src/seo/PortraitFamilleSEO/JSON-LD_Breadcrumb_PortraitFamille.schema.seo.ts
+++ client/src/seo/PortraitFamilleSEO/JSON-LD_Breadcrumb_PortraitFamille.schema.seo.ts
@@ -1,26 +1,28 @@
+/* Import des Components */
 import { dataSEO_Root_data_SEO } from "../dataSEO_Root.data.seo";
-import DataSEORootType from "../../types/seo/dataSEORoot.type";
+import { dataSEO_PortraitFamille_data_SEO } from "./dataSEO_PortraitFamille.data.seo";
 
-import DataSEO_PortraitFamilles from "./dataSEO_PortraitFamille.data.seo";
-import DataSEOTargetOneType from "../../types/seo/dataSEOTargetOne.type";
+/* Import des Types */
+import { DataSEORoot_Type } from "../../types/seo/dataSEORoot.type";
+import { DataSEOTargetOne_Type } from "../../types/seo/dataSEOTargetOne.type";
 
-function JSON_LD_Breadcrumb_PortraitFamille_Schema_SEO() : string {
-    const DataSEORoot: DataSEORootType = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
-    const DataSEO_PortraitFamille: DataSEOTargetOneType = DataSEO_PortraitFamilles(); /* Récupération des données SEO de la page */
+function JSON_LD_Breadcrumb_PortraitFamille_schema_SEO() : string {
+    const dataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
+    const dataSEO_PortraitFamille: DataSEOTargetOne_Type = dataSEO_PortraitFamille_data_SEO(); /* Récupération des données SEO de la page */
 
     const JSON_LD = JSON.stringify({
-        "@context": DataSEORoot["@context"],
-        "@type": DataSEORoot["@type"].BreadcrumbList,
+        "@context": dataSEORoot["@context"],
+        "@type": dataSEORoot["@type"].BreadcrumbList,
         "itemListElement": [
             {
                 "@type": "ListItem",
-                "position": DataSEO_PortraitFamille.position,
-                "name": DataSEO_PortraitFamille.name_Breadcrumb, /* Le libellé du lien dans le fil d’Ariane. Exemple : Portfolio ou Portrait Famille */
+                "position": dataSEO_PortraitFamille.position,
+                "name": dataSEO_PortraitFamille.name_Breadcrumb, /* Le libellé du lien dans le fil d’Ariane. Exemple : Portfolio ou Portrait Famille */
                 "item": {
-                    "@type": DataSEORoot["@type"].WebPage,
-                    "@id": DataSEO_PortraitFamille.id_Service,
-                    "url": DataSEO_PortraitFamille.url,
-                    "name": DataSEO_PortraitFamille.name_Breadcrumb
+                    "@type": dataSEORoot["@type"].WebPage,
+                    "@id": dataSEO_PortraitFamille.id_Service,
+                    "url": dataSEO_PortraitFamille.url,
+                    "name": dataSEO_PortraitFamille.name_Breadcrumb
                 }
             },
         ],
@@ -31,4 +33,4 @@ function JSON_LD_Breadcrumb_PortraitFamille_Schema_SEO() : string {
     )
 }
 
-export default JSON_LD_Breadcrumb_PortraitFamille_Schema_SEO;
+export { JSON_LD_Breadcrumb_PortraitFamille_schema_SEO };
