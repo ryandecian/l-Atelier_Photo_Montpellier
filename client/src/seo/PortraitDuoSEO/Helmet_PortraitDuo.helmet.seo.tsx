60501f68dadd93af83e1f9086c37c0c70c9fe9cf
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-21 20:47:33 +0200

    Refactorisation de Helmet_PortraitDuo_helmet_SEO

--- client/src/seo/PortraitDuoSEO/Helmet_PortraitDuo.helmet.seo.tsx
+++ client/src/seo/PortraitDuoSEO/Helmet_PortraitDuo.helmet.seo.tsx
@@ -1,24 +1,27 @@
+/* Import des composants React */
 import { Helmet } from "react-helmet-async";
-import DataSEOHelmetType from "../../types/seo/dataSEOHelmet.type";
 
+/* Import des Components */
 import { dataSEO_Root_data_SEO } from "../dataSEO_Root.data.seo";
-import DataSEO_PortraitDuos from "./dataSEO_PortraitDuo.data.seo";
+import { dataSEO_PortraitDuo_data_SEO } from "./dataSEO_PortraitDuo.data.seo";
 
-import DataSEORootType from "../../types/seo/dataSEORoot.type";
-import DataSEOTargetOneType from "../../types/seo/dataSEOTargetOne.type";
-
+/* Import des JSON_LD */
 import { JSON_LD_LocalBusiness_Root_schema_SEO } from "../JSON-LD_LocalBusiness_Root.schema.seo";
-import JSON_LD_Service_PortraitDuo_Schema_SEO from "./JSON-LD_Service_PortraitDuo.schema.seo";
-import JSON_LD_Breadcrumb_PortraitDuo_Schema_SEO from "./JSON-LD_Breadcrumb_PortraitDuo.schema.seo";
+import { JSON_LD_Breadcrumb_PortraitDuo_schema_SEO } from "./JSON-LD_Breadcrumb_PortraitDuo.schema.seo";
+import { JSON_LD_Service_PortraitDuo_schema_SEO } from "./JSON-LD_Service_PortraitDuo.schema.seo";
 
+/* Import des Types */
+import { DataSEOHelmet_Type } from "../../types/seo/dataSEOHelmet.type";
+import { DataSEORoot_Type } from "../../types/seo/dataSEORoot.type";
+import { DataSEOTargetOne_Type } from "../../types/seo/dataSEOTargetOne.type";
 
-function Helmet_PortraitDuo_SEO() {
-    const DataSEORoot: DataSEORootType = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
-    const DataSEO_PortraitDuo: DataSEOTargetOneType = DataSEO_PortraitDuos(); /* Récupération des données SEO de la page */
+function Helmet_PortraitDuo_helmet_SEO() {
+    const DataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
+    const DataSEO_PortraitDuo: DataSEOTargetOne_Type = dataSEO_PortraitDuo_data_SEO(); /* Récupération des données SEO de la page */
 
-    const SEO: DataSEOHelmetType = {
+    const SEO: DataSEOHelmet_Type = {
         title: DataSEO_PortraitDuo.title,
-        autor: DataSEO_PortraitDuo.autor,
+        author: DataSEO_PortraitDuo.author,
         description: DataSEO_PortraitDuo.description,
         url: DataSEO_PortraitDuo.url,
         img: DataSEO_PortraitDuo.img_Helmet,
@@ -37,10 +40,10 @@ function Helmet_PortraitDuo_SEO() {
                 {JSON_LD_LocalBusiness_Root_schema_SEO()}
             </script>
             <script type="application/ld+json">
-                {JSON_LD_Service_PortraitDuo_Schema_SEO()}
+                {JSON_LD_Service_PortraitDuo_schema_SEO()}
             </script>
             <script type="application/ld+json">
-                {JSON_LD_Breadcrumb_PortraitDuo_Schema_SEO()}
+                {JSON_LD_Breadcrumb_PortraitDuo_schema_SEO()}
             </script>
 
             {/* Langue principale du document */}
@@ -51,7 +54,7 @@ function Helmet_PortraitDuo_SEO() {
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="description" content={SEO.description} />
-            <meta name="author" content={SEO.autor} />
+            <meta name="author" content={SEO.author} />
             {filterKeywords && <meta name="keywords" content={filterKeywords} />}
 
             {/* SEO pour les moteurs de recherche */}
@@ -78,4 +81,4 @@ function Helmet_PortraitDuo_SEO() {
     );
 }
 
-export default Helmet_PortraitDuo_SEO;
+export { Helmet_PortraitDuo_helmet_SEO };
