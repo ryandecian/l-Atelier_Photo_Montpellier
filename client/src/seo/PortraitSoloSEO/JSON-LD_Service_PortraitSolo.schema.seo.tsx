58b3e92066a6896b5e4f331666c53e0d621cc2c3
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-24 23:51:25 +0200

    Refactorisation de JSON_LD_Service_PortraitSolo_schema_SEO

--- client/src/seo/PortraitSoloSEO/JSON-LD_Service_PortraitSolo.schema.seo.tsx
+++ client/src/seo/PortraitSoloSEO/JSON-LD_Service_PortraitSolo.schema.seo.tsx
@@ -1,41 +1,43 @@
+/* Import des Components */
 import { dataSEO_Root_data_SEO } from "../dataSEO_Root.data.seo";
-import DataSEORootType from "../../types/seo/dataSEORoot.type";
+import { dataSEO_PortraitSolo_data_SEO } from "./dataSEO_PortraitSolo.data.seo";
 
-import DataSEO_PortraitSolos from "./dataSEO_PortraitSolo.data.seo";
-import DataSEOTargetOneType from "../../types/seo/dataSEOTargetOne.type";
+/* Import des Types */
+import { DataSEORoot_Type } from "../../types/seo/dataSEORoot.type";
+import { DataSEOTargetOne_Type } from "../../types/seo/dataSEOTargetOne.type";
 
-function JSON_LD_Service_PortraitSolo_Schema_SEO() : string {
-    const DataSEORoot: DataSEORootType = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
-    const DataSEO_PortraitSolo: DataSEOTargetOneType = DataSEO_PortraitSolos(); /* Récupération des données SEO de la page */
+function JSON_LD_Service_PortraitSolo_schema_SEO() : string {
+    const dataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
+    const dataSEO_PortraitSolo: DataSEOTargetOne_Type = dataSEO_PortraitSolo_data_SEO(); /* Récupération des données SEO de la page */
 
     const JSON_LD = JSON.stringify({
-        "@context": DataSEORoot["@context"], /* (Obligatoire) Contexte de la donnée */
-        "@type": DataSEORoot["@type"].Service, /* (Obligatoire) Type de la donnée */
-        "@id": DataSEO_PortraitSolo.id_Service,
-        "serviceType": DataSEO_PortraitSolo.serviceType,
-        "name": DataSEO_PortraitSolo.name_Service, /* (Obligatoire) Nom du service */
-        "image": DataSEO_PortraitSolo.img_JSON_LD, /* (Obligatoire) URL de l'image de l'entreprise */
-        "description": DataSEO_PortraitSolo.description,
-        "url": DataSEO_PortraitSolo.url,
-        "termsOfService": DataSEORoot.termsOfService,
+        "@context": dataSEORoot["@context"], /* (Obligatoire) Contexte de la donnée */
+        "@type": dataSEORoot["@type"].Service, /* (Obligatoire) Type de la donnée */
+        "@id": dataSEO_PortraitSolo.id_Service,
+        "serviceType": dataSEO_PortraitSolo.serviceType,
+        "name": dataSEO_PortraitSolo.name_Service, /* (Obligatoire) Nom du service */
+        "image": dataSEO_PortraitSolo.img_JSON_LD, /* (Obligatoire) URL de l'image de l'entreprise */
+        "description": dataSEO_PortraitSolo.description,
+        "url": dataSEO_PortraitSolo.url,
+        "termsOfService": dataSEORoot.termsOfService,
         "provider": {
-            "@id": DataSEORoot.id_LocalBusiness /* (Obligatoire) Référence à l'ID de l'Entreprise */
+            "@id": dataSEORoot.id_LocalBusiness /* (Obligatoire) Référence à l'ID de l'Entreprise */
         },
         "publisher": {
             "@type": "Organization",
-            "name": DataSEORoot.name,
+            "name": dataSEORoot.name,
             "logo": {
             "@type": "ImageObject",
-            "url": DataSEORoot.logo,
+            "url": dataSEORoot.logo,
             "width": 200,
             "height": 200
             },
         },
-        "areaServed": DataSEORoot.areaServed, /* Zone géographique desservie par le service */
+        "areaServed": dataSEORoot.areaServed, /* Zone géographique desservie par le service */
         // ⚠️ A décommenter lors du passage de site vitrine en e-commerce
         // "availableChannel": {
         // "@type": "ServiceChannel",
-        // "serviceUrl": DataSEO_PortraitSolo.url
+        // "serviceUrl": dataSEO_PortraitSolo.url
         // },
     });
 
@@ -44,4 +46,4 @@ function JSON_LD_Service_PortraitSolo_Schema_SEO() : string {
     )
 }
 
-export default JSON_LD_Service_PortraitSolo_Schema_SEO;
+export { JSON_LD_Service_PortraitSolo_schema_SEO };
