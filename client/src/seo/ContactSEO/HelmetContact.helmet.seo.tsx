ced03a7763fa71d91fbb33f578c92973d5e401ca
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-25 01:49:18 +0200

    Mise a jour de HelmetContact_helmet_SEO

--- client/src/seo/ContactSEO/HelmetContact.helmet.seo.tsx
+++ client/src/seo/ContactSEO/HelmetContact.helmet.seo.tsx
@@ -15,7 +15,7 @@ import { DataSEOHelmet_Type } from "../../types/seo/dataSEOHelmet.type";
 import { DataSEORoot_Type } from "../../types/seo/dataSEORoot.type";
 import { DataSEOTargetOne_Type } from "../../types/seo/dataSEOTargetOne.type";
 
-function Helmet_Contact_helmet_SEO() {
+function HelmetContact_helmet_SEO() {
     const dataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
     const dataSEO_Contact: DataSEOTargetOne_Type = dataSEO_Contact_data_SEO(); /* Récupération des données SEO de la page */
 
@@ -81,4 +81,4 @@ function Helmet_Contact_helmet_SEO() {
     );
 }
 
-export { Helmet_Contact_helmet_SEO };
+export { HelmetContact_helmet_SEO };
