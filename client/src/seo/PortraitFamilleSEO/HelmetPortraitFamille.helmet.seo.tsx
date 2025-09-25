c08f2c8271104246c2214819c713408cbee9b678
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-25 12:44:41 +0200

    Mise a jour de HelmetPortraitFamille_helmet_SEO

--- client/src/seo/PortraitFamilleSEO/HelmetPortraitFamille.helmet.seo.tsx
+++ client/src/seo/PortraitFamilleSEO/HelmetPortraitFamille.helmet.seo.tsx
@@ -15,7 +15,7 @@ import { DataSEOHelmet_Type } from "../../types/seo/dataSEOHelmet.type";
 import { DataSEORoot_Type } from "../../types/seo/dataSEORoot.type";
 import { DataSEOTargetOne_Type } from "../../types/seo/dataSEOTargetOne.type";
 
-function Helmet_PortraitFamille_helmet_SEO() {
+function HelmetPortraitFamille_helmet_SEO() {
     const dataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
     const dataSEO_PortraitFamille: DataSEOTargetOne_Type = dataSEO_PortraitFamille_data_SEO(); /* Récupération des données SEO de la page */
 
@@ -81,4 +81,4 @@ function Helmet_PortraitFamille_helmet_SEO() {
     );
 }
 
-export { Helmet_PortraitFamille_helmet_SEO };
+export { HelmetPortraitFamille_helmet_SEO };
