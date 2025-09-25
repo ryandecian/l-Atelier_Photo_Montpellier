35b57790ad3c5ac38ed7659e32de5632d96f393e
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-25 13:14:49 +0200

    Mise a jour de Blog_20250601_Page

--- client/src/pages/Public/BlogPage/ListeBlogPage/Blog_20250601.page.tsx
+++ client/src/pages/Public/BlogPage/ListeBlogPage/Blog_20250601.page.tsx
@@ -1,16 +1,23 @@
+/* Import des modules CSS */
 import style from "../../../stylePage.module.css";
-import NavRoot from "../../../../components/layout/NavRoot/NavRoot";
+
+/* Import des Components */
 import Blog_20250601_Root from "../../../../components/Public/BlogRoot/ListeBlogRoot/Blog_20250601_Root/Blog_20250601_Root";
-import Footer from "../../../../components/layout/footer/Footer.layout";
-import { Helmet_Blog_20250601_helmet_SEO } from "../../../../seo/blog_SEO/liste-article-blog_SEO/blog-20250601_SEO/HelmetBlog20250601.helmet.seo";
-import useScrollToHash from "../../../../hook/useScrollToHash";
+import NavRoot from "../../../../components/layout/NavRoot/NavRoot";
+import Footer_layout from "../../../../components/layout/footer/Footer.layout";
+
+/* Import des composants SEO */
+import { HelmetBlog20250601_helmet_SEO } from "../../../../seo/blog_SEO/liste-article-blog_SEO/blog-20250601_SEO/HelmetBlog20250601.helmet.seo";
+
+/* Import des Hooks */
+import useScrollToHash_utils_Hook from "../../../../hook/useScrollToHash";
 
 function Blog_20250601_Page() {
-    useScrollToHash() /* Utilisation du hook pour gérer le scroll vers les sections avec des IDs */
-    
+    useScrollToHash_utils_Hook() /* Utilisation du hook pour gérer le scroll vers les sections avec des IDs */
+
      return (
         <section className={`Blog_20250601_Page ${style.Page}`}>
-            <Helmet_Blog_20250601_helmet_SEO />
+            <HelmetBlog20250601_helmet_SEO />
             <header id="top" className={style.Header}>
                  <NavRoot />
             </header>
@@ -18,7 +25,7 @@ function Blog_20250601_Page() {
                 <Blog_20250601_Root />
             </main>
             <footer className={style.Footer}>
-                <Footer />
+                <Footer_layout />
             </footer>
         </section>
     )
