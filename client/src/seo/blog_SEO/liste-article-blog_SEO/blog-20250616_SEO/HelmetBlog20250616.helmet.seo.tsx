328dbdbdb50ec9ba5ac8bf6afa0ef1d56b967072
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-25 13:18:10 +0200

    Mise jour de  HelmetBlog20250616_helmet_SEO

--- client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250616_SEO/HelmetBlog20250616.helmet.seo.tsx
+++ client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20250616_SEO/HelmetBlog20250616.helmet.seo.tsx
@@ -15,7 +15,7 @@ import { DataSEOHelmet_Type } from "../../../../types/seo/dataSEOHelmet.type";
 import { DataSEORoot_Type } from "../../../../types/seo/dataSEORoot.type";
 import { DataSEOTargetOne_Type } from "../../../../types/seo/dataSEOTargetOne.type";
 
-function Helmet_Blog_20250616_helmet_SEO() {
+function HelmetBlog20250616_helmet_SEO() {
     const dataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
     const dataSEO_Blog_20250616: DataSEOTargetOne_Type = dataSEO_Blog_20250616_data_SEO(); /* Récupération des données SEO de la page */
 
@@ -81,4 +81,4 @@ function Helmet_Blog_20250616_helmet_SEO() {
     );
 }
 
-export { Helmet_Blog_20250616_helmet_SEO };
+export { HelmetBlog20250616_helmet_SEO };
