dc738b4e8a3716fb4a23323c55da1fff62473fc0
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-26 23:45:53 +0200

    Refactorisation de Portfolio_Page

---
+++ client/src/pages/Public/PortfolioPage/Portfolio.page.tsx
@@ -0,0 +1,34 @@
+/* Import des modules CSS */
+import style from "../../../stylePage.module.css";
+
+/* Import des Components */
+import PortfolioRoot from "../../../components/Public/PortfolioRoot/PortfolioRoot";
+import NavRoot from "../../../components/layout/NavRoot/NavRoot";
+import Footer_layout from "../../../components/layout/footer/Footer.layout";
+
+/* Import des composants SEO */
+import { HelmetPortfolio_helmet_SEO } from "../../../seo/PortfolioSEO/HelmetPortfolio.helmet.seo";
+
+/* Import des Hooks */
+import useScrollToHash_utils_Hook from "../../../hook/useScrollToHash";
+
+function Portfolio_Page() {
+    useScrollToHash_utils_Hook() /* Utilisation du hook pour gérer le scroll vers les sections avec des IDs */
+
+    return (
+        <section className={`Portfolio ${style.Page}`}>
+            <HelmetPortfolio_helmet_SEO />
+            <header id="top" className={style.Header}>
+                 <NavRoot />
+            </header>
+            <main className={style.Main}>
+                <PortfolioRoot />
+            </main>
+            <footer className={style.Footer}>
+                <Footer_layout />
+            </footer>
+        </section>
+    )
+}
+
+export default Portfolio_Page;
