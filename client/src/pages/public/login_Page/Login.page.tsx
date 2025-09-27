6aec52ae4cf2545dcd6758250b93136615dcd67a
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-27 18:42:36 +0200

    Correction CSS

--- client/src/pages/public/login_Page/Login.page.tsx
+++ client/src/pages/public/login_Page/Login.page.tsx
@@ -1,5 +1,5 @@
 /* Import des modules CSS */
-import style from "../../../stylePage.module.css";
+import style from "../../stylePage.module.css";
 
 /* Import des Components */
 import LoginRoot from "../../../components/Public/LoginRoot/LoginRoot";
