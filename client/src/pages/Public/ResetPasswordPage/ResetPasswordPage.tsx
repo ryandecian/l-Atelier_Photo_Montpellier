cc612aef41ded2b826555f4be00b0dfc855822e4
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-27 14:12:00 +0200

    Refactroisation de ResetPasswordPage

--- client/src/pages/Public/ResetPasswordPage/ResetPasswordPage.tsx
+++ client/src/pages/Public/ResetPasswordPage/ResetPasswordPage.tsx
@@ -1,16 +1,19 @@
-import style from "../../stylePage.module.css";
-import NavRoot from "../../../components/layout/NavRoot/NavRoot";
-import ResetPasswordRoot from "../../../components/Public/ResetPasswordRoot/ResetPasswordRoot";
-import Footer from "../../../components/layout/footer/Footer.layout";
-import Helmet_QuiSuisJe_SEO from "../../../seo/QuiSuisJeSEO/HelmetQuiSuisJe.helmet.seo";
-import useScrollToHash from "../../../hook/useScrollToHash";
+/* Import des modules CSS */
+import style from "../../../stylePage.module.css";
 
-function ResetPasswordPage() {
-    useScrollToHash() /* Utilisation du hook pour gérer le scroll vers les sections avec des IDs */
+/* Import des Components */
+import ResetPasswordRoot from "../../../components/Public/ResetPasswordRoot/ResetPasswordRoot";
+import NavRoot from "../../../components/layout/NavRoot/NavRoot";
+import Footer_layout from "../../../components/layout/footer/Footer.layout";
+
+/* Import des Hooks */
+import useScrollToHash_utils_Hook from "../../../hook/useScrollToHash";
+
+function ResetPassword_Page() {
+    useScrollToHash_utils_Hook() /* Utilisation du hook pour gérer le scroll vers les sections avec des IDs */
 
     return (
-        <section className={`ResetPasswordPage ${style.Page}`}>
-            <Helmet_QuiSuisJe_SEO />
+        <section className={`ResetPassword_Page ${style.Page}`}>
             <header id="top" className={style.Header}>
                  <NavRoot />
             </header>
@@ -18,10 +21,10 @@ function ResetPasswordPage() {
                 <ResetPasswordRoot />
             </main>
             <footer className={style.Footer}>
-                <Footer />
+                <Footer_layout />
             </footer>
         </section>
     )
 }
 
-export default ResetPasswordPage;
+export default ResetPassword_Page;
