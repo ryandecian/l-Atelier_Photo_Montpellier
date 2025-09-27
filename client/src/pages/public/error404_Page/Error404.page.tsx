d4f753505ac477de8a258237076371b2ac8e5a71
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-09-27 18:42:02 +0200

    Correction CSS

--- client/src/pages/public/error404_Page/Error404.page.tsx
+++ client/src/pages/public/error404_Page/Error404.page.tsx
@@ -1,5 +1,5 @@
 /* Import des modules CSS */
-import style from "../../../stylePage.module.css";
+import style from "../../stylePage.module.css";
 
 /* Import des Components */
 import Error404Root from "../../../components/Public/Error404Root/Error404Root";
