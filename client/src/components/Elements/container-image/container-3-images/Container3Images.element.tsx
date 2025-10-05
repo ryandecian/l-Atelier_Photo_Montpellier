80ed99bbe0ee4a9874f96c6427d05c3320fbdb9b
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-05 19:50:17 +0200

    Mise a jour de Container3Images_Element

--- client/src/components/Elements/container-image/container-3-images/Container3Images.element.tsx
+++ client/src/components/Elements/container-image/container-3-images/Container3Images.element.tsx
@@ -2,7 +2,7 @@
 import css from "./container3Images.module.css";
 
 /* Import des composants d'Elements */
-import PremiumCardImgSudoRoot from "../../premium-card-img-modal/PremiumCardImgModal.element";
+import { PremiumCardImgModal_Element } from "../../premium-card-img-modal/PremiumCardImgModal.element";
 
 /* Import des Types */
 import { Container3Images_Type } from "../../../../types/elements/container3Images.type";
@@ -11,21 +11,21 @@ function Container3Images_Element({ img1, img2, img3, metaNameImg1, metaNameImg2
     return (
         <div className={css.Container3ImagesRoot}>
             <div className={css.ContainerImg}>
-                <PremiumCardImgSudoRoot
+                <PremiumCardImgModal_Element
                 src={img1}
                 alt={metaNameImg1}
                 className={css.Image}
                 />
             </div>
             <div className={css.ContainerImg}>
-                <PremiumCardImgSudoRoot
+                <PremiumCardImgModal_Element
                 src={img2}
                 alt={metaNameImg2}
                 className={css.Image}
                 />
             </div>
             <div className={css.ContainerImg}>
-                <PremiumCardImgSudoRoot
+                <PremiumCardImgModal_Element
                 src={img3}
                 alt={metaNameImg3}
                 className={css.Image}
