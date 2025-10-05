b2dcc39957026ee1bc9b5c3d7143fdbc053f8d5d
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-05 10:42:19 +0200

    Correction de l'indentation de PremiumCardImgModal_Element

--- client/src/components/Elements/premium-card-img-modal/PremiumCardImgModal.element.tsx
+++ client/src/components/Elements/premium-card-img-modal/PremiumCardImgModal.element.tsx
@@ -7,35 +7,47 @@ import { useState } from "react";
 /* Import des Types */
 import { PremiumCardImgModal_Type } from "../../../types/elements/premiumCardImgModal.type";
 
-function PremiumCardImgModal_Element ({src, alt, className}: PremiumCardImgModal_Type) {
-  const [isOpen, setIsOpen] = useState(false);
+function PremiumCardImgModal_Element({ src, alt, className }: PremiumCardImgModal_Type) {
+    const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <>
-      {/* Image cliquable */}
-      <img
-        src={src}
-        alt={alt}
-        className={`${className} ${css.img}`}
-        role="button"
-        onClick={() => setIsOpen(true)}
-        onContextMenu={(e) => e.preventDefault()}
-        onDragStart={(e) => e.preventDefault()}
-      />
+    return (
+        <>
+            {/* Image cliquable */}
+            <img
+                src={src}
+                alt={alt}
+                className={`${className} ${css.img}`}
+                role="button"
+                onClick={() => setIsOpen(true)}
+                onContextMenu={(e) => e.preventDefault()}
+                onDragStart={(e) => e.preventDefault()}
+            />
 
-      {/* Modal d'affichage en grand */}
-      {isOpen && (
-        <div className={css.modal} onClick={() => setIsOpen(false)}>
-          <div className={css.modalContent} onClick={(e) => e.stopPropagation()}>
-            <span className={css.close} onClick={() => setIsOpen(false)}>
-              &times;
-            </span>
-            <img src={src} alt={alt} className={css.fullImage}onContextMenu={(e) => e.preventDefault()} onDragStart={(e) => e.preventDefault()}/>
-          </div>
-        </div>
-      )}
-    </>
-  );
-};
+            {/* Modal d'affichage en grand */}
+            {isOpen && (
+                <div className={css.modal} onClick={() => setIsOpen(false)}>
+                    <div
+                        className={css.modalContent}
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        <span
+                            className={css.close}
+                            onClick={() => setIsOpen(false)}
+                        >
+                            &times;
+                        </span>
+                        <img
+                            src={src}
+                            alt={alt}
+                            className={css.fullImage}
+                            onContextMenu={(e) => e.preventDefault()}
+                            onDragStart={(e) => e.preventDefault()}
+                        />
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
 
 export { PremiumCardImgModal_Element };
