36e685054686dd533430b2edb4c710688fb9fe1d
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-05 19:56:29 +0200

    Mise a jour de GeneratorAlbumPhoto_Element

--- client/src/components/Elements/generator-album-photo/GeneratorAlbumPhoto.element.tsx
+++ client/src/components/Elements/generator-album-photo/GeneratorAlbumPhoto.element.tsx
@@ -3,7 +3,7 @@
 import css from "./generatorAlbumPhoto.module.css";
 
 /* Import des composants d'Elements */
-import PremiumCardImgSudoRoot from "../premium-card-img-modal/PremiumCardImgModal.element";
+import { PremiumCardImgModal_Element } from "../premium-card-img-modal/PremiumCardImgModal.element";
 
 /* Import des Types */
 import { DataAlbumPhoto_Type } from "../../../types/elements/albumPhoto.type";
@@ -13,7 +13,7 @@ function GeneratorAlbumPhoto_Element({ album }: DataAlbumPhoto_Type) {
         <div className={css.flexWrap}>
             {album.map((img, index) => (
                 <div key={index} className={css.ContainerImg}>
-                    <PremiumCardImgSudoRoot
+                    <PremiumCardImgModal_Element
                         src={img.src}
                         alt={img.alt}
                         className={css.Image}
