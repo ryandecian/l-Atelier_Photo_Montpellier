99d97d9f758547d901b97d817cd768cff4860f22
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-22 13:23:11 +0200

    Mise a jour de imagesPortraitDuo_Data

--- client/src/components/Public/portrait-duo/imagesPortraitDuo.data.ts
+++ client/src/components/Public/portrait-duo/imagesPortraitDuo.data.ts
@@ -8,24 +8,24 @@ import { ImagesBlog_Type } from "../../../types/components/blogs/imagesBlog.type
  * photographie-professionnelle : 2/2
  */
 
-const ImagesPortraitDuoControllerData: ImagesBlog_Type[] = [
+const imagesPortraitDuo_Data: ImagesBlog_Type[] = [
     /* Tableau 1 : PortraitDuoComponentA */
-    /* {ImagesPortraitDuoControllerData[0].src} */
+    /* {imagesPortraitDuo_Data[0].src} */
     {
         src: "/images/image-portrait-duo-couple/lapm-photographe-professionnel-portrait-duo-jeune-couple-studio-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographe professionnel - portrait duo - jeune couple - studio - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitDuoControllerData[1].src} */
+    /* {imagesPortraitDuo_Data[1].src} */
     {
         src: "/images/image-portrait-duo-couple/lapm-photographie-professionnel-portrait-duo-couple-studio-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographie professionnel - portrait duo - couple - studio - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitDuoControllerData[2].src} */
+    /* {imagesPortraitDuo_Data[2].src} */
     {
         src: "/images/image-portrait-duo-couple/lapm-photo-professionnel-portrait-duo-tandem-exterieur-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photo professionnel - portrait duo - tandem - extérieur - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitDuoControllerData[3].src} */
+    /* {imagesPortraitDuo_Data[3].src} */
     {
         src: "/images/image-portrait-duo-couple/lapm-photographie-professionnelle-portrait-duo-studio-couple-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographie professionnelle - portrait duo - studio - couple - L'Atelier Photo Montpellier",
@@ -34,26 +34,26 @@ const ImagesPortraitDuoControllerData: ImagesBlog_Type[] = [
 
 
     /* Tableau 2 : PortraitDuoComponentB */
-    /* {ImagesPortraitDuoControllerData[4].src} */
+    /* {imagesPortraitDuo_Data[4].src} */
     {
         src: "/images/image-portrait-duo-couple/lapm-photographe-professionnel-portrait-duo-flash-studio-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographe professionnel - portrait duo - flash studio - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitDuoControllerData[5].src} */
+    /* {imagesPortraitDuo_Data[5].src} */
     {
         src: "/images/image-portrait-duo-couple/lapm-photo-professionnel-portrait-duo-flash-studio-detente-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photo professionnel - portrait duo - flash studio - détente - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitDuoControllerData[6].src} */
+    /* {imagesPortraitDuo_Data[6].src} */
     {
         src: "/images/image-portrait-duo-couple/lapm-photographe-professionnelle-portrait-duo-exterieur-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographe professionnelle - portrait duo - extérieur - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitDuoControllerData[7].src} */
+    /* {imagesPortraitDuo_Data[7].src} */
     {
         src: "/images/image-portrait-duo-couple/lapm-photo-professionnel-portrait-duo-couple-montpellier-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photo professionnel - portrait duo - couple - Montpellier - L'Atelier Photo Montpellier",
     },
 ];
 
-export default ImagesPortraitDuoControllerData;
+export { imagesPortraitDuo_Data };
