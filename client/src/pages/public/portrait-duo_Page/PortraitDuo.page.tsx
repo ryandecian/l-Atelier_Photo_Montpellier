72331da43306ea43b7059f544fb99f7e9d8ab5b1
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-22 14:23:17 +0200

    Mise a jour de PortraitDuo_Page

--- client/src/pages/public/portrait-duo_Page/PortraitDuo.page.tsx
+++ client/src/pages/public/portrait-duo_Page/PortraitDuo.page.tsx
@@ -2,7 +2,7 @@
 import style from "../../stylePage.module.css";
 
 /* Import des Components */
-import PortraitDuoRoot from "../../../components/Public/portrait-duo/PortraitDuoRoot";
+import PortraitDuo_Root from "../../../components/Public/portrait-duo/PortraitDuo.root";
 import NavRoot from "../../../components/layout/NavRoot/NavRoot";
 import Footer_layout from "../../../components/layout/footer/Footer.layout";
 
@@ -22,7 +22,7 @@ function PortraitDuo_Page() {
                  <NavRoot />
             </header>
             <main className={style.Main}>
-                <PortraitDuoRoot />
+                <PortraitDuo_Root />
             </main>
             <footer className={style.Footer}>
                 <Footer_layout />
