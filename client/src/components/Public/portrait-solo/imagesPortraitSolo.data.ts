7ef07ef09ee81c0760545ba10f17356070aaddf4
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-22 13:42:50 +0200

    Mise a jour de imagesPortraitSolo_Data

--- client/src/components/Public/portrait-solo/imagesPortraitSolo.data.ts
+++ client/src/components/Public/portrait-solo/imagesPortraitSolo.data.ts
@@ -1,31 +1,24 @@
+/* Import des Types */
 import { ImagesBlog_Type } from "../../../types/components/blogs/imagesBlog.type";
 
-/**
- * Compteur mot clés : 
- * 
- * photographe-professionnel : 4/4
- * photo-professionnel : 2/2
- * photographie-professionnelle : 2/2
- */
-
-const ImagesPortraitSoloControllerData: ImagesBlog_Type[] = [
+const imagesPortraitSolo_Data: ImagesBlog_Type[] = [
     /* Tableau 1 : CorporateComponentA */
-    /* {ImagesPortraitSoloControllerData[0].src} */
+    /* {imagesPortraitSolo_Data[0].src} */
     {
         src: "/images/image-portrait-solo/lapm-photographe-professionnel-portrait-solo-en-jardin-lifestyle-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographe professionnel - portrait solo - en jardin - lifestyle - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitSoloControllerData[1].src} */
+    /* {imagesPortraitSolo_Data[1].src} */
     {
         src: "/images/image-portrait-solo/lapm-photographie-professionnelle-portrait-solo-homme-exterieur-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographie professionnelle - portrait solo - homme - extérieur - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitSoloControllerData[2].src} */
+    /* {imagesPortraitSolo_Data[2].src} */
     {
         src: "/images/image-portrait-solo/lapm-photographie-professionnelle-portrait-solo-jeune-fille-grabels-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographie professionnelle - portrait solo - jeune fille - Grabels - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitSoloControllerData[3].src} */
+    /* {imagesPortraitSolo_Data[3].src} */
     {
         src: "/images/image-portrait-solo/lapm-photographe-professionnel-portrait-solo-homme-lifestyle-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographe professionnel - portrait solo - homme - lifestyle - L'Atelier Photo Montpellier",
@@ -34,26 +27,26 @@ const ImagesPortraitSoloControllerData: ImagesBlog_Type[] = [
 
 
     /* Tableau 2 : CorporateComponentB */
-    /* {ImagesPortraitSoloControllerData[4].src} */
+    /* {imagesPortraitSolo_Data[4].src} */
     {
         src: "/images/image-portrait-solo/lapm-photo-professionnel-portrait-solo-clair-obscur-homme-studio-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photo professionnel - portrait solo - clair-obscur - homme - studio - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitSoloControllerData[5].src} */
+    /* {imagesPortraitSolo_Data[5].src} */
     {
         src: "/images/image-portrait-solo/lapm-photo-professionnel-portrait-solo-parc-de-fontcolombe-flash-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photo professionnel - portrait solo - parc de Fontcolombe - flash - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitSoloControllerData[6].src} */
+    /* {imagesPortraitSolo_Data[6].src} */
     {
         src: "/images/image-portrait-solo/lapm-photographe-professionnel-portrait-solo-exterieur-domicile-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographe professionnel - portrait solo - extérieur - domicile - L'Atelier Photo Montpellier",
     },
-    /* {ImagesPortraitSoloControllerData[7].src} */
+    /* {imagesPortraitSolo_Data[7].src} */
     {
         src: "/images/image-portrait-solo/lapm-photographe-professionnel-portrait-solo-homme-montpellier-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - photographe professionnel - portrait solo - homme - Montpellier - L'Atelier Photo Montpellier",
     },
 ];
 
-export default ImagesPortraitSoloControllerData;
+export { imagesPortraitSolo_Data };
