24ce7caa997e0ca07d9b572ac7ded5b1605b11b0
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-17 18:37:41 +0100

    Création d'un utils pour eviter la lecture de plusieur video en même temps

--- client/src/components/Public/video/Video.root.tsx
+++ client/src/components/Public/video/Video.root.tsx
@@ -3,19 +3,110 @@ import css from "./video.module.css";
 import style from "../../StyleRootComponent.module.css";
 
 function Video_Root() {
+    const currentVideo = e.currentTarget;
+
+    // Pause toutes les autres vidéos
+    const videos = document.querySelectorAll("video");
+
+    videos.forEach((video) => {
+        if (video !== currentVideo) {
+            video.pause();
+        }
+    });
     return (
         <section className={style.ContainerRootRacine}>
             <header className={style.ContainerTitle}>
                 <h1 className={style.TitleH1}>
-                    Vidéo Corporate & Institutionnelle à Montpellier – l'Atelier Photo Montpellier
+                    Vidéos d’Entreprise & de Mariage à Montpellier - Atelier Photo Montpellier
                 </h1>
             </header>
 
             <p className={style.TextP4}>
-                Chez l’Atelier Photo Montpellier, nous créons des vidéos corporate et institutionnelles pensées pour 
-                servir vos objectifs : attirer plus de clients, présenter vos équipes, renforcer votre crédibilité, 
-                clarifier vos services et améliorer votre visibilité.
+                A l’Atelier Photo Montpellier, nous créons des <strong>vidéos autant pour les entreprises</strong> afin 
+                de servir leurs objectifs : attirer plus de clients, présenter les équipes, renforcer la crédibilité, 
+                clarifier les services et améliorer la visibilité, que pour les moments de vie tels que votre mariage 
+                afin d’immortaliser toutes les sensations de ce jour exceptionnel.
             </p>
+
+            <p className={style.TextP4}>
+                Nous accompagnons tout public de la <strong>PME</strong> à la <strong>TPE</strong>, du dirigeant à 
+                l’explorateur désirant partager son aventure, des jeunes parents aux jeunes (ou moins jeunes) mariés !
+            </p>
+
+            <p className={style.TextP4}>
+                Chaque vidéo est conçue pour
+                <strong> mettre en scène une entreprise, un métier, un parcours et des valeurs, un moment de vie</strong>
+                en créant une narration visuelle qui donne envie de vous suivre.
+            </p>
+
+            <h2 className={style.TitleH2}>
+                Pourquoi faire appel à une vidéaste professionnelle pour votre société ?
+            </h2>
+
+            <h3 className={style.TitleLNH3}>
+                1. Booster vos conversions (l’argument que vos clients regardent en premier)
+            </h3>
+
+            <p className={style.TextP4}>
+                Une vidéo corporate bien conçue augmente fortement les réservations, les demandes de devis et les prises de contact.
+            </p>
+
+            <p className={style.TextLiP4}>
+                Elle répond immédiatement aux trois questions essentielles :
+            </p>
+
+            <ul className={style.ContainerUl}>
+                <li className={style.TextLiP4}>
+                    Est-ce que cette entreprise est fiable ?
+                </li>
+                <li className={style.TextLiP4}>
+                    Est-ce que je comprends clairement ce qu’elle fait ?
+                </li>
+                <li className={style.TextLiP4}>
+                    Est-ce que j’ai envie de travailler avec elle ?
+                </li>
+            </ul>
+
+            <p className={style.TextP4}>
+                Quand ces trois leviers sont activés, la conversion grimpe naturellement.
+            </p>
+
+            <section className={style.ContainerVideo}>
+                <h3 className={style.VideoTitle}>Vidéo : Les crocodiles jaunes</h3>
+                <video
+                    className={style.VideoPlayer}
+                    src="/video/les-crocodiles-jaunes/les-crocodiles-jaunes.mp4"
+                    controls
+                    preload="none"
+                    controlsList="nodownload"
+                    onPlay={handleVideoPlay}
+                    onContextMenu={(e) => e.preventDefault()} /* Désactive le clic droit */
+                    poster="/video/les-crocodiles-jaunes/les-crocodiles-jaunes.jpg"
+                    playsInline
+                />
+                <p className={style.VideoDescription}>
+                    Un cadre unique : découverte du centre de jeûne Les Crocodiles Jaunes à Albi.
+                </p>
+            </section>
+
+            <h3 className={style.TitleLNH3}>
+                2. Humaniser votre entreprise en valorisant les personnes qui la portent
+            </h3>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             <p className={style.TextLiP4}>
                 Nous accompagnons aussi bien :
@@ -47,54 +138,25 @@ function Video_Root() {
                 narration visuelle qui donne envie de vous suivre.
             </p>
 
-            <h2 className={style.TitleH2}>
-                Pourquoi opter pour une vidéo corporate pour votre communication ?
-            </h2>
-
-            <h3 className={style.TitleLNH3}>
-                1. Booster vos conversions (l’argument que vos clients regardent en premier)
-            </h3>
-
-            <p className={style.TextP4}>
-                Une vidéo corporate bien conçue augmente fortement les réservations, les demandes de devis et les prises 
-                de contact.
-            </p>
-            
-            <p className={style.TextLiP4}>
-                Elle répond immédiatement aux trois questions essentielles :
-            </p>
-
-            <ul className={style.ContainerUl}>
-                <li className={style.TextLiP4}>
-                    Est-ce que cette entreprise est fiable ?
-                </li>
-                <li className={style.TextLiP4}>
-                    Est-ce que je comprends clairement ce qu’elle fait ?
-                </li>
-                <li className={style.TextLiP4}>
-                    Est-ce que j’ai envie de travailler avec elle ?
-                </li>
-            </ul>
-
-            <p className={style.TextP4}>
-                Quand ces trois leviers sont activés, la conversion grimpe naturellement.
-            </p>
-
             <h3 className={style.TitleLNH3}>
                 Exemple — Vidéo “Mélodie du Désert”
             </h3>
 
-            <section className={css.ContainerVideo}>
+            <section className={style.ContainerVideo}>
+                <h3 className={style.VideoTitle}>Trek dans le désert du Maroc – Extrait vidéo</h3>
                 <video
-                    className={css.VideoPlayer}
-                    src="/video/video-trek-desert-maroc.mp4"
+                    className={style.VideoPlayer}
+                    src="/video/video-trek-desert-maroc/video-trek-desert-maroc.mp4"
                     controls
                     preload="none"
                     controlsList="nodownload"
                     onContextMenu={(e) => e.preventDefault()} /* Désactive le clic droit */
-                    poster="/video/video-trek-desert-maroc.jpg"
+                    poster="/video/video-trek-desert-maroc/video-trek-desert-maroc.jpg"
                     playsInline
                 />
+                <p className={style.VideoDescription}>
+                    Une courte vidéo présentant l’ambiance du trek dans les dunes marocaines.
+                </p>
             </section>
 
             <h3 className={style.TitleNH3R}>
