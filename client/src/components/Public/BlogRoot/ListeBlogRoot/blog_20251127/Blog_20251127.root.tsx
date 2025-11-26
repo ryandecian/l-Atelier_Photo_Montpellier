607aba29c800c530f493b4f4225b4c2d12c0ef53
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-26 23:20:32 +0100

    Initialisation de la page Blog_20251127_Root

---
+++ client/src/components/Public/BlogRoot/ListeBlogRoot/blog_20251127/Blog_20251127.root.tsx
@@ -0,0 +1,16 @@
+/* Import des modules CSS */
+import style from "../../../../StyleRootComponent.module.css";
+
+function Blog_20251127_Root() {
+    return (
+        <section className={`Blog_20251127_Root ${style.ContainerRootRacine}`}>
+            <header className={style.ContainerTitle}>
+                <h1 className={style.TitleH1}>
+                    {`Trouvez les Bons Profils : Comment la Vidéo vous fait Économiser sur vos Coûts de Recrutement`}
+                </h1>
+            </header>
+        </section>
+    )
+}
+
+export default Blog_20251127_Root;
