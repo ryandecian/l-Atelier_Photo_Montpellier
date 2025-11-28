54f1392f115a6e1a934b217a17abda9cb158443b
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-28 23:33:44 +0100

    Création de JSON_LD_Breadcrumb_Blog_20251127_schema_SEO

--- client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20251127_SEO/JSON-LD_Breadcrumb_Blog_20251127.schema.seo.ts
+++ client/src/seo/blog_SEO/liste-article-blog_SEO/blog-20251127_SEO/JSON-LD_Breadcrumb_Blog_20251127.schema.seo.ts
@@ -1,14 +1,14 @@
 /* Import des Components */
 import { dataSEO_Root_data_SEO } from "../../../dataSEO_Root.data.seo";
-import { dataSEO_Blog_20250917_data_SEO } from "./dataSEO_Blog_20251127.data.seo";
+import { dataSEO_Blog_20251127_data_SEO } from "./dataSEO_Blog_20251127.data.seo";
 
 /* Import des Types */
 import { DataSEORoot_Type } from "../../../../types/seo/dataSEORoot.type";
 import { DataSEOTargetOne_Type } from "../../../../types/seo/dataSEOTargetOne.type";
 
-function JSON_LD_Breadcrumb_Blog_20250917_schema_SEO() : string {
+function JSON_LD_Breadcrumb_Blog_20251127_schema_SEO() : string {
     const dataSEO_Root: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
-    const dataSEO_Blog_20250917: DataSEOTargetOne_Type = dataSEO_Blog_20250917_data_SEO(); /* Récupération des données SEO de la page */
+    const dataSEO_Blog_20251127: DataSEOTargetOne_Type = dataSEO_Blog_20251127_data_SEO(); /* Récupération des données SEO de la page */
 
     const JSON_LD = JSON.stringify({
         "@context": dataSEO_Root["@context"],
@@ -16,13 +16,13 @@ function JSON_LD_Breadcrumb_Blog_20250917_schema_SEO() : string {
         "itemListElement": [
             {
                 "@type": "ListItem",
-                "position": dataSEO_Blog_20250917.position,
-                "name": dataSEO_Blog_20250917.name_Breadcrumb, /* Le libellé du lien dans le fil d’Ariane. Exemple : Portfolio ou Portrait Duo */
+                "position": dataSEO_Blog_20251127.position,
+                "name": dataSEO_Blog_20251127.name_Breadcrumb, /* Le libellé du lien dans le fil d’Ariane. Exemple : Portfolio ou Portrait Duo */
                 "item": {
                     "@type": dataSEO_Root["@type"].WebPage,
-                    "@id": dataSEO_Blog_20250917.id_Service,
-                    "url": dataSEO_Blog_20250917.url,
-                    "name": dataSEO_Blog_20250917.name_Breadcrumb
+                    "@id": dataSEO_Blog_20251127.id_Service,
+                    "url": dataSEO_Blog_20251127.url,
+                    "name": dataSEO_Blog_20251127.name_Breadcrumb
                 }
             },
         ],
@@ -33,4 +33,4 @@ function JSON_LD_Breadcrumb_Blog_20250917_schema_SEO() : string {
     )
 }
 
-export { JSON_LD_Breadcrumb_Blog_20250917_schema_SEO };
+export { JSON_LD_Breadcrumb_Blog_20251127_schema_SEO };
