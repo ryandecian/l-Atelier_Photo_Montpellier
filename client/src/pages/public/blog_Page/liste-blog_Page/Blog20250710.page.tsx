0944ad865d51eaecbf1d0e3f99715dc7842e7598
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-02 12:17:33 +0100

    Changement nom dossier article de blog

--- client/src/pages/public/blog_Page/liste-blog_Page/Blog20250710.page.tsx
+++ client/src/pages/public/blog_Page/liste-blog_Page/Blog20250710.page.tsx
@@ -2,7 +2,7 @@
 import style from "../../../stylePage.module.css";
 
 /* Import des Components */
-import Blog_20250710_Root from "../../../../components/Public/blog-root/ListeBlogRoot/Blog_20250710_Root/Blog_20250710.root";
+import Blog_20250710_Root from "../../../../components/Public/blog-root/list-blog-root/Blog_20250710_Root/Blog_20250710.root";
 import NavRoot from "../../../../components/layout/NavRoot/NavRoot";
 import Footer_layout from "../../../../components/layout/footer/Footer.layout";
 
