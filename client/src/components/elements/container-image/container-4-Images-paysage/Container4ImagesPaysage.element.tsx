2554d0210deb156165684d257df20fdc84df8072
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-02 22:49:48 +0100

    Création de Container4ImagesPaysage_Element

---
+++ client/src/components/elements/container-image/container-4-Images-paysage/Container4ImagesPaysage.element.tsx
@@ -0,0 +1,51 @@
+/* Import des modules CSS */
+import css from "./container4Imagesportrait.module.css";
+
+/* Import des composants d'Elements */
+import { PremiumCardImgModal_Element } from "../../premium-card-img-modal/PremiumCardImgModal.element";
+
+/* Import des Types */
+import { Container4Images_Type } from "../../../../types/elements/container4Images.type";
+
+function Container4ImagesPaysage_Element(
+    {
+        img1, img2, img3, img4,
+        metaNameImg1, metaNameImg2, metaNameImg3, metaNameImg4
+    }: Container4Images_Type) {
+
+
+    return (
+        <div className={css.Container4ImagesRoot}>
+            <div className={css.ContainerImg}>
+                <PremiumCardImgModal_Element
+                src={img1}
+                alt={metaNameImg1}
+                className={css.Image}
+                />
+            </div>
+            <div className={css.ContainerImg}>
+                <PremiumCardImgModal_Element
+                src={img2}
+                alt={metaNameImg2}
+                className={css.Image}
+                />
+            </div>
+            <div className={css.ContainerImg}>
+                <PremiumCardImgModal_Element
+                src={img3}
+                alt={metaNameImg3}
+                className={css.Image}
+                />
+            </div>
+            <div className={css.ContainerImg}>
+                <PremiumCardImgModal_Element
+                src={img4}
+                alt={metaNameImg4}
+                className={css.Image}
+                />
+            </div>
+        </div>
+    );
+}
+
+export { Container4ImagesPaysage_Element };
