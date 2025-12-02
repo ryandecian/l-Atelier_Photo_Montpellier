a1ed6860c843574aedb2d12509a9672dbe2f41d2
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-02 11:07:34 +0100

    Mise a jour de la documentation de imagesBlog_20250710_img_data

--- client/src/components/Public/BlogRoot/ListeBlogRoot/Blog_20250710_Root/imagesBlog_20250710.img.data.ts
+++ client/src/components/Public/BlogRoot/ListeBlogRoot/Blog_20250710_Root/imagesBlog_20250710.img.data.ts
@@ -9,17 +9,17 @@ import { ImagesBlog_Type } from "../../../../../types/components/blogs/imagesBlo
 */
 
 const imagesBlog_20250710_img_data: ImagesBlog_Type[] = [
-    /* {ImagesBlog_20250710_RootControllerData[0].src} */
+    /* {imagesBlog_20250710_img_data[0].src} */
     {
         src: "/images/image-blog/blog_20250710/lapm-photo-portrait-feel-good-professionnel-montpellier-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - Photographe professionnel à Montpellier - Portrait Feel Good - L'Atelier Photo Montpellier",
     },
-    /* {ImagesBlog_20250710_RootControllerData[1].src} */
+    /* {imagesBlog_20250710_img_data[1].src} */
     {
         src: "/images/image-blog/blog_20250710/lapm-photographie-professionnelle-portrait-jeune-homme-fellgood-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - Photographie professionnelle à Montpellier - Portrait Jeune Homme Feel Good - L'Atelier Photo Montpellier",
     },
-    /* {ImagesBlog_20250710_RootControllerData[2].src} */
+    /* {imagesBlog_20250710_img_data[2].src} */
     {
         src: "/images/image-blog/blog_20250710/lapm-photographie-professionnelle-portrait-lifestyle-exterieur-flash-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - Photographie professionnelle à Montpellier - Portrait Lifestyle Extérieur Flash - L'Atelier Photo Montpellier",
