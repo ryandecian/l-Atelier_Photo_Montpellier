90e204b24348f2aab4d47cf1342ee571685f884a
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-14 16:22:47 +0100

    Ajout des .env et amélioration du fichier vite config

--- client-kaleidopix/vite.config.ts
+++ client-kaleidopix/vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+/* Ce fichier de config ne s'applique que lorsque que l'on fait npm run dev, pas en production donc aucun problème */
+
+export default defineConfig(() => {
+
+    /* Récupère le chemin absolue ou est exécuté ce fichier */
+    const root = process.cwd();
+
+    /* On récupère la variable d'environnement injectée par Docker */
+    const viteNodeEnv = process.env.VITE_NODE_ENV || 'development';
+
+    /* Typage clé valeur des variables d'environnements */
+    let envDev: Record<string, string> = {};
+
+    /* Initialisation conditionnelle et chargement des variables d'environnement en fonction du mode (development) */
+    /* depuis .env.development et .env (fallback), filtrées sur le préfixe VITE_. */
+    if (viteNodeEnv === 'development') {
+        envDev = loadEnv('development', root, 'VITE_'); // .env.development
+    } 
+
+
+    /* Fusion manuelle des variables (ordre de priorité : configurable, premier lu premier chargé) */
+    const env: Record<string, string> = {
+        ...envDev,
+    };
+
+    /* ✅ Injection dans process.env pour un usage possible dans ce fichier */
+    for (const [key, value] of Object.entries(env)) {
+        process.env[key] = value;
+   }
+
+    return {
+        plugins: [react()],
+        server: {
+            host: true,
+            port: 4500,
+        },
+  };
+});
