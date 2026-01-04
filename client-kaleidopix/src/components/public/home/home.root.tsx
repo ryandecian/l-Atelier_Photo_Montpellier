0237708100872b94b8a1e9d4ae96fc13660665eb
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-01-04 18:12:36 +0100

    Mise a jour des style

--- client-kaleidopix/src/components/public/home/home.root.tsx
+++ client-kaleidopix/src/components/public/home/home.root.tsx
@@ -1,8 +1,9 @@
-
+/* Import des modules CSS */
+import style from "../../style.root.module.css";
 
 function Home_Root() {
     return (
-        <section>
+        <section className={`Home_Root ${style.ContainerRootRacine}`}>
             <h1>Bienvenue chez Kaleidopix</h1>
             <p>Votre destination pour des services photographiques professionnels.</p>
         </section>
