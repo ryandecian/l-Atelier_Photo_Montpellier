c39486a893a8d3717a23dafe7e33538e5fd772f1
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-01-04 18:55:42 +0100

    Intégration des nouveau font-family

--- client-kaleidopix/src/components/public/home/home.root.tsx
+++ client-kaleidopix/src/components/public/home/home.root.tsx
@@ -4,8 +4,12 @@ import style from "../../style.root.module.css";
 function Home_Root() {
     return (
         <section className={`Home_Root ${style.ContainerRootRacine}`}>
-            <h1>Bienvenue chez Kaleidopix</h1>
-            <p>Votre destination pour des services photographiques professionnels.</p>
+            <header className={style.ContainerTitle}>
+                <h1 className={style.TitleH1}>
+                    Bienvenue chez Kaleidopix
+                </h1>
+            </header>
+            <p className={style.TextP4}>Votre destination pour des services photographiques professionnels.</p>
         </section>
     )
 }
