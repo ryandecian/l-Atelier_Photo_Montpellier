51b2c5fd432a750d3e488d751ccd97ffc36554ef
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-01-20 22:31:05 +0100

    Création des composants RouterExt

---
+++ client-kaleidopix/src/types/router/routerExt.type.ts
@@ -0,0 +1,20 @@
+type RouterExt_Type = {
+    // emailRyan: string;
+    
+    emailKaleidopix: string;
+    // phonePowerWatt34: string;
+    // linkPowerWatt34: string;
+    // adressPowerWatt34: string;
+
+    // instagram: string;
+    // facebook: string;
+
+    // phoneInfomaniak: string;
+    // adressInfomaniak: string;
+
+    // loi_01: string;
+    // loi_02: string;
+    // loi_03: string;
+};
+
+export type { RouterExt_Type };
