aa1668babaa369351a112b22fa32e4c72cf68beb
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-01-24 10:54:15 +0100

    Création de useMediaQueriesHeight_rem

---
+++ client-kaleidopix/src/components/public/home/components/Home_A.component.tsx
@@ -0,0 +1,65 @@
+/* Import des modules CSS */
+import css from "./home_A.module.css";
+
+/* Import des Hooks */
+import useMediaQueriesHeight_rem from "../../../../hook/useMediaQueriesHeight_rem.utils.hook";
+
+function Home_A_Component() {
+    const viewportHeight: number = useMediaQueriesHeight_rem();
+
+    return (
+        <section className={`Home_A_Component ${css.ContainerAccueil}`}>
+            <div className={css.ContainerSubAccueil}>
+
+                {/* MediaQuery maison 450px */}
+                {viewportHeight <= 28.125 && (
+                    <>
+                        <article className={css.ContainerSloganMobile}>
+                            <p className={css.SloganMobile}>
+                                {"DES PORTRAITS"}
+                            </p>
+                            <p className={css.SloganMobile}>
+                                {"QUI"}
+                            </p>
+                            <p className={css.SloganMobile}>
+                                {"PARLENT DE"}
+                            </p>
+                            <p className={css.SloganMobile}>
+                                {"VOUS"}
+                            </p>
+                        </article>
+                        <header className={css.ContainerTitleMobile}>
+                            <h1>PHOTOGRAPHE DE PORTRAITS ET DE MARIAGE A MONTPELLIER</h1>
+                        </header>
+                    </>
+                )}
+
+
+                {/* MediaQuery maison 450px */}
+                {viewportHeight > 28.125 && (
+                    <>
+                        <article className={css.ContainerSlogan}>
+                            <p className={css.Slogan}>
+                                {"DES PORTRAITS"}
+                            </p>
+                            <p className={css.Slogan}>
+                                {"QUI"}
+                            </p>
+                            <p className={css.Slogan}>
+                                {"PARLENT DE"}
+                            </p>
+                            <p className={css.Slogan}>
+                                {"VOUS"}
+                            </p>
+                        </article>
+                        <header className={css.ContainerTitle}>
+                            <h1>Photographe et Vidéaste professionnelle pour contenus visuels à Montpellier</h1>
+                        </header>
+                    </>
+                )}
+            </div>
+        </section>
+    );
+}
+
+export { Home_A_Component };
