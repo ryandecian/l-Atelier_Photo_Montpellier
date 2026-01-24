1de06ae6b884c5f132705d24442a789ab7e736f1
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-01-24 15:44:38 +0100

    Mise a jour de la page Home

--- client-kaleidopix/src/pages/public/home_Page/Home.page.tsx
+++ client-kaleidopix/src/pages/public/home_Page/Home.page.tsx
@@ -3,7 +3,7 @@ import style from "../../stylePage.module.css";
 
 /* Import des Components */
 import Nav_root_Layout from "../../../components/layout/nav_Layout/Nav.root.layout";
-import Home_Root from "../../../components/public/home/Hhome.root";
+import Home_Root from "../../../components/public/home/home.root";
 
 function Home_Page() {
      return (
