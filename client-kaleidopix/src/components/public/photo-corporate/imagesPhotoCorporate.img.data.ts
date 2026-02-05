bc53b0fa6cf169936bd34068400ba9712cc3acc5
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-02-05 16:59:32 +0100

    Mise a jour de imagesPhotoCorporate_img_data, intégration de 4 nouvelles photos

--- client-kaleidopix/src/components/public/photo-corporate/imagesPhotoCorporate.img.data.ts
+++ client-kaleidopix/src/components/public/photo-corporate/imagesPhotoCorporate.img.data.ts
@@ -13,23 +13,23 @@ const imagesPhotoCorporate_img_data: Images_data_Type[] = [
     /* Tableau 1 d'images : 4 images */
     /* {imagesPhotoCorporate_img_data[0].src} */
     {
-        src: "/images/image-photo-corporate/portrait-corporate-professionnelle-fond-gris-kaleidopix.jpg",
-        alt: "Portrait corporate professionnelle fond gris - Kaleidopix",
+        src: "/images/image-photo-corporate/portrait-linkdin-chef-entreprise-developpement-web-montpellier-kaleidopix.jpg",
+        alt: "Portrait Linkdin chef entreprise développement web Montpellier - Kaleidopix",
     },
     /* {imagesPhotoCorporate_img_data[1].src} */
     {
-        src: "/images/image-photo-corporate/portrait-corporate-professionnelle-style-moderne-kaleidopix.jpg",
-        alt: "Portrait corporate professionnelle style moderne - Kaleidopix",
+        src: "/images/image-photo-corporate/portrait-chef-entreprise-developpement-web-montpellier-kaleidopix.jpg",
+        alt: "Portrait chef entreprise développement web Montpellier - Kaleidopix",
     },
     /* {imagesPhotoCorporate_img_data[2].src} */
     {
-        src: "/images/image-photo-corporate/portrait-corporate-professionnelle-environnement-travail-kaleidopix.jpg",
-        alt: "Portrait corporate professionnelle en environnement de travail - Kaleidopix",
+        src: "/images/image-photo-corporate/photo-collaborateur-service-qualité-montpellier-kaleidopix.jpg",
+        alt: "Photo collaborateur service qualité Montpellier - Kaleidopix",
     },
     /* {imagesPhotoCorporate_img_data[3].src} */
     {
-        src: "/images/image-photo-corporate/portrait-corporate-professionnelle-fond-sombre-kaleidopix.jpg",
-        alt: "Portrait corporate professionnelle fond sombre - Kaleidopix",
+        src: "/images/image-photo-corporate/portrait-cv-entreprise-communication-web-montpellier-kaleidopix.jpg",
+        alt: "Portrait CV entreprise communication web Montpellier - Kaleidopix",
     },
 ];
 
