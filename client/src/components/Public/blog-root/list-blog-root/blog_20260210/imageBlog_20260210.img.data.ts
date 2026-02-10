2f3bceb8d76f9ae388a0b1c0769d44f98217717c
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-02-10 22:34:57 +0100

    Intégration des 8 nouvelles images du blog dans le projet

--- client/src/components/Public/blog-root/list-blog-root/blog_20260210/imageBlog_20260210.img.data.ts
+++ client/src/components/Public/blog-root/list-blog-root/blog_20260210/imageBlog_20260210.img.data.ts
@@ -9,18 +9,43 @@ const imagesBlog_20260210_img_data: ImagesBlog_Type[] = [
     },
     /* {ImagesBlog_20260210_RootControllerData[1].src} */
     {
-        src: "/images/image-blog/blog_20260210/.jpg",
-        alt: "",
+        src: "/images/image-blog/blog_20260210/photo-professionnel-mariage-préparatifs-maquillage-l-atelier-photo-montpellier.jpg",
+        alt: "Photo professionnel de mariage - Préparatifs maquillage - L'Atelier Photo Montpellier",
     },
     /* {ImagesBlog_20260210_RootControllerData[2].src} */
     {
-        src: "/images/image-blog/blog_20260210/.jpg",
-        alt: "",
+        src: "/images/image-blog/blog_20260210/photo-professionnel-mariage-préparatifs-maries-l-atelier-photo-montpellier.jpg",
+        alt: "Photo professionnel de mariage - Préparatifs mariés - L'Atelier Photo Montpellier",
     },
     /* {ImagesBlog_20260210_RootControllerData[3].src} */
     {
+        src: "/images/image-blog/blog_20260210/photo-professionnelle-bagues-mariage-préparatifs-l-atelier-photo-montpellier.jpg",
+        alt: "Photo professionnelle bagues mariage préparatifs - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20260210_RootControllerData[4].src} */
+    {
+        src: "/images/image-blog/blog_20260210/photo-bagues-mariage-préparatifs-l-atelier-photo-montpellier.jpg",
+        alt: "Photo bagues mariage préparatifs - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20260210_RootControllerData[5].src} */
+    {
+        src: "/images/image-blog/blog_20260210/photo-professionnel-mariage-maquillage-salon-maries-l-atelier-photo-montpellier.jpg",
+        alt: "Photo professionnel de mariage - Maquillage salon mariés - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20260210_RootControllerData[6].src} */
+    {
         src: "/images/image-blog/blog_20260210/.jpg",
         alt: "",
+    },
+    /* {ImagesBlog_20260210_RootControllerData[7].src} */
+    {
+        src: "/images/image-blog/blog_20260210/lapm-photographe-professionnel-portrait-mariage-bouquet-mariee-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photographe professionnel portrait mariage bouquet mariée - L'Atelier Photo Montpellier",
+    },
+    /* {ImagesBlog_20260210_RootControllerData[8].src} */
+    {
+        src: "/images/image-blog/blog_20260210/photo-professionnelle-alliances-mariage-préparatifs-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Photo professionnelle alliances mariage préparatifs - L'Atelier Photo Montpellier",
     },
 ];
 
