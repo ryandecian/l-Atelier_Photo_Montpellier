003c096416bc4b2cd15687c5784fc8f932c53dd9
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-02-10 21:42:32 +0100

    Création du style css spécifique a la page blog

--- client/src/components/Public/blog-root/list-blog-root/blog_20260210/Blog_20260210.root.tsx
+++ client/src/components/Public/blog-root/list-blog-root/blog_20260210/Blog_20260210.root.tsx
@@ -1,5 +1,12 @@
 /* Import des modules CSS */
 import style from "../../../../StyleRootComponent.module.css";
+import css from "./blog_20260210.module.css";
+
+/* Import des Components de Data */
+import { imagesBlog_20260210_img_data } from "./imageBlog_20260210.img.data";
+
+/* Import des composants d'Elements */
+import { PremiumCardImgModal_Element } from "../../../../elements/premium-card-img-modal/PremiumCardImgModal.element";
 
 function Blog_20260210_Root() {
     return (
@@ -20,7 +27,6 @@ function Blog_20260210_Root() {
 
             {/* Images x 4 */}
 
-            
             <h2 className={style.TitleH2}>
                 Pourquoi immortaliser les préparatifs de votre union ?
             </h2>
@@ -44,9 +50,9 @@ function Blog_20260210_Root() {
                 , par la fébrilité intérieure des acteurs et la concentration qu’elle occasionne. Car l’impatience 
                 sous-tendue des futurs mariés est un moteur de réussite invisible de la <strong>photographie de mariage</strong>.
             </p>
-            
+
             {/* Images x 4 */}
-            
+
             <h3 className={style.TitleLNH3}>
                 Les regards avant le grand basculement
             </h3>
@@ -55,6 +61,84 @@ function Blog_20260210_Root() {
                 Avant les échanges de la cérémonie, il y a les regards des préparatifs. Des regards libres, furtifs, 
                 adressés à personne en particulier. Ils racontent le couple avant qu’il soit exposé aux yeux de tous.
             </p>
+
+            <h3 className={style.TitleLNH3}>
+                La joie attendue… et déjà présente
+            </h3>
+
+            <p className={style.TextP4}>
+                On parle souvent de la joie comme d’un moment à venir. Mais ici, elle est déjà là : un sourire sans 
+                raison, une légèreté soudaine. C’est une joie en devenir, solide et intime.
+            </p>
+
+            <h2 className={style.TitleH2}>
+                Est-ce que j’ai vraiment besoin de photos des préparatifs ?
+            </h2>
+
+            <p className={style.TextP4}>
+                Les photos des préparatifs de mariage sont souvent celles que l’on hésite le plus à inclure, alors 
+                qu’elles sont aussi celles qui donnent le plus de profondeur et de cohérence à l’ensemble du {` `}
+                <strong>reportage photo</strong>. La vraie question est : souhaitez-vous que votre reportage commence 
+                au début de l’histoire, ou en cours de route ? Les préparatifs servent à poser une base, une atmosphère. 
+                Quelques images suffisent à donner du sens à tout ce qui suivra : la cérémonie, la réception et les 
+                échanges avec les invités.
+            </p>
+
+            <h2 className={style.TitleH2}>
+                La FAQ des préparatifs : Durée, Lieu, Stress supplémentaire
+            </h2>
+
+            <h3 className={style.TitleLNH3}>
+                Combien de temps faut-il pour photographier les préparatifs ?
+            </h3>
+
+            <p className={style.TextLiP4}>
+                Il n’est pas nécessaire d’y consacrer des heures. Dans la majorité des mariages que je photographie à 
+                Montpellier, dans l’Hérault et ailleurs  le temps à compter est d’environ :
+            </p>
+            
+            <ul className={style.ContainerUl}>
+                <li className={style.TextLiP4}>
+                    <strong>30 à 45 minutes</strong> pour capter l’essentiel.
+                </li>
+                <li className={style.TextLiP4}>
+                    <strong>1 heure</strong> pour un vrai confort, sans alourdir la journée.
+                </li>
+            </ul>
+
+            <p className={style.TextP4}>
+                L’objectif est de saisir l’ambiance, les transitions et les moments calmes avant l’intensité.
+            </p>
+
+            <h3 className={style.TitleLNH3}>
+                Quels endroits choisir pour les préparatifs ?
+            </h3>
+
+            <p className={style.TextP4}>
+                Il n’existe pas de lieu parfait, mais certains choix facilitent le <strong>shooting</strong> : une pièce 
+                avec de la <strong>lumière naturelle</strong> et un espace relativement calme. Que ce soit une maison de 
+                famille ou un hôtel, ce qui compte c'est que le cadre soit "habitable" pour vous.
+            </p>
+
+            <h2 className={style.TitleH2}>
+                Les préparatifs sont-ils stressants si je n’aime pas être photographié ?
+            </h2>
+
+            <p className={style.TextP4}>
+                C’est une inquiétude légitime. Mais les préparatifs ne sont pas une séance photo posée. Mon approche est 
+                simple : être présente sans envahir, observer plus que diriger. La plupart des couples me disent après 
+                coup que ce moment a été plus apaisant qu’ils ne l’imaginaient grâce à cette <strong>bienveillance</strong>.
+            </p>
+
+            <h2 className={style.TitleH2}>
+                De l’écran au papier : prolonger l’émotion des préparatifs
+            </h2>
+
+            <PremiumCardImgModal_Element
+                src={imagesBlog_20260210_img_data[0].src}
+                alt={imagesBlog_20260210_img_data[0].alt}
+                className={css.img}
+            />
         </section>
     )
 }
--- client/src/components/Public/blog-root/list-blog-root/blog_20260210/imageBlog_20260210.img.data.ts
+++ client/src/components/Public/blog-root/list-blog-root/blog_20260210/imageBlog_20260210.img.data.ts
@@ -4,8 +4,8 @@ import { ImagesBlog_Type } from "../../../../../types/components/blogs/imagesBlo
 const imagesBlog_20260210_img_data: ImagesBlog_Type[] = [
     /* {ImagesBlog_20260210_RootControllerData[0].src} */
     {
-        src: "/images/image-blog/blog_20260210/.jpg",
-        alt: "",
+        src: "/images/image-blog/blog_20260210/lapm-album-photo-mariage-l-atelier-photo-montpellier.jpg",
+        alt: "LAPM - Album photo de mariage - L'Atelier Photo Montpellier",
     },
     /* {ImagesBlog_20260210_RootControllerData[1].src} */
     {
