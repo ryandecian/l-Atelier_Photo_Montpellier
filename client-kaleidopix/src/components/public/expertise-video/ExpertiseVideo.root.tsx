3748dc8142469043d09d49fa5867dae8b5132e2f
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-02-11 15:18:05 +0100

    Initialisation de ExpertiseVideo_Root

---
+++ client-kaleidopix/src/components/public/expertise-video/ExpertiseVideo.root.tsx
@@ -0,0 +1,16 @@
+/* Import des modules CSS */
+import style from "../../style.root.module.css";
+
+function ExpertiseVideo_Root() {
+    return (
+        <section className={`PhotoCorporate_Root ${style.ContainerRootRacine}`}>
+            <header className={style.ContainerTitle}>
+                <h1 className={style.TitleH1_NNNN}>
+                    FAQ : tout savoir sur la vidéo corporate à Montpellier
+                </h1>
+            </header>
+        </section>
+    )
+}
+
+export default ExpertiseVideo_Root
