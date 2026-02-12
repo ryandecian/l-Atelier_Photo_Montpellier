9d1eb369f499d4908d4d408e8b65733ac391e82f
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-02-12 21:23:49 +0100

    Création de Tarif_Root

---
+++ client-kaleidopix/src/components/public/tarif/Tarif.root.tsx
@@ -0,0 +1,13 @@
+
+
+function Tarif_Root() {
+    return (
+        <section className={`Tarif_Root`}>
+            <header>
+                <h1>Tarif_Root</h1>
+            </header>
+        </section>
+    )
+}
+
+export default Tarif_Root;
