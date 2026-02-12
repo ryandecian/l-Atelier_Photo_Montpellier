b6dde18297729ddcb8d4e80866ee0d8449a9ec62
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-02-12 21:25:22 +0100

    Mise a jour de Tarif_Root

--- client-kaleidopix/src/components/public/tarif/Tarif.root.tsx
+++ client-kaleidopix/src/components/public/tarif/Tarif.root.tsx
@@ -1,10 +1,13 @@
-
+/* Import des modules CSS */
+import style from "../../style.root.module.css";
 
 function Tarif_Root() {
     return (
-        <section className={`Tarif_Root`}>
-            <header>
-                <h1>Tarif_Root</h1>
+        <section className={`Tarif_Root ${style.ContainerRootRacine}`}>
+            <header className={style.ContainerTitle}>
+                <h1 className={style.TitleH1_NNNN}>
+                    Tarif_Root
+                </h1>
             </header>
         </section>
     )
