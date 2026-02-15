99fa69350311f17cc69824f77471e06848a27d68
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-02-15 18:06:45 +0100

    Création de TarifCardProps

--- client-kaleidopix/src/components/elements/tarif-card/tarifCard.element.type.ts
+++ client-kaleidopix/src/components/elements/tarif-card/tarifCard.element.type.ts
@@ -1,3 +1,4 @@
+/* Type utilisée dans les composants data */
 type TarifCard_element_Type = {
     title: string;
     price: string;
@@ -7,3 +8,11 @@ type TarifCard_element_Type = {
 }[]
 
 export type { TarifCard_element_Type };
+
+
+/* Type utilisée dans le composant élément TarifCard.element.tsx */
+type TarifCardProps = {
+    items: TarifCard_element_Type;
+};
+
+export type { TarifCardProps };
