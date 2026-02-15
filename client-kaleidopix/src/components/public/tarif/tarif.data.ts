ff77aa00a6a82bc48bb56327731c3ce2d7737ac3
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-02-15 18:27:38 +0100

    Création de la première carte de prix pour les prestations de videos : TarifCard_element_Type

--- client-kaleidopix/src/components/public/tarif/tarif.data.ts
+++ client-kaleidopix/src/components/public/tarif/tarif.data.ts
@@ -3,6 +3,30 @@ import type { TarifCard_element_Type } from "../../elements/tarif-card/tarifCard
 
 const tarif_Data: TarifCard_element_Type = [
     {
+        title: "Pack Essentiel",
+        price:"À partir de 1 200 € HT",
+        description: [
+            {
+                item: "Échange de cadrage et définition de l’objectif",
+            },
+            {
+                item: "Une demi-journée de tournage sur site à Montpellier ou alentours",
+            },
+            {
+                item: "Interview et images d’activité",
+            },
+            {
+                item: "1 vidéo principale (1 à 2 minutes)",
+            },
+            {
+                item: "Montage professionnel et étalonnage",
+            },
+            {
+                item: "Livraison optimisée pour site internet",
+            },
+        ],
+    },
+    {
         title: "",
         price:"",
         description: [
@@ -11,9 +35,9 @@ const tarif_Data: TarifCard_element_Type = [
             },
             {
                 item: "",
-            }
+            },
         ],
-    }
+    },
 ]
 
 export { tarif_Data };
