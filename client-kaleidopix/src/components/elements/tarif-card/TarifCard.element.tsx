133eda654ffb2a734040af1f5547fc5ccb7809bc
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-02-15 18:18:19 +0100

    Création de TarifCard_Element, le composant est prêt a être testé

--- client-kaleidopix/src/components/elements/tarif-card/TarifCard.element.tsx
+++ client-kaleidopix/src/components/elements/tarif-card/TarifCard.element.tsx
@@ -1,30 +1,31 @@
 /* Import des modules CSS */
 import css from "./tarifCard.module.css";
 
-function TarifCard_Element() {
+/* Import des Types */
+import type { TarifCard_props_Type } from "./tarifCard.element.type";
+
+function TarifCard_Element({ items }: TarifCard_props_Type) {
         return (
             <section className={css.container}>
-                <article className={css.card}>
-                    <h2 className={css.TitleH2}>
-                        Essentiel
-                    </h2>
+                {items.map((data, index) => (
+                    <article key={index} className={css.card}>
+                        <h2 className={css.TitleH2}>
+                            {data.title}
+                        </h2>
 
-                    <p className={css.TextP4}>
-                        <strong>À partir de 1 200 € HT</strong>
-                    </p>
+                        <p className={css.TextP4}>
+                            <strong>{data.price}</strong>
+                        </p>
 
-                    <ul className={css.list}>
-                        <li className={css.item}>
-                            Échange de cadrage et définition de l’objectif
-                        </li>
-                        <li className={css.item}>
-                            Une demi-journée de tournage sur site à Montpellier ou alentours
-                        </li>
-                        <li className={css.item}>
-                            Interview et images d’activité
-                        </li>
-                    </ul>
-                </article>
+                        <ul className={css.list}>
+                            {data.description.map((desc, descIndex) => (
+                                <li key={descIndex} className={css.item}>
+                                    {desc.item}
+                                </li>
+                            ))}
+                        </ul>
+                    </article>
+                ))}
             </section>
         )
 }
