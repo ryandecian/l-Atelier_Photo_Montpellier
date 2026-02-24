8b57dbd040a8e9d090eb53f9e976218574ce7b81
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-02-24 13:34:52 +0100

    Initialisation du SEO global du site, création et initialisation des datas SEO root

---
+++ client-kaleidopix/src/seo/dataSEO_Root.data.seo.ts
@@ -0,0 +1,7 @@
+/* Import des composants Router */
+import routerExt from "../router/routerExt.router";
+import router from "../router/router";
+
+
+/* Import des Types */
+import type { DataSEORoot_Type } from "../types/seo/dataSEORoot.type";
