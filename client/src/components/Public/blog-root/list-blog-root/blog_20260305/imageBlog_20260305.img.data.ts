582595b48c5cbe4ba0e14a24edfe21492881e43e
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-03-06 09:36:28 +0100

    Création de imagesBlog_20260305_img_data

---
+++ client/src/components/Public/blog-root/list-blog-root/blog_20260305/imageBlog_20260305.img.data.ts
@@ -0,0 +1,12 @@
+/* Import des Types */
+import { ImagesBlog_Type } from "../../../../../types/components/blogs/imagesBlog.type";
+
+const imagesBlog_20260305_img_data: ImagesBlog_Type[] = [
+    /* {ImagesBlog_20260305_RootControllerData[0].src} */
+    {
+        src: "/images/image-blog/blog_20260305/lapm-10-criteres-essentiels-choix-photographe-atelier-photo-montpellier.jpg",
+        alt: "LAPM - 10 critères essentiels pour choisir un photographe - L'Atelier Photo Montpellier",
+    },
+];
+
+export { imagesBlog_20260305_img_data };
