d5dcc3075aaf310dc948135da02292d9d5e09623
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-03-06 20:49:14 +0100

    Refactorisation de navMenu_Data, modification du router

--- client/src/components/layout/nav_Layout/navMenu.data.ts
+++ client/src/components/layout/nav_Layout/navMenu.data.ts
@@ -1,5 +1,5 @@
 /* Import des composants Router */
-import ListDataRouter from "../../../router/router";
+import router from "../../../router/router";
 
 /* Import des Types */
 import type { NavMenu_data_Type } from "../../../types/layout/navMenu.data.type";
@@ -10,18 +10,18 @@ function navMenu_Data(): NavMenu_data_Type {
             {
                 nameMenu: "PRESTATIONS",
                 nameSubMenu: [
-                    { title: "Corporate", link: ListDataRouter[11].path },
-                    { title: "LifeStyle", link: ListDataRouter[12].path },
-                    { title: "Mariage", link: ListDataRouter[13].path },
-                    { title: "Vidéo", link: ListDataRouter[36].path },
+                    { title: "Corporate", link: router[11].path },
+                    { title: "LifeStyle", link: router[12].path },
+                    { title: "Mariage", link: router[13].path },
+                    { title: "Vidéo", link: router[36].path },
                 ],
             },
         ],
         dataMenu: [
-            { nameMenu: "TARIFS", link: ListDataRouter[2].path },
-            { nameMenu: "CONTACT", link: ListDataRouter[3].path },
-            { nameMenu: "PORTFOLIO", link: ListDataRouter[8].path },
-            { nameMenu: "COMPTE", link: ListDataRouter[6].path },
+            { nameMenu: "TARIFS", link: router[2].path },
+            { nameMenu: "CONTACT", link: router[3].path },
+            { nameMenu: "PORTFOLIO", link: router[8].path },
+            { nameMenu: "COMPTE", link: router[6].path },
         ],
     };
 }
