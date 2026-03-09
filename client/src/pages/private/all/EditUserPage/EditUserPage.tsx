8dcb953602612db8adc29667fcb46421cd0101e8
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-03-09 22:01:03 +0100

    Push for rename 1/3

--- client/src/pages/private/all/EditUserPage/EditUserPage.tsx
+++ client/src/pages/private/all/EditUserPage/EditUserPage.tsx
@@ -1,6 +1,6 @@
 import style from "../../../stylePage.module.css";
 import NavRoot from "../../../../components/layout/nav_Layout/Nav.root.layout";
-import EditUserRoot from "../../../../components/private/All/EditUserRoot/EditUserRoot";
+import EditUserRoot from "../../../../components/private/Alls/EditUserRoot/EditUserRoot";
 import Footer from "../../../../components/layout/footer_Layout/Footer.layout";
 
 function EditUserPage() {
