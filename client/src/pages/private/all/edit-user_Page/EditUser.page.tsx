18d9ef67362e81755150fc88e23fca2434fa84b1
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-03-11 23:10:37 +0100

    Refactorisation de EditUser_Page

--- client/src/pages/private/all/edit-user_Page/EditUser.page.tsx
+++ client/src/pages/private/all/edit-user_Page/EditUser.page.tsx
@@ -1,6 +1,9 @@
+/* Import des modules CSS */
 import style from "../../../stylePage.module.css";
+
+/* Import des Components */
 import NavRoot from "../../../../components/layout/nav_Layout/Nav.root.layout";
-import EditUserRoot from "../../../../components/private/all/EditUserRoot/EditUserRoot";
+import EditUser_Root from "../../../../components/private/all/edit-user/EditUser.root";
 import Footer from "../../../../components/layout/footer_Layout/Footer.layout";
 
 function EditUser_Page() {
@@ -10,7 +13,7 @@ function EditUser_Page() {
                 <NavRoot />
             </header>
             <main className={style.Main}>
-                <EditUserRoot />
+                <EditUser_Root />
             </main>
             <footer className={style.Footer}>
                 <Footer />
