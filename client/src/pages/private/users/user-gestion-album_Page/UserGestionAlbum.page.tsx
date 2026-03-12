bf531add9f3c85b917253f325a14afc3b9fe0128
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-03-12 22:09:32 +0100

    Refactorisation de UserGestionAlbum_Page

--- client/src/pages/private/users/user-gestion-album_Page/UserGestionAlbum.page.tsx
+++ client/src/pages/private/users/user-gestion-album_Page/UserGestionAlbum.page.tsx
@@ -1,16 +1,19 @@
+/* Import des modules CSS */
 import style from "../../../stylePage.module.css";
+
+/* Import des Components */
 import NavRoot from "../../../../components/layout/nav_Layout/Nav.root.layout";
-import UserGestionAlbumRoot from "../../../../components/private/users/UserGestionAlbumRoot/UserGestionAlbumRoot";
+import UserGestionAlbum_Root from "../../../../components/private/users/user-gestion-album/UserGestionAlbum.root";
 import Footer from "../../../../components/layout/footer_Layout/Footer.layout";
 
-function UserGestionAlbumPage() {
+function UserGestionAlbum_Page() {
     return (
-        <div className={`UserGestionAlbumPage ${style.Page}`}>
+        <div className={`UserGestionAlbum_Page ${style.Page}`}>
             <header className={style.Header}>
                 <NavRoot />
             </header>
             <main className={style.Main}>
-                <UserGestionAlbumRoot />
+                <UserGestionAlbum_Root />
             </main>
             <footer className={style.Footer}>
                 <Footer />
@@ -19,4 +22,4 @@ function UserGestionAlbumPage() {
     );
 }
 
-export default UserGestionAlbumPage;
+export default UserGestionAlbum_Page;
