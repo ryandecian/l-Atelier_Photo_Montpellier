608a0568c0866ec40422f3b942cd20d7d25ac5c6
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-03-12 22:28:12 +0100

    Mise a jour temporaire de AlbumPortfolio

--- client/src/components/public/portfolio/AlbumPortfolio.ts
+++ client/src/components/public/portfolio/AlbumPortfolio.ts
@@ -1,8 +1,8 @@
-import ImagesPortraitPortfolioControllerData from "./ImagesPortfolioControllerData";
+import { imagesPortfolio_Data } from "./imagesPortfolio.data";
 
 /* Ce composant a pour objectif d'éviter d'exécuter systématiquement ImagesPortraitPortfolioControllerData à chaque import */
 /* Cela permet une éco-conception et une économie de mise en mémoire */
 
-const AlbumPortfolio = ImagesPortraitPortfolioControllerData
+const AlbumPortfolio = imagesPortfolio_Data;
 
 export default AlbumPortfolio;
