8400e289e512a1dfdf40184129289fad1d21e6d9
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-03-20 21:48:55 +0100

    Refactorisation de mailtoLinkPortraitFamille_Data

--- client/src/components/public/portrait-famille/tarifsPortraitFamille.data.ts
+++ client/src/components/public/portrait-famille/tarifsPortraitFamille.data.ts
@@ -1,5 +1,5 @@
 /* Import des composants Router */
-import externalLinks from "../../../router/routerExt.router";
+import { routerExt } from "../../../router/routerExt.router";
 
 /* Import des Types */
 import { Prestation_Type, FormuleGroupe1_Type, DataEmailFormuleGroupe1_Type } from "../../../types/prestations.type";
@@ -66,7 +66,7 @@ const buildMailtoLink = (to: string, subject: string, body: string) =>
 
 const dataRootEmail: DataEmailFormuleGroupe1_Type = {
     essentiel: {
-        mailto: externalLinks.emailAnne,
+        mailto: routerExt.emailAnne,
         subject: `Réservation ${Service} - Formule ${formule.essentiel}`,
         body: makeBody([
             "Bonjour Anne,",
@@ -82,7 +82,7 @@ const dataRootEmail: DataEmailFormuleGroupe1_Type = {
         ]),
     },
     confort: {
-        mailto: externalLinks.emailAnne,
+        mailto: routerExt.emailAnne,
         subject: `Réservation ${Service} - Formule ${formule.confort}`,
         body: makeBody([
             "Bonjour Anne,",
@@ -98,7 +98,7 @@ const dataRootEmail: DataEmailFormuleGroupe1_Type = {
         ]),
     },
     premium: {
-        mailto: externalLinks.emailAnne,
+        mailto: routerExt.emailAnne,
         subject: `Réservation ${Service} - Formule ${formule.premium}`,
         body: makeBody([
             "Bonjour Anne,",
