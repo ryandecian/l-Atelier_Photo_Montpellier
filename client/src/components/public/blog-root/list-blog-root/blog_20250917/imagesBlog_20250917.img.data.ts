c2f6be30420e7bb5ce2990e571597d2fb0d1b619
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-03-21 23:05:01 +0100

    Correctif de la documentation de imagesBlog_20250917_img_Data

--- client/src/components/public/blog-root/list-blog-root/blog_20250917/imagesBlog_20250917.img.data.ts
+++ client/src/components/public/blog-root/list-blog-root/blog_20250917/imagesBlog_20250917.img.data.ts
@@ -2,12 +2,12 @@
 import { ImagesBlog_Type } from "../../../../../types/components/blogs/imagesBlog.type";
 
 const imagesBlog_20250917_img_Data: ImagesBlog_Type[] = [
-    /* {ImagesBlog_20250917_RootControllerData[0].src} */
+    /* {ImagesBlog_20250917_img_Data[0].src} */
     {
         src: "/images/image-blog/blog_20250917/lapm-photo-professionnel-conference-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - Photographe professionnel à Montpellier - Conférence - L'Atelier Photo Montpellier",
     },
-    /* {ImagesBlog_20250917_RootControllerData[1].src} */
+    /* {ImagesBlog_20250917_img_Data[1].src} */
     {
         src: "/images/image-blog/blog_20250917/lapm-photo-professionnel-congres-corporate-l-atelier-photo-montpellier.jpg",
         alt: "LAPM - Photographe professionnel à Montpellier - Congrès Corporate - L'Atelier Photo Montpellier",
