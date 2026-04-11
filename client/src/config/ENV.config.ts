99c3e65bd185eaa3fbd067bfc4d38b54e2b3bda5
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-04-11 19:50:25 +0200

    Mise a jour de la fonction ENV

--- client/src/config/ENV.config.ts
+++ client/src/config/ENV.config.ts
@@ -1,10 +1,10 @@
 /* Import des Types */
-import { ENV_config_Type } from "./ENV.config.type";
+import { ENV_config_Type } from "./env.config.type";
 
 /**
- * Ce composant permet une utilisation simplifié et strict des variable d'environnement.
- * Le typage est strict et permet donc d'éviter toute erreur de syntaxe ou utilisation de variable inexistant.
- * Si une ou plusieurs variables sont manquantes, cela permet de lancer immédiatement un averticement dans la console.
+ * Ce composant permet une utilisation simplifiée et stricte des variables d'environnement.
+ * Le typage est strict et permet donc d'éviter toute erreur de syntaxe ou utilisation de variable inexistante.
+ * Si une ou plusieurs variables sont manquantes, cela permet de lancer immédiatement un avertissement dans la console.
  */
 
 function ENV(key: ENV_config_Type): string | undefined {
