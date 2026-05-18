7efe148234d7fbf907ed07fd198046baad7eab6e
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-05-18 13:08:52 +0200

    Initialisation du script webPage pour le script JSON-LD

---
+++ client/src/seo/script-json-ld/webPage.script.seo.ts
@@ -0,0 +1,13 @@
+/* Import des Components de Data */
+import { dataMasterSEO_data_SEO as dataMasterSEO } from "../data/dataMasterSEO.data.seo";
+
+function webPage_script_SEO() {
+
+    const JSON_LD = JSON.stringify({});
+
+    return (
+        JSON_LD
+    )
+}
+
+export { webPage_script_SEO };
