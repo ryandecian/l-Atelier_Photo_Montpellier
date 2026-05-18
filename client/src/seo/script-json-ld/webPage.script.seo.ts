32e72465dbadf327696835c8710f918b9c2fa1c5
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-05-19 00:02:31 +0200

    Mise a jour des dataMasterSEO

--- client/src/seo/script-json-ld/webPage.script.seo.ts
+++ client/src/seo/script-json-ld/webPage.script.seo.ts
@@ -5,7 +5,10 @@ import { dataMasterSEO_data_SEO as dataMasterSEO } from "../data/dataMasterSEO.d
 import { generateURLSlug_Utils } from "../utils/generateURLSlug.utils";
 
 type Data = {
-    "uri": string, /* URI de la page sans le nom de domaine (router) */
+    "uri_cgv": string, /* URI de la page des CGV sans le nom de domaine (router) */
+    "description": string, /* Description de la page pour les Mentions Légales */
+    "image"?: string, /* Image représentative de la page */
+    "uri_page": string, /* URI de la page sans le nom de domaine (router) */
     "id": string, /* Identifiant unique de la page (simple string avec des - et sans le #). En cas d'erreur de syntaxe une fonction le corrigera */
 }
 
@@ -15,11 +18,27 @@ function webPage_script_SEO(data: Data): string {
     const JSON_LD = JSON.stringify({
         "@context": dataMasterSEO["@context"],   /* (Obligatoire) URL de Google schéma */
         "@type": "WebPage", /* (Obligatoire) Type de JSON-LD */
-        "@id": `${url}${data.uri}/#${generateURLSlug_Utils(data.id)}`, /* @id SEO-friendly, identifiant unique de la page, correspond à l'URL de la page + mots clés (ex : domain/uri/#mot-clé ) */
-        "name": dataMasterSEO.name, /* (Obligatoire) Nom de la page */
-        "url": `${url}${data.uri}`, /*(Obligatoire) URL de la page + ancre SEO-friendly (ex : domain/uri) */
+        "@id": `${url}${data.uri_page}/#${generateURLSlug_Utils(data.id)}`, /* @id SEO-friendly, identifiant unique de la page, correspond à l'URL de la page + mots clés (ex : domain/uri/#mot-clé ) */
+        "name": dataMasterSEO.name, /* (Obligatoire) Nom de l'entreprise */
+        "url": `${url}${data.uri_page}`, /*(Obligatoire) URL de la page + ancre SEO-friendly (ex : domain/uri) */
         "description": dataMasterSEO.description, /* (Obligatoire) Description de la page pour les Mentions Légales */
         "inLanguage": "fr", /* (Obligatoire) Langue de la page */
+        "specialty": "LegalService", /* Indique explicitement aux IA la nature juridique du document */
+        ...(data.image && { "image": data.image }), /* (Facultatif) Image représentative de la page */
+        "termsOfService": `${url}${data.uri_cgv}` /* (Obligatoire) URL de la page des CGV (ex : domain/uri-cgv) */,
+        "provider": {
+            "@id": dataMasterSEO.id_LocalBusiness       /* (Obligatoire) Référence à l'ID de l'Entreprise */
+        },
+        "isPartOf": { /* Liaison indiquant que la page appartient au site global */
+            "@type": "WebSite",
+            "name": dataMasterSEO.name, /* (Obligatoire) Nom de l'entreprise */
+            "url": url /* URL de la page d'accueil de l'entreprise (Dynamique) */
+        },
+        "mainEntity": {
+            "@type": "Organization", 
+            "name": dataMasterSEO.name, /* (Obligatoire) Nom de l'entreprise */
+            "url": url /* URL de la page d'accueil de l'entreprise (Dynamique) */
+        }, /* Entité principale de l'entreprise */
     });
 
     return (
