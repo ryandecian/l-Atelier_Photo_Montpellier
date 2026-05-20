ce986409595d72d2f3f3cbce1e08c82ee97ea946
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-05-21 01:11:56 +0200

    Refacto du script webPage, déplacement du script dans un dossier dédié, création d'un fichier type spécifique

--- client/src/seo/script-json-ld/json-ld-webPage/webPage.script.seo.ts
+++ client/src/seo/script-json-ld/json-ld-webPage/webPage.script.seo.ts
@@ -1,18 +1,11 @@
 /* Import des Components de Data */
-import { dataMasterSEO_data_SEO as dataMasterSEO } from "../data/dataMasterSEO.data.seo";
+import { dataMasterSEO_data_SEO as dataMasterSEO } from "../../data/dataMasterSEO.data.seo";
 
 /* Import des Utils */
-import { generateURLSlug_Utils } from "../utils/generateURLSlug.utils";
+import { generateURLSlug_Utils } from "../../utils/generateURLSlug.utils";
 
-type Data = {
-    /*"description": "Mentions légales de l’Atelier Photo Montpellier. Retrouvez toutes les informations juridiques concernant le site, l’éditeur, l’hébergement et la propriété intellectuelle." */
-    "description": string; /* Description de la page pour les Mentions Légales */
-    "id": string; /* Identifiant unique de la page (string simple, même règle que les URL). */
-    "image"?: string; /* Image représentative de la page */
-    "name_page": string; /* Nom de la page (espace autorisé, 10 à 60 caractères max), (Généralement = au H1 HTML) */
-    "uri_cgv": string; /* URI de la page des CGV sans le nom de domaine (router) */
-    "uri_page": string; /* URI de la page sans le nom de domaine (router) */
-}
+import { WebPage_script_Type } from "./webPage.script.type";
+
 
 /**
  * ### Documentation : Script JSON-LD Service de Google.
@@ -26,7 +19,7 @@ type Data = {
  * @param {Object} data - L'objet contenant les information necessaire au script JSON-LD.
  * @param {string} data.description - **[description]** - Description de la page pour les Mentions Légales
  * @param {string} data.id - **[id]** - Identifiant unique de la page (string simple, même règle que les URL).
- * @param {string} [data.image] - **[image]** - Image représentative de la page (Facultatif)
+ * @param {string} [data.uri_image] - **[image]** - Image représentative de la page (Facultatif)
  * @param {string} data.name_page - **[name_page]** - Nom de la page (espace autorisé, 10 à 60 caractères max), (Généralement = au H1 HTML)
  * @param {string} data.uri_cgv - **[uri_cgv]** - URI de la page des CGV sans le nom de domaine (router)
  * @param {string} data.uri_page - **[uri_page]** - URI de la page sans le nom de domaine (router)
@@ -36,13 +29,13 @@ type Data = {
  * @returns {string} Retourne une string grâce à la fonction JSON.stringify, necessaire pour l'injection dans une page.
  */
 
-function webPage_script_SEO(data: Data): string {
+function webPage_script_SEO(data: WebPage_script_Type): string {
     const JSON_LD = JSON.stringify({
         "@context": dataMasterSEO["@context"], /* (Obligatoire) URL de Google schéma */
         "@type": "WebPage",                    /* (Obligatoire) Type de JSON-LD */
         "@id": `${dataMasterSEO.url}${data.uri_page}/#${generateURLSlug_Utils(data.id)}`, /* @id SEO-friendly, identifiant unique de la page, correspond à l'URL de la page + mots clés (ex : domain/uri/#mot-clé ) */
         "description": data.description,   /* (Obligatoire) Description de la page pour les Mentions Légales */
-        ...(data.image && { "image": data.image }), /* (Facultatif) Image représentative de la page */
+        ...(data.uri_image && { "image": `${dataMasterSEO.url}${data.uri_image}` }), /* (Facultatif) Image représentative de la page */
         "inLanguage": "fr", /* (Obligatoire) Langue de la page */
 
         /* Indique le site web global auquel appartient cette page spécifique (Rattachement sémantique) */
---
+++ client/src/seo/script-json-ld/json-ld-webPage/webPage.script.type.ts
@@ -0,0 +1,60 @@
+type WebPage_script_Type = {
+    /*"description": "Mentions légales de l’Atelier Photo Montpellier. Retrouvez toutes les informations juridiques concernant le site, l’éditeur, l’hébergement et la propriété intellectuelle." */
+
+    /**
+     * ⚠️ **NOTE ET CONTRAINTE SEO** : Description de la page, utilisée pour les Mentions Légales et pour fournir un résumé clair du contenu de la page aux moteurs de recherche et aux utilisateurs.
+     * - Doit faire entre 50 et 300 caractères.
+     * _Une string hors de ces limites pénalisera le score GEO/SEO._
+     */
+    "description": string; /* Description de la page pour les Mentions Légales */
+
+    /**
+     * ⚠️ **NOTE ET CONTRAINTE** : Doit être une string simple, même règle que les URL, et correspondre à l'ID utilisée dans les autres scripts JSON-LD de la page (ex : BreadcrumbList).
+     * - URI uniquement
+     * - Ne pas passer le # au début de l'ID, le script s'en chargera automatiquement.
+     * - Ne pas commencer par un slash (/), le script s'en chargera automatiquement.
+     * - Doit être au format URL. Si le format est incorrect, une fonction de correction tentera de le corriger automatiquement.
+     * 
+     * ---
+     * @example "mentions-legales" (correspondra à domain/uri/#mentions-legales)
+     * @example "Mentions légales " (correspondra à domain/uri/#mentions-legales)
+     */
+    "id": string; /* Identifiant unique de la page (string simple, même règle que les URL). */
+
+    /**
+     * ⚠️ **DESCRIPTION** : Image optionnel représentative de la page, qui peut être utilisée par les moteurs de recherche et les réseaux sociaux pour illustrer la page dans les résultats de recherche ou les partages.
+     * - Doit être une URI valide, sans le nom de domaine et commençant par un slash (ex : /images/photo-article.jpg).
+     * - Ratio : 16:9 - 1200 x 675 px minimum, 1920 x 1080 px maximum recommandé.
+     * - 50 à 150 Ko maximum, 100 Ko recommandé.
+     * - JPG
+     * 
+     * ---
+     * @example "/images/photo-article.jpg"
+     */
+    "uri_image"?: string; /* Image représentative de la page */
+
+    /** 
+     * ⚠️ **DESCRIPTION** : Représente le nom de la page.
+     * 
+     * ---
+     * ⚠️ **CONTRAINTE SEO** : Doit faire entre 10 et 60 caractères.
+     * _Une string hors de ces limites pénalisera le score GEO/SEO._
+     */
+    "name_page": string; /* Nom de la page (espace autorisé, 10 à 60 caractères max), (Généralement = au H1 HTML) */
+
+    /**
+     * ⚠️ **NOTE ET CONTRAINTE** : Doit être une URI valide, sans le nom de domaine, et correspondre à l'URI utilisée dans les autres scripts JSON-LD de la page (ex : BreadcrumbList).
+     * - Doit être récupérer depuis le router.
+     * - Ne doit pas contenir le nom de domaine.
+     */
+    "uri_cgv": string;
+
+    /**
+     * ⚠️ **NOTE ET CONTRAINTE** : Doit être une URI valide, sans le nom de domaine, et correspondre à l'URI utilisée dans les autres scripts JSON-LD de la page (ex : BreadcrumbList).
+     * - Doit être récupérer depuis le router.
+     * - Ne doit pas contenir le nom de domaine.
+     */
+    "uri_page": string; /* URI de la page sans le nom de domaine (router) */
+}
+
+export type { WebPage_script_Type };
