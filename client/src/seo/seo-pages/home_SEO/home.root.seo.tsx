e82260d292808868bb018ad652f31b6e2bb249bd
Ryan DECIAN <ryan.decian.propgmail.com>
2026-07-29 22:45:59 +0200

    Push

--- client/src/seo/seo-pages/home_SEO/home.root.seo.tsx
+++ client/src/seo/seo-pages/home_SEO/home.root.seo.tsx
@@ -2,14 +2,14 @@
 import { home_data_SEO } from "./home.data.seo";
 
 /* Import des JSON_LD */
-import { homePage_script_SEO } from "../../script-json-ld/json-ld-homePage/homePage.script.seo";
+import { HomePage_script_SEO } from "../../script-json-ld/json-ld-homePage/homePage.script.seo";
 import { LocalBusiness_script_SEO } from "../../json-ld-localbusiness/localBusiness.script.seo";
 
 function Home_root_SEO() {
     return (
         <>
             <LocalBusiness_script_SEO />
-            <homePage_script_SEO data={home_data_SEO.homePage_data_SEO} />
+            <HomePage_script_SEO data={home_data_SEO.homePage_data_SEO} />
         </>
     )
 }
